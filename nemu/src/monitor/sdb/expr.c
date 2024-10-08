caa65b974d6fdedd9a96032b78e2ea9ad713a14a
tracer-ysyx <tracer@ysyx.org>
2024-10-08 17:20:06 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:20:06 up  2:57,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -196,7 +196,7 @@ uint32_t eval(int p, int  q) {
       case '+': return val1 + val2;
       case '-': return val1 - val2;
       case '*': return val1 * val2;
-      case '/': return val1 / val2;
+      case '/': Assert(val2 != 0,"fenmuwei0."); return val1 / val2;
       default: assert(0);
     }
   }
