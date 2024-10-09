ad1c99fb1a73bb2608d782e406473964feba08e1
tracer-ysyx <tracer@ysyx.org>
2024-10-09 22:37:11 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:37:11 up 46 min,  1 user,  load average: 1.48, 0.45, 0.26

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -186,7 +186,7 @@ uint32_t eval(int p, int  q) {
 			if(!flag&&(tokens[i].type == '*' || tokens[i].type == '/')) {
 				op = max(op, i);
 			}
-			printf("%d %d %d\n",i,flag,op);
+//			printf("%d %d %d\n",i,flag,op);
 		}
 
     uint32_t val1 = eval(p, op - 1);
