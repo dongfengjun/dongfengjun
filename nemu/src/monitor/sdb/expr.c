27ed36ab24e33e601d88ceecaada50978f91e03c
tracer-ysyx <tracer@ysyx.org>
2024-10-10 21:50:17 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:50:17 up  1:00,  1 user,  load average: 0.03, 0.04, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -104,7 +104,7 @@ static bool make_token(char *e) {
 					case TK_NOTYPE:
 						break;
           default:
-						Assert(nr_token < 65536, "The tokens array has insufficient storage space."); //32*32*(4+4)*8=65536
+						Assert(nr_token < 65536, "The tokens array has insufficient storage space.");
 						Assert(substr_len < 32, "The token is too long");
 						tokens[nr_token].type = rules[i].token_type;
 						strncpy(tokens[nr_token].str, substr_start, substr_len);
@@ -117,7 +117,7 @@ static bool make_token(char *e) {
       }
     }
 
-		printf("%d\n", nr_token);
+//	printf("%d\n", nr_token);
     if (i == NR_REGEX) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
@@ -145,6 +145,7 @@ word_t expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
+	printf("%d\n", nr_token - 1);
 	printf("%d\n", eval(0, nr_token - 1));
 	return 0;
 }
@@ -152,7 +153,7 @@ word_t expr(char *e, bool *success) {
 uint32_t eval(int p, int  q) {
   if (p > q) {
     /* Bad expression */
-//	assert(0);
+		assert(0);
 		return -1;
   }
   else if (p == q) {
