f943a2f37fcd6ac0809b5755a9d9338eec661beb
tracer-ysyx <tracer@ysyx.org>
2024-10-12 14:28:37 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:28:36 up  1:17,  1 user,  load average: 0.10, 0.05, 0.05

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -48,8 +48,8 @@ static struct rule {
 
 	{"<=", LEQ},			//leq
 	{"!=", NOTEQ},		//noteq
-	{"\\|\\|", OR},				//or
-	{"\\&\\&", AND},			//and
+	{"||", OR},				//or
+	{"&&", AND},			//and
 	{"\\!", '!'},					//non
 	{"\\$[a-zA-Z]*[0-9]*", REG},		//reg_name
 	{"\\0[xX][0-9a-fA-F]+", HEX},		//hexadecimal-number
