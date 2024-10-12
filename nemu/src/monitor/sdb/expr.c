c75ef4735b86452c80e7ec9ff4fed3a981356c79
tracer-ysyx <tracer@ysyx.org>
2024-10-12 15:29:55 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:29:55 up  2:18,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -109,6 +109,19 @@ static bool make_token(char *e) {
         switch (rules[i].token_type) {
 					case TK_NOTYPE:
 						break;
+					case UNEQ:
+					case TK_EQ:
+					case OR:
+					case AND:
+					case REG:
+					case HEX:
+						Assert(nr_token < 32, "The tokens array has insufficient storage     space.");
+            Assert(substr_len < 32, "The token is too long");
+            tokens[nr_token].type = rules[i].token_type;
+            strncpy(tokens[nr_token].str, substr_start, substr_len);
+            tokens[nr_token].str[substr_len] = '\0';
+            nr_token++;
+            break;
           default:
 						Assert(nr_token < 32, "The tokens array has insufficient storage space.");
 						Assert(substr_len < 32, "The token is too long");
