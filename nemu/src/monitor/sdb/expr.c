94194c8a3ec2679de2c263998c9f60d5ee12d1da
tracer-ysyx <tracer@ysyx.org>
2024-10-14 15:15:27 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:15:27 up  2:22,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -372,6 +372,7 @@ uint32_t eval(int p, int  q) {
 				}
 			case TK_EQ: return val1 == val2;
 			case UNEQ: return val1 != val2;
+			case LEQ: return val1 <= val2;
 			case OR: return val1 || val2;
 			case AND: return val1 && val2;
       default: printf("No op type.\n"); assert(0);
