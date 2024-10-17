c5e2c2630bc40c57a050bff80a63de72ddc8adf8
tracer-ysyx <tracer@ysyx.org>
2024-10-17 18:02:30 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-45-generic #45-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 30 12:02:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:02:30 up 25 min,  1 user,  load average: 0.02, 0.05, 0.08

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -65,11 +65,13 @@ word_t isa_reg_str2val(const char *s, bool *success) {
   for(int i = 0 ; i < 32 ; i ++){
    	if(strcmp(regs[i], s) == 0){
 			return cpu.gpr[i];
+			printf("str=%s\n", s);
 			break;
 		}
 	}
 	if(strcmp(s, "pc") == 0) {
 		return cpu.pc;
+		printf("str=pc\n");
 	}
   success = false;
   return 0;
