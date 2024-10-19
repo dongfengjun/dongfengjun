d615a4f291781a81ddf83e49ec1e6237b8d04843
tracer-ysyx <tracer@ysyx.org>
2024-10-19 09:07:38 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-47-generic #47-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 21:40:26 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:07:38 up 29 min,  1 user,  load average: 0.04, 0.07, 0.07

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -210,7 +210,7 @@ static int cmd_p(char *args) {
 	}
 //printf("args = %s\n", args);
 	bool success = false;
-	printf("expr result = %u", expr(args, &success));
+	printf("expr result = %u\n", expr(args, &success));
 	return 0;
 }
 
