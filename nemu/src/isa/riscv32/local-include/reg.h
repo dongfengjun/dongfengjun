95c65f28a1e401e49e2c07cf3b615ffc0632a3b7
tracer-ysyx <tracer@ysyx.org>
2024-10-19 10:09:11 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-47-generic #47-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 21:40:26 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:09:11 up  1:30,  1 user,  load average: 0.11, 0.06, 0.01

--- nemu/src/isa/riscv32/local-include/reg.h
+++ nemu/src/isa/riscv32/local-include/reg.h
@@ -23,9 +23,9 @@ static inline int check_reg_idx(int idx) {
   return idx;
 }
 
-#define gpr(idx) cpu.gpr[check_reg_idx(idx)]
+#define gpr(idx) (cpu.gpr[check_reg_idx(idx)])
 
-static inline const char* reg_name(int idx, int width) {
+static inline const char* reg_name(int idx) {
   extern const char* regs[];
   return regs[check_reg_idx(idx)];
 }
--- npc/vsrc/top.v
+++
@@ -1,8 +0,0 @@
-module top(
-	//input clk;
-	//input rst;
-  input a,
-  input b,
-  output f);
-  assign f = a ^ b;
-endmodule
