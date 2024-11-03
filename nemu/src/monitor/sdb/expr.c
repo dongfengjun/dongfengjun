88fd6b5ad7c47a21551a6ee497b23b9c1e5d9529
tracer-ysyx <tracer@ysyx.org>
2024-11-03 22:34:43 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-47-generic #47-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 21:40:26 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:34:43 up 50 min,  1 user,  load average: 0.09, 0.17, 0.19

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -353,7 +353,7 @@ uint32_t eval(int p, int  q) {
 				while(tokens[j].type != ')') {//s循环
 					j++;
 					if(j == q) {
-						printf("ERROR\n");
+						printf("ERROR ) \n");
 						assert(0);
 					}
 				}
