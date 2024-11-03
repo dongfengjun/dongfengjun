e076c3532b54b0d55ce86da78066ee13b3fd9b28
tracer-ysyx <tracer@ysyx.org>
2024-11-03 15:28:22 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-47-generic #47-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 21:40:26 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:28:22 up  1:17,  1 user,  load average: 0.01, 0.04, 0.12

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -36,7 +36,7 @@ static char *code_format =
 int index_buf = 0;
 
 int choose(int n){
-	int flag =rand() % 3 ;
+	int flag =rand() % n;
 	return flag;
 }
 
@@ -79,6 +79,7 @@ static void gen_rand_expr() {
 		case 1: gen('('); gen_rand_expr(); gen(')'); break;
 		default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
 	}
+//	buf[index_buf ++] = '\0';
 }
 
 int main(int argc, char *argv[]) {
