4d05f88adb506540198e37d6272d60e6e6758936
tracer-ysyx <tracer@ysyx.org>
2024-11-03 22:26:52 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-47-generic #47-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 21:40:26 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:26:52 up 42 min,  1 user,  load average: 0.51, 0.54, 0.29

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -414,7 +414,6 @@ bool check_parentheses(int p, int q) {
 	if(tokens[p].type != '(' || tokens[q].type != ')')
 		return false;
 	int l = p+1, r = q-1;
-	printf("l=%d,r=%d\n",l,r);
 	while(l < r) {
 		if(tokens[l].type == '(') {
 			if(tokens[r].type == ')') {
