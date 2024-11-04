eee09c33d73e876455ffa24de8964a97a77ac1ec
tracer-ysyx <tracer@ysyx.org>
2024-11-04 20:59:12 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:59:12 up  2:38,  1 user,  load average: 0.07, 0.12, 0.16

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -1,18 +1,3 @@
-/***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
-
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,9 +6,66 @@
 #include <string.h>
 
 // this should be enough
-static char buf[65536] = {};
-static char code_buf[65536 + 128] = {}; // a little larger than `buf`
+static char buf[65536];
 
+/*
+ * inline expansion, or inlining, is a manual or compiler optimization that
+ * replaces a function call site with the body of the called function
+ * 
+ * it's faster than call function
+ */
+uint32_t choose(uint32_t n) {
+	return rand() % n;	
+}
+
+static inline void gen_num() {
+	char s[4];
+	uint32_t n = choose(99);
+	
+	/* send the formatted data to string */
+	sprintf(s, "%u", n);
+	strcat(buf, s);		
+}
+
+static inline void gen(char str) {
+	/* generate random white space */
+	uint32_t lSpace = choose(2);
+	uint32_t rSpace = choose(2);
+	
+	char s[lSpace + 1 + rSpace];
+
+	uint32_t i;
+
+	for (i = 0; i < lSpace; i++) s[i] = ' ';
+	s[i++] = str;
+	for (;i < lSpace + 1 + rSpace; i++) s[i] = ' ';
+    s[lSpace + 1 + rSpace] = '\0';	
+	strcat(buf, s);
+
+}
+
+static inline void gen_rand_op() {
+
+	switch (choose(4)) {
+		case 0: gen('+'); break;
+		case 1: gen('-'); break;
+		case 2: gen('*'); break;
+		case 3: gen('/'); break;
+	}	
+}
+
+static inline void gen_rand_expr() {
+  
+  switch(choose(3)) {
+	case 0: gen_num(); break;
+	case 1: gen('('); gen_rand_expr(); gen(')'); break;
+	default:  gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break; 
+  
+  }
+
+}
+
+static char code_buf[65536];
 static char *code_format =
 "#include <stdio.h>\n"
 "int main() { "
@@ -32,47 +74,6 @@ static char *code_format =
 "  printf(\"%%u\", result); "
 "  return 0; "
 "}";
-
-/***补充函数***/
-int choose(int n){
-	return =rand() % n ;
-}
-
-void gen_num() {
-	int num = choose(99);
-	char s[4];
-	sprintf(s, "%d", num);
-	strcat(buf, s);
-}
-
-void gen_rand_op() {
-/***    char op[4] = {'+', '-', '*', '/'};
-    char s[1];
-		s[0] = op[choose(4)];
-		strcat(buf, s);  ***/
-		switch (choose(4)) {
-			case 0: gen('+'); break;
-			case 1: gen('-'); break;
-			case 2: gen('*'); break;
-			case 3: gen('/'); break;
-	}
-}
-
-static inline void gen(char c){
-	char s[1];
-  s[0] = c;
-  strcat(buf, s);
-}
-
-/******/
-static void gen_rand_expr() {
-//  buf[0] = '\0';
-	switch (choose(3)) {
-		case 0: gen_num(); break;
-		case 1: gen('('); gen_rand_expr(); gen(')'); break;
-		default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
-	}
-}
 
 int main(int argc, char *argv[]) {
   int seed = time(0);
@@ -101,12 +102,10 @@ int main(int argc, char *argv[]) {
     int result;
     int fsn = fscanf(fp, "%d", &result);
     pclose(fp);
-
-//	printf("%s\n", buf);
-		if(fsn == 1) {
-			printf("%u %s\n", result, buf);
+		if(fsn) {
+			printf("%u\t %s\n", result, buf);
 		}
-		memset(buf, '\0', 65536);
+	memset(buf, '\0', 65536);
   }
   return 0;
 }
