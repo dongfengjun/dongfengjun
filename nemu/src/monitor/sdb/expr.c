c39d1638b3ab7b97f5ce08f8ab13d8a4937f7ace
tracer-ysyx <tracer@ysyx.org>
2024-11-04 11:36:45 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-47-generic #47-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 21:40:26 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:36:45 up 36 min,  1 user,  load average: 0.12, 0.25, 0.21

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -234,6 +234,13 @@ static bool make_token(char *e) {
 bool check_parentheses(int p, int q);
 int max(int a, int b);
 uint32_t eval(int p, int  q);
+int min(int a, int b) {
+    if (a > b) {
+        return b;
+    } else {
+        return a;
+    }
+} 
 
 word_t expr(char *e, bool *success) {
   if (!make_token(e)) {
@@ -358,6 +365,7 @@ uint32_t eval(int p, int  q) {
     return eval(p + 1, q - 1);
   }
   else {
+		//TODO：优先级添加判断括号
     int op = -1;//the position of 主运算符 in the token expression;
 		bool flag = false;
 		for(int i = p; i < q; i++) {
@@ -387,7 +395,7 @@ uint32_t eval(int p, int  q) {
 			}
 			if(!flag && (tokens[i].type == '*' || tokens[i].type == '/')) {
 //				op = max(op, i);
-				op = i;
+				op = min(op, i);
 			}
 			printf("%d %d %d\n",i,flag,op);
 		}
