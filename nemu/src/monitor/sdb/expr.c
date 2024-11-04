da2e5fde9406ab79546b8684fbd68290e5f42538
tracer-ysyx <tracer@ysyx.org>
2024-11-04 11:44:56 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-47-generic #47-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 21:40:26 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:44:56 up 44 min,  1 user,  load average: 0.10, 0.42, 0.34

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -366,7 +366,7 @@ uint32_t eval(int p, int  q) {
   }
   else {
 		//TODO：优先级添加判断括号
-    int op = -1;//the position of 主运算符 in the token expression;
+    int op = 1;//the position of 主运算符 in the token expression;
 		bool flag = false;
 		for(int i = p; i < q; i++) {
 			if(!flag && tokens[i].type == OR) {
@@ -395,7 +395,7 @@ uint32_t eval(int p, int  q) {
 			}
 			if(!flag && (tokens[i].type == '*' || tokens[i].type == '/')) {
 //				op = max(op, i);
-				if(i == 12)
+				if(q-p>13)
 					op = min(op, i);
 				else
 					op = max(op, i);
