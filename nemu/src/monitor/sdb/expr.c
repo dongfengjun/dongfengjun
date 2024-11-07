031f0ebc6e3b908d25d850c32dd64adb0633888c
tracer-ysyx <tracer@ysyx.org>
2024-11-07 18:39:26 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:39:26 up 36 min,  1 user,  load average: 0.38, 0.21, 0.13

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -349,8 +349,8 @@ uint32_t eval(int p, int  q) {
 		bool as = false;
 		for(int i = p; i <= q; i++) {
 			if((tokens[i].type == '(')) {
-				int countl = 1, countr = 0;
 				for(int j = i; j <= q; j++) {
+					int countl = 1, countr = 0;
 					if((tokens[j].type == '('))
 						countl++;
 					if((tokens[j].type == ')'))
