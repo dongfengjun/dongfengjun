1fcd3704b1a1157ca1c872cfbf94edd81ff2fb1c
tracer-ysyx <tracer@ysyx.org>
2024-11-08 11:36:53 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:36:53 up  1:07,  1 user,  load average: 0.08, 0.04, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -394,10 +394,8 @@ uint32_t eval(int p, int  q) {
 			printf("%d %d %d\n",i,flag,op);
 		}
 
-    uint32_t val1 = eval(p, op - 1);
-		printf("val1=%u\n",val1);
-    uint32_t val2 = eval(op + 1, q);
-		printf("val2=%u\n",val2);
+    int val1 = eval(p, op - 1);
+    int val2 = eval(op + 1, q);
 		int op_type = tokens[op].type;
     switch (op_type) {
       case '+': return val1 + val2;
