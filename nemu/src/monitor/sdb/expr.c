dd47327694087a9f6a25233be3d86817694c6296
tracer-ysyx <tracer@ysyx.org>
2024-11-11 18:05:40 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:05:40 up  7:42,  1 user,  load average: 0.25, 0.07, 0.02

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -169,20 +169,7 @@ static bool make_token(char *e) {
         switch (rules[i].token_type) {
 					case TK_NOTYPE:
 						break;
-          case TK_EQ:
-					case NUM:
-					case UNEQ:
-					case LEQ:
-					case OR:
-					case AND:
-					case REG:
-					case HEX:
-					case '+':
-					case '-':
-					case '*':
-					case '/':
-					case '(':
-					case ')':
+          default:
 						Assert(nr_token < 65536, "The tokens array has insufficient storage space.");
 						Assert(substr_len < 32, "The token is too long");
 						tokens[nr_token].type = rules[i].token_type;
@@ -190,9 +177,10 @@ static bool make_token(char *e) {
 						tokens[nr_token].str[substr_len] = '\0';
 						nr_token++;
 						break;
-					default:
+/**
 						printf("no match at position %d\n%s\n%*.s^\n", position, e, position,"");
 						continue;
+						*/
         }
 				break;
     }
