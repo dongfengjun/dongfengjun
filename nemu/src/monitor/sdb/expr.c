ed6a9d8f20d859cc48732c12d7a7461f889be85c
tracer-ysyx <tracer@ysyx.org>
2024-11-11 19:52:17 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:52:17 up 19 min,  1 user,  load average: 0.01, 0.04, 0.08

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -181,7 +181,7 @@ static bool make_token(char *e) {
 				break;
 			}
 		}
-		if(i == NR_REGEX-1) {
+		if(i == NR_REGEX) {
 			printf("no match at position %d\n%s\n%*.s^\n", position, e, position,"");
 			return false;
 		}
