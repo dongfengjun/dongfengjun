1ee2358277894eb561789299b06d8890a67fa88c
tracer-ysyx <tracer@ysyx.org>
2024-11-11 20:46:44 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:46:44 up  1:13,  1 user,  load average: 0.25, 0.14, 0.09

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -53,7 +53,7 @@ static struct rule {
 	{"\\$[a-zA-Z]*[0-9]*", REG},		//reg_name
 	{"\\0[xX][0-9a-fA-F]+", HEX},		//hexadecimal-number
 	{"[0-9]*", NUM},
-	{"\\?", UNID}
+	{"\\%", UNID}
 };
 
 #define NR_REGEX ARRLEN(rules)
