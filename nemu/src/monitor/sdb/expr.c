b468401fc4d31f4692a4453c6551b6c7ca2ab13f
tracer-ysyx <tracer@ysyx.org>
2024-11-11 20:05:32 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:05:32 up 32 min,  1 user,  load average: 0.00, 0.00, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -151,7 +151,7 @@ static bool make_token(char *e) {
 
   while (e[position] != '\0') {
     /* Try all rules one by one. */
-    for (i = 0; i <= NR_REGEX; i ++) {
+    for (i = 0; i < NR_REGEX; i ++) {
 			if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
@@ -178,10 +178,10 @@ static bool make_token(char *e) {
 						nr_token++;
 						break;
         }
-				break;
+//				break;
 			}
 		}
-		if(i == NR_REGEX) {
+		if(i == NR_REGEX-1) {
 			printf("no match at position %d\n%s\n%*.s^\n", position, e, position,"");
 			return false;
 		}
