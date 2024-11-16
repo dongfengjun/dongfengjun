79764f093a439fb0fe0eb5af7453c534526b8edf
tracer-ysyx <tracer@ysyx.org>
2024-11-16 17:49:45 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:49:45 up  1:37,  1 user,  load average: 0.09, 0.03, 0.02

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -273,9 +273,9 @@ word_t expr(char *e, bool *success) {
 			tokens[i].type = TK_NOTYPE;
 			uint32_t addr = char2int(tokens[i+1].str);
 			uintptr_t addrptr = addr;
-			int* ptr = (int*)addrptr;
+			uint32_t* ptr =(uint32_t*)addrptr;
 			printf("%u\n%lu\n",addr,addrptr);
-			printf("%d\n",*ptr);
+			printf("%u\n",*ptr);
 			int value = 0;
 //			memcpy(&value, (void*)a, sizeof(int));
 			int2char(value, tokens[i+1].str);
