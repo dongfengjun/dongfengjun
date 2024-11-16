6a85a5fd4360d7cc06b6465ae0cb2c294308be07
tracer-ysyx <tracer@ysyx.org>
2024-11-16 20:50:32 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:50:32 up  1:56,  1 user,  load average: 0.00, 0.02, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -273,17 +273,10 @@ word_t expr(char *e, bool *success) {
 	for (int i = 0; i < nr_token; i ++) {
 		if (tokens[i].type == '*' && (i == 0 || ((tokens[i-1].type != NUM && tokens[i-1].type != HEX) && tokens[i-1].type != (int)(')')))) {
 			tokens[i].type = TK_NOTYPE;
-			bool flag = false;
-			int tmp = expr(tokens[i+1].str,&flag);
-			printf("tmp=%d\n",tmp);
-			char s[32];
-			sprintf(s, "%x", tmp);
 			paddr_t addr = 0;
-			sscanf(s, "%x", &addr);
+			sscanf(tokens[i+1].str, "%x", &addr);
 			uint32_t value = *(uint32_t *)guest_to_host(addr);
 			uint2char(value, tokens[i+1].str);
-			printf("=%s\n", tokens[i+1].str);
-			printf("value=%u\n", value);
 			for(int j = 0 ; j < nr_token ; j ++) {
 				if(tokens[j].type == TK_NOTYPE) {
 					for(int k = j +1 ; k < nr_token; k ++) {
@@ -318,7 +311,7 @@ word_t expr(char *e, bool *success) {
 }
 
 word_t eval(int p, int  q) {
-	printf("p=%d,q=%d\n",p,q);
+//	printf("p=%d,q=%d\n",p,q);
 	if (p > q) {
     /* Bad expression */
 //		Assert(0,"ERROR:Bad expression\n");
