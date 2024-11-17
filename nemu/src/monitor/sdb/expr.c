bba4613f367bb367692cfe4fc552a796138f9260
tracer-ysyx <tracer@ysyx.org>
2024-11-17 11:11:20 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:11:20 up 47 min,  1 user,  load average: 0.04, 0.01, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -325,7 +325,7 @@ word_t expr(char *e, bool *success) {
 }
 
 word_t eval(int p, int  q) {
-//	printf("p=%d,q=%d\n",p,q);
+	printf("p=%d,q=%d\n",p,q);
 	if (p > q) {
     /* Bad expression */
 //		Assert(0,"ERROR:Bad expression\n");
