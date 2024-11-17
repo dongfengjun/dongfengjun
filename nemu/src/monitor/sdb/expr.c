50b56d221b4b0e8172f8ba38254b38534cc851f0
tracer-ysyx <tracer@ysyx.org>
2024-11-17 13:50:05 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:50:05 up  3:26,  1 user,  load average: 0.61, 0.26, 0.10

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -229,7 +229,7 @@ word_t expr(char *e, bool *success) {
 	}
 	/***处理负号***/
 	for(int i = 0; i < nr_token; i++) {
-		if(tokens[i].type == '-' && (i == 0 || ((tokens[i-1].type != NUM && tokens[i+1].type == NUM ) && tokens[i-1].type != ')' ))) {
+		if(tokens[i].type == '-' && (i == 0 || ((tokens[i-1].type != NUM && tokens[i-1].type != HEX && tokens[i+1].type == NUM ) && tokens[i-1].type != ')' ))) {
 			printf("The EXPR contains negative signs.\n");
 			tokens[i].type = TK_NOTYPE;
 			for(int j = 31; j >= 0; j--) {
