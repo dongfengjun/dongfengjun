eec692f7df6fb4c7108f75203ca293b788927fa5
tracer-ysyx <tracer@ysyx.org>
2024-11-17 11:53:44 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:53:44 up  1:30,  1 user,  load average: 0.08, 0.21, 0.18

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -304,7 +304,7 @@ word_t expr(char *e, bool *success) {
 	}
 	word_t result = 0;
 	int numl = 0,numr = 0;
-  for(int i = 0; i < nr_token; i++) {
+  for(int i = 0; i <= nr_token; i++) {
 		if(tokens[i].type == '(')
         numl += 1;
     if(tokens[i].type == ')')
