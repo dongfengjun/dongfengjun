507fce6f6708bb37f47c441c6e294532bef52025
tracer-ysyx <tracer@ysyx.org>
2024-11-17 11:28:54 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:28:54 up  1:05,  1 user,  load average: 0.15, 0.13, 0.05

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -318,7 +318,7 @@ word_t expr(char *e, bool *success) {
 //		Assert(0, "ERROR:The brackets don't match.\n");
 	}
 	result = eval(0, nr_token-1);
-//	printf("expr result = %u\n", result);
+	printf("expr result = %u\n", result);
 	return result;
 }
 
