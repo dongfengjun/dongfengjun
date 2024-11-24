352e841ba84901f63ba138b16e49bd2222f7bb07
tracer-ysyx <tracer@ysyx.org>
2024-11-24 21:46:35 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:46:35 up 10:57,  1 user,  load average: 0.08, 0.10, 0.15

--- nemu/src/cpu/iringbuf.c
+++ nemu/src/cpu/iringbuf.c
@@ -28,7 +28,7 @@ int ringq_push(RINGQ * ringqp, char *data) {
 	printf("data:%s\n", data);
 	int len = strlen(data);
 	printf("len=%d\n", len);
-	char s[2] = {0};
+	char s[200] = {0};
 	for(int i = 0; i < len; i ++) {
 		printf("index = %d\n", i);
 //		ringqp->str[ringqp->tail] = *data;
