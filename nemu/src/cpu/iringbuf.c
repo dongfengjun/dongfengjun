584135fd864642f0d7d012232cb3c4911af51c54
tracer-ysyx <tracer@ysyx.org>
2024-11-25 11:28:22 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:28:22 up  1:10,  1 user,  load average: 0.09, 0.05, 0.07

--- nemu/src/cpu/iringbuf.c
+++ nemu/src/cpu/iringbuf.c
@@ -3,9 +3,9 @@
 #include <string.h>
 #include "iringbuf.h" 
 #define RQ_SIZE 10
+char strarray[RQ_SIZE]={0};
 
 int ringq_init(RINGQ * ringqp) {
-	 char strarray[RQ_SIZE]={0};
 	 ringqp->str = strarray;
    ringqp->head = 0;
    ringqp->tail = 0;
