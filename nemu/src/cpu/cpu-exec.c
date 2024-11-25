05ab475388465ef2594222e9c824be261e45f00f
tracer-ysyx <tracer@ysyx.org>
2024-11-25 14:32:14 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:32:14 up  3:45,  1 user,  load average: 0.06, 0.03, 0.01

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -67,7 +67,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
 	ringq_push(&rq, buf);
 #ifdef CONFIG_ITRACE
   char *p = s->logbuf;
-	printf("bflog:%s\n", p);
+	printf("bflog:%s\n", s->logbuf);
   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
   int ilen = s->snpc - s->pc;
   int i;
