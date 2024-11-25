987ae41ae2701eb966f2dbfffaee16a0897e25c7
tracer-ysyx <tracer@ysyx.org>
2024-11-25 16:46:12 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:46:12 up  5:51,  1 user,  load average: 0.66, 0.27, 0.15

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -24,6 +24,14 @@ static uint8_t *pmem = NULL;
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 #endif
 
+/***MTRACE***/
+#ifdef CONFIG_MTRACE
+char buf[1024] = {0};
+char *p = buf;
+//IFDEF(CONFIG_MTRACE_COND, puts("%s\n", buf));
+#endif
+
+
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
@@ -51,13 +59,16 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
-  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
+	word_t result;
+  if (likely(in_pmem(addr))) result = pmem_read(addr, len);
+  IFDEF(CONFIG_DEVICE, result = mmio_read(addr, len));
   out_of_bound(addr);
+	p += sprintf(p, "addr:%u read:%u\n", addr, result);//wtrace
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+	p += sprintf(p, "addr:%u write:%u\n", addr, data);//wtrace
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
