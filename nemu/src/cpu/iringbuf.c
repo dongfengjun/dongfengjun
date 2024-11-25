74c99aed3e7eee0cdbb9172cd2c91fa469d2795a
tracer-ysyx <tracer@ysyx.org>
2024-11-25 13:21:17 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:21:17 up  2:34,  1 user,  load average: 0.00, 0.05, 0.07

--- nemu/src/cpu/iringbuf.c
+++ nemu/src/cpu/iringbuf.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "iringbuf.h" 
-#define RQ_SIZE 1000
+#define RQ_SIZE 240
 #define ringq_is_empty(q) (q->head == q->tail)
 #define ringq_is_full(q) (((q->tail+1)%RQ_SIZE) == q->head )
 
