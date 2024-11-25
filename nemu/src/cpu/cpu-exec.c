fe60ae302a0b22b50a3a0dff32ec8ffbbe796e75
tracer-ysyx <tracer@ysyx.org>
2024-11-25 14:02:19 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:02:19 up  3:15,  1 user,  load average: 0.06, 0.05, 0.08

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -102,7 +102,7 @@ static void execute(uint64_t n) {
     if (nemu_state.state != NEMU_RUNNING) break;
     IFDEF(CONFIG_DEVICE, device_update());
   }
-//	ringq_display(&rq);
+	ringq_display(&rq);
 }
 
 static void statistic() {
