a9feb571d543113f6209abd0aa820430605daf5f
tracer-ysyx <tracer@ysyx.org>
2024-11-25 14:14:15 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:04:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:14:15 up  3:27,  1 user,  load average: 0.00, 0.00, 0.03

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -76,6 +76,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   }
   int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
   int space_len = ilen_max - ilen;
+	printf("space len = %d\n", space_len);
   if (space_len < 0) space_len = 0;
   space_len = space_len * 3 + 1;
   memset(p, ' ', space_len);
