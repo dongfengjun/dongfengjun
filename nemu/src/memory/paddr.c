d4d3abe2f2bf8555db006a790745a94a03ac1bc2
tracer-ysyx <tracer@ysyx.org>
2024-11-26 20:27:37 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-49-generic #49-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov  4 02:06:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:27:37 up 35 min,  1 user,  load average: 0.04, 0.04, 0.12

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -64,6 +64,7 @@ word_t paddr_read(paddr_t addr, int len) {
 	word_t result;
   if(likely(in_pmem(addr))) {
 		result = pmem_read(addr, len);
+		wtracelog = fopen("build/nemu-wtrace-log.txt", "w");
 		p += sprintf(p, "addr:%u write:%u\n", addr, result);//wtrace
 		fprintf(wtracelog, "%s", buf);
     fclose(wtracelog);
@@ -76,7 +77,7 @@ word_t paddr_read(paddr_t addr, int len) {
 
 void paddr_write(paddr_t addr, int len, word_t data) {
 	p += sprintf(p, "addr:%u write:%u\n", addr, data);//wtrace
-	wtracelog = fopen("build/nemu-wtrace-log.txt", "w+");
+	wtracelog = fopen("build/nemu-wtrace-log.txt", "w");
   fprintf(wtracelog, "%s", buf);
   fclose(wtracelog);  
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
