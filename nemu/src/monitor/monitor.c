8b5c547394671b763e98be63ec69ae2a294f01a0
tracer-ysyx <tracer@ysyx.org>
2024-11-27 09:54:30 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-49-generic #49-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov  4 02:06:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:54:30 up 6 min,  1 user,  load average: 0.64, 0.44, 0.23

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -131,7 +131,7 @@ void init_monitor(int argc, char *argv[]) {
 
 	/* Initialize the IRingBuffer. */
 	iringbuf_init(&rq);
-//	iringbuf_display(&rq);
+	iringbuf_display(&rq);
 
 #ifndef CONFIG_ISA_loongarch32r
   IFDEF(CONFIG_ITRACE, init_disasm(
