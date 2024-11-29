446a78fa19b736c993c6a8b53130fafb15e8d892
tracer-ysyx <tracer@ysyx.org>
2024-11-29 11:00:06 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-49-generic #49-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov  4 02:06:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:00:06 up  1:06,  1 user,  load average: 0.08, 0.03, 0.11

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -138,6 +138,24 @@ static int decode_exec(Decode *s) {
 
   R(0) = 0; // reset $zero to 0
 
+	//fbreak jalr: 0b1100111 ; jal: 0b1101111
+	uint32_t opcode = BITS(s->isa.inst.val, 6, 0);
+  if (opcode == 0b1100111 || opcode == 0b1101111) {
+    ftracebuf[ftracehead].pc = s->pc;
+    ftracebuf[ftracehead].npc = s->dnpc;
+    // jalr x0, 0(x1): 0x00008067, a.k.a. ret
+    if (s->isa.inst.val == 0x00008067) {
+      ftracebuf[ftracehead].ret = true;
+      ftracedepth--;
+      ftracebuf[ftracehead].depth = ftracedepth;
+    } else {
+      ftracebuf[ftracehead].ret = false;
+      ftracebuf[ftracehead].depth = ftracedepth;
+      ftracedepth++;
+    }
+    ftracehead = (ftracehead + 1) % MAX_FTRACE_SIZE;
+  }
+
   return 0;
 }
 
