e4a9167f9618318ae2e7220d5bcc00e47d855111
tracer-ysyx <tracer@ysyx.org>
2024-11-29 10:47:15 +0800

    > compile NEMU ysyx_22040000 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-49-generic #49-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov  4 02:06:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:47:15 up 53 min,  1 user,  load average: 0.21, 0.33, 0.29

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -201,7 +201,7 @@ void isa_parser_elf(char *filename) {
 
 void cpu_show_ftrace() {
 	printf("ftrace\n");
-	printf("ftracehead=%d\n", ftracehead);
+	printf("ftracehead=%d\n", ftracedepth);
   Elf_Sym *sym = NULL;
   Ftrace *ftrace = NULL;
   for (size_t i = 0; i < ftracehead; i++) {
