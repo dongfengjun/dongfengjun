ad6f330224eed0adf1269d96b9b5025639daa5e6
tracer-ysyx <tracer@ysyx.org>
2024-12-15 22:58:19 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:58:19 up 17 min,  1 user,  load average: 0.00, 0.06, 0.07

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -80,7 +80,8 @@ static void itrace(){
 
 #ifndef CONFIG_ISA_loongarch32r
 	disassemble(p, logbuf + sizeof(logbuf) - p, pc, (uint8_t *)code, 4);
-	p[0] = '\0'; // the upstream llvm does not support loongarch32r
+#else
+p[0] = '\0'; // the upstream llvm does not support loongarch32r
 #endif
 
 }
