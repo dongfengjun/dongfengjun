d01427760ccceb666ea48f0747f3a6816a01cada
tracer-ysyx <tracer@ysyx.org>
2024-12-15 22:59:02 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:59:02 up 17 min,  1 user,  load average: 0.35, 0.13, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -72,6 +72,8 @@ static void itrace(){
   for (i = ilen - 1; i >= 0; i --) {
     p += snprintf(p, 4, " %02x", insts[i]);
   }
+	memset(p, ' ', 1);
+	p += 1;
 
 	uint32_t pc = 0x80000004;
 	uint8_t codes[4] = {0x17, 0x91, 0x0, 0x0};
