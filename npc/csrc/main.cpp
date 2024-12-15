d6513e4730137e5677f28ddd5a1a6dc3f5d3c78c
tracer-ysyx <tracer@ysyx.org>
2024-12-15 19:57:36 +0800

    > compile NEMU ysyx_24110017 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:57:36 up  1:55,  1 user,  load average: 0.05, 0.05, 0.05

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -77,12 +77,12 @@ static void itrace(){
 	uint8_t codes[4] = {0x17, 0x91, 0x0, 0x0};
 	uint8_t *code = codes;
 	uint32_t inst = 0x13040000;
-/***
+
 #ifndef CONFIG_ISA_loongarch32r
 	disassemble(p, logbuf + sizeof(logbuf) - p, pc, (uint8_t *)code, 4);
 	p[0] = '\0'; // the upstream llvm does not support loongarch32r
 #endif
-***/
+
 }
 #endif
 
