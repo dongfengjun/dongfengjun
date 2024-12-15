200959eb679668f2de0c8c98fdea2219bb09fd35
tracer-ysyx <tracer@ysyx.org>
2024-12-15 22:54:33 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:54:33 up 13 min,  1 user,  load average: 0.03, 0.10, 0.09

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -61,10 +61,10 @@ IFDEF(CONFIG_ITRACE, char logbuf[128]);
 static void itrace(){
 	uint8_t insts[4];
 	uint8_t *insts_ptr = insts;
-	insts[0] = top->inst & 0xFF;
-  insts[1] = (top->inst >>  8) & 0xFF;
-  insts[2] = (top->inst >> 16) & 0xFF;
-	insts[3] = (top->inst >> 24) & 0xFF;
+	insts[0] = (top->inst >> 24) & 0xFF;
+  insts[1] = (top->inst >> 16) & 0xFF;
+  insts[2] = (top->inst >>  8) & 0xFF;
+	insts[3] =  top->inst        & 0xFF;
   char *p = logbuf;
   p += snprintf(p, sizeof(logbuf), FMT_WORD ":", top->pc);
   int ilen = 4;
@@ -75,13 +75,8 @@ static void itrace(){
 	memset(p, ' ', 1);
 	p += 1;
 
-	uint32_t pc = 0x80000004;
-	uint8_t codes[4] = {0x0, 0x0, 0x91, 0x17};
-	uint8_t *code = codes;
-	uint32_t inst = 0x13040000;
-
 #ifndef CONFIG_ISA_loongarch32r
-	disassemble(p, logbuf + sizeof(logbuf) - p, pc, (uint8_t *)code, 4);
+	disassemble(p, logbuf + sizeof(logbuf) - p, top->pc, (uint8_t *)insts_ptr, 4);
 #else
 	p[0] = '\0'; // the upstream llvm does not support loongarch32r
 #endif
