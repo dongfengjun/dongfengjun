57d4a9959c593f1cd804d7d0490dd6883cb05b08
tracer-ysyx <tracer@ysyx.org>
2024-12-15 11:38:54 +0800

    > compile NEMU ysyx_24110017 董峰君 Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:38:54 up  1:02,  1 user,  load average: 0.83, 0.54, 0.29

--- npc/csrc/include/disasm.cc
+++ npc/csrc/include/disasm.cc
@@ -98,8 +98,8 @@ extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int
   uint64_t dummy_size = 0;
 	printf("arr:%x %x %x %x\n", arr[0], arr[1], arr[2], arr[3]);
 	printf("size:%d\n", size);
-	printf("pc:%u\n", pc);
-	printf("code:x\n", code);
+	printf("pc:%lu\n", pc);
+	printf("code:%x\n", *code);
   gDisassembler->getInstruction(inst, dummy_size, arr, pc, llvm::nulls());
 	
   std::string s;
