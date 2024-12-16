73aad360004c38391122f916ed8ce34e4713beab
tracer-ysyx <tracer@ysyx.org>
2024-12-16 17:59:30 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:59:30 up  7:04,  1 user,  load average: 0.05, 0.05, 0.07

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -89,7 +89,7 @@ static void itrace(){
 
 static void trace_and_difftest() {
 #ifdef CONFIG_ITRACE_COND
-  if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
+  if (ITRACE_COND) { log_write("%s\n", logbuf); }
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(logbuf)); }
 		IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
