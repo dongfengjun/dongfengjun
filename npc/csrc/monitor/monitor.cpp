4829e176405cee3612f9af733b2cba5882ccad87
tracer-ysyx <tracer@ysyx.org>
2024-12-16 10:38:53 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:38:53 up 35 min,  1 user,  load average: 0.00, 0.04, 0.14

--- npc/csrc/monitor/monitor.cpp
+++ npc/csrc/monitor/monitor.cpp
@@ -1,10 +1,6 @@
 #include "../include/common.h"
 #include <getopt.h>
 
-#ifdef CONFIG_ITRACE
-//extern "C" void init_disasm(const char *triple);
-#endif
-
 static void welcome() {
   Log("Trace: %s", MUXDEF(CONFIG_TRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));
   IFDEF(CONFIG_TRACE, Log("If trace is enabled, a log file will be generated "
