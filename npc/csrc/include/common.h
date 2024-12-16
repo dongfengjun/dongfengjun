a762ec7718e5568fc0bfd3da903b17b7da3debd9
tracer-ysyx <tracer@ysyx.org>
2024-12-16 10:39:50 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:39:50 up 36 min,  1 user,  load average: 0.32, 0.11, 0.15

--- npc/csrc/include/common.h
+++ npc/csrc/include/common.h
@@ -50,9 +50,7 @@ void init_sdb();
 void sdb_mainloop();
 void sdb_set_batch_mode();
 
-#ifdef CONFIG_TRACE
 extern "C" void init_disasm(const char *triple);
 extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
-#endif
 
 #endif
