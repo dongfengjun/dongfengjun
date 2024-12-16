42bf39f41e1a102f275edbe7b13f3496a57ac290
tracer-ysyx <tracer@ysyx.org>
2024-12-16 15:39:43 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:39:43 up  4:44,  1 user,  load average: 0.22, 0.14, 0.14

--- npc/csrc/include/common.h
+++ npc/csrc/include/common.h
@@ -8,7 +8,7 @@
 #include "macro.h"//宏定义函数
 #include "debug.h"//Assert\Log
 #include "utils.h"//Log
-#include "iringbuf.h"
+//#include "iringbuf.h"
 
 //Macro definition
 typedef MUXDEF(CONFIG_ISA64, uint64_t, uint32_t) word_t;
