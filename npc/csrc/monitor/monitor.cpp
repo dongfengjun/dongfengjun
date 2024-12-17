a30763784abfa58f408468d6b129dfcefad1522d
tracer-ysyx <tracer@ysyx.org>
2024-12-17 22:09:21 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:09:21 up  7:58,  1 user,  load average: 0.89, 0.38, 0.18

--- npc/csrc/monitor/monitor.cpp
+++ npc/csrc/monitor/monitor.cpp
@@ -15,6 +15,7 @@ static char *log_file = NULL;
 static char *img_file = NULL;
 static char *diff_so_file = NULL;
 static int difftest_port = 1234;
+extern uint8_t mem;
 
 static long load_img() {
   if(img_file == NULL) {
