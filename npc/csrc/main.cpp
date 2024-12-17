aeb6b8ff6a35a1867830a349fae2fa6fff341fc4
tracer-ysyx <tracer@ysyx.org>
2024-12-17 14:56:09 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:56:09 up 45 min,  1 user,  load average: 0.04, 0.26, 0.22

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -109,6 +109,7 @@ Elf_Shdr *elfshdr_symtab = NULL;//符号表
 Elf_Shdr *elfshdr_strtab = NULL;//字符串表
 
 void ftrace_push() {
+	printf("check function\n");
 	uint32_t opcode = top->inst;
 	if(opcode == 0b1100111 || opcode == 0b1101111) {
 		if(ftracebuf[ftracehead].pc != 0) {
