c39b3f98181637518b372c04a93ab2cafdf9c80e
tracer-ysyx <tracer@ysyx.org>
2024-12-17 11:13:06 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:13:06 up  2:23,  1 user,  load average: 0.14, 0.07, 0.09

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -137,8 +137,9 @@ void isa_parser_elf(char *filename) {
 	fseek(fp, 0, SEEK_END);
 	long size = ftell(fp);
 	Assert(size < MAX_ELF_SIZE, "elf file is too large");
-	fseek(fp, 0, SEEK_SET);
+	fseek(fp, -1, SEEK_SET);
 	int ret = fread(&elf_ehdr, sizeof(elf_ehdr), 1, fp);
+
 	assert(ret == 1);
 	//assert(memcmp(elf_ehdr.e_ident, ELFMAG, SELFMAG) == 0);//魔数字节
 	fseek(fp, 0, SEEK_SET);
