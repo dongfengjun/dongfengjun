64346d03df5dd44dc6ec83bd4d66f3f1138691d0
tracer-ysyx <tracer@ysyx.org>
2024-12-17 22:58:24 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:58:24 up  8:47,  1 user,  load average: 0.10, 0.23, 0.21

--- npc/csrc/include/common.h
+++ npc/csrc/include/common.h
@@ -31,6 +31,7 @@ typedef uint16_t ioaddr_t;
 	#define CONFIG_FTRACE 1
 	#define CONFIG_WATCHPOINT 1
 #endif
+#define CONFIG_DIFFTEST 1
 
 #define CONFIG_MBASE 0x80000000//IM
 #define CONFIG_MSIZE 0x8000000
