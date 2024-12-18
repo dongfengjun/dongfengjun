a8f203a6b5b0ea339e7d82ba911e3b7ddbc624cb
tracer-ysyx <tracer@ysyx.org>
2024-12-18 14:24:14 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:24:14 up  1:06,  1 user,  load average: 0.12, 0.25, 0.22

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -253,7 +253,7 @@ int main(int argc, char *argv[]) {
 /***code***/
 	init_monitor(argc, argv);//load inst
 //测试inst  std::cout<<std::hex<<pmem_read(0x80000000)<<"\n";	
-	reset(2);
+	reset(3);
 #ifdef CONFIG_TARGET_AM
   cpu_exec(-1);
 #else
