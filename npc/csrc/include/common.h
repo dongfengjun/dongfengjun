ede244a7128c6e38e9c055738dc86949938875f7
tracer-ysyx <tracer@ysyx.org>
2024-12-18 17:24:12 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:24:12 up  4:06,  1 user,  load average: 0.04, 0.03, 0.08

--- npc/csrc/include/common.h
+++ npc/csrc/include/common.h
@@ -36,8 +36,6 @@ typedef uint16_t ioaddr_t;
 #define CONFIG_MBASE 0x80000000//IM
 #define CONFIG_MSIZE 0x8000000
 #define CONFIG_MEM_RANDOM 1
-#define PMEM_LEFT ((uint32_t)CONFIG_MBASE)
-#define PMEM_RIGHT ((uint32_t)CONFIG_MBASE + CONFIG_MSIZE - 1)
 #define CONFIG_TRACE_START 0
 #define CONFIG_TRACE_END 10000
 
--- npc/csrc/memory/instmemory.cpp
+++ npc/csrc/memory/instmemory.cpp
@@ -9,6 +9,14 @@
 	uint8_t mem[CONFIG_MSIZE] PG_ALIGN = {};
 #endif
 
+#define PMEM_LEFT  ((paddr_t)CONFIG_MBASE)
+#define PMEM_RIGHT ((paddr_t)CONFIG_MBASE + CONFIG_MSIZE - 1)
+#define RESET_VECTOR (PMEM_LEFT + CONFIG_PC_RESET_OFFSET)
+
+static inline bool in_pmem(paddr_t addr) {
+  return addr - CONFIG_MBASE < CONFIG_MSIZE;
+}
+
 uint8_t* guest_to_host(paddr_t paddr) { return mem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - mem + CONFIG_MBASE; }
 
