4681c94ac09a1694a3c38c35177a5164c9cef681
tracer-ysyx <tracer@ysyx.org>
2024-12-18 17:12:53 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:12:53 up  3:55,  1 user,  load average: 0.04, 0.17, 0.18

--- npc/csrc/memory/instmemory.cpp
+++ npc/csrc/memory/instmemory.cpp
@@ -12,6 +12,19 @@
 uint8_t* guest_to_host(paddr_t paddr) { return mem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - mem + CONFIG_MBASE; }
 
+static word_t pmem_read(paddr_t addr, int len) {
+	word_t ret = host_read(guest(addr), len);
+	return ret;
+}
+
+static void pmem_write(paddr_t addr, int len, word_t data) {
+	host_write(guest_to_host(addr), len, data);
+}
+
+static void out_of_bound(paddr_t addr) {
+	panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_WORD, addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
+}
+
 void init_mem() {
 #if   defined(CONFIG_PMEM_MALLOC)
   mem = malloc(CONFIG_MSIZE);
