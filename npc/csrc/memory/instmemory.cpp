2fb08b16d3a49b6f04c4cf51afcde40fd44988c5
tracer-ysyx <tracer@ysyx.org>
2024-12-18 17:16:56 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:16:56 up  3:59,  1 user,  load average: 0.03, 0.10, 0.15

--- npc/csrc/memory/instmemory.cpp
+++ npc/csrc/memory/instmemory.cpp
@@ -13,7 +13,7 @@ uint8_t* guest_to_host(paddr_t paddr) { return mem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - mem + CONFIG_MBASE; }
 
 static word_t pmem_read(paddr_t addr, int len) {
-	word_t ret = host_read(guest(addr), len);
+	word_t ret = host_read(guest_to_host(addr), len);
 	return ret;
 }
 
