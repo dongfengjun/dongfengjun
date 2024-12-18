94dabf8e8c4d6873b11f107f0aa76c9cb51f9413
tracer-ysyx <tracer@ysyx.org>
2024-12-18 10:43:14 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:43:14 up 13 min,  1 user,  load average: 0.04, 0.09, 0.09

--- npc/csrc/difftest/dut.c
+++ npc/csrc/difftest/dut.c
@@ -5,7 +5,7 @@ extern uint8_t mem[CONFIG_MSIZE];
 extern CPU_state cpu;
 enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
 
-void *ref_difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
+void *(*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;//中断
