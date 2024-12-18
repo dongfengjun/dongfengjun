579b6109520ba970ec5183111383008a756112e0
tracer-ysyx <tracer@ysyx.org>
2024-12-18 20:30:05 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:30:05 up  7:12,  1 user,  load average: 0.08, 0.03, 0.00

--- npc/vsrc/PCU_ysyx_24110017.v
+++ npc/vsrc/PCU_ysyx_24110017.v
@@ -13,13 +13,8 @@ wire [31:0]dnpc;
 always@(posedge clk)begin
 	if(rst)
 		pc <= 32'h80000000;
-	else begin
-		if(jalen)
-			pc <= pc + offset;//jal
-		else if(jalren)
-			pc <= ((r1 + offset) & ~1);//jalr
-		else
-			pc <= pc + 4;
+	else
+		pc <= dnpc;
 	end
 end
 assign dnpc = (jalen) ? (pc + offset) : (jalren) ? ((r1 + offset) & ~1) : pc + 4;
