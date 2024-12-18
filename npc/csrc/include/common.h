a231d602195ccb6a5389102a8ad887b5d82fd848
tracer-ysyx <tracer@ysyx.org>
2024-12-18 17:56:59 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:56:59 up  4:39,  1 user,  load average: 0.04, 0.01, 0.06

--- npc/csrc/include/common.h
+++ npc/csrc/include/common.h
@@ -53,7 +53,7 @@ uint8_t* guest_to_host(paddr_t paddr);
 paddr_t host_to_guest(uint8_t *haddr);
 void init_mem();
 word_t paddr_read(paddr_t addr, int len);
-word_t paddr_write(paddr_t addr, int len, word_t data);
+void paddr_write(paddr_t addr, int len, word_t data);
 //reg DPI-C
 word_t gpr_regs_display(int raddr);//抓取reg  DPI-C in RF
 void isa_regs_display();
