ae95bfab40da73869523d6385fc25ec8d74d5595
tracer-ysyx <tracer@ysyx.org>
2024-12-20 17:04:14 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:04:14 up  2:54,  1 user,  load average: 0.16, 0.20, 0.22

--- npc/csrc/dpic.cpp
+++ npc/csrc/dpic.cpp
@@ -8,7 +8,7 @@
 #include "Vtop_ysyx_24110017__Dpi.h"
 #include "./include/common.h"
 
-uint32_t pmem_read(uint32_t raddr) {
+int pmem_read(uint32_t raddr) {
   // 总是读取地址为`raddr & ~0x3u`的4字节返回
 	return paddr_read(raddr, 4);
 }
