8b9b77772f5188d156cf01737df30f3da626af84
tracer-ysyx <tracer@ysyx.org>
2024-12-20 11:26:56 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:26:56 up  1:07,  1 user,  load average: 0.08, 0.02, 0.01

--- npc/csrc/memory/instmemory.cpp
+++ npc/csrc/memory/instmemory.cpp
@@ -36,12 +36,12 @@ static inline void host_write(void *addr, int len, word_t data) {
 uint8_t* guest_to_host(paddr_t paddr) { return mem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - mem + CONFIG_MBASE; }
 
-static word_t pmem_read(paddr_t addr, int len) {
+static word_t mem_read(paddr_t addr, int len) {
 	word_t ret = host_read(guest_to_host(addr), len);
 	return ret;
 }
 
-static void pmem_write(paddr_t addr, int len, word_t data) {
+static void mem_write(paddr_t addr, int len, word_t data) {
 	host_write(guest_to_host(addr), len, data);
 }
 
@@ -60,14 +60,14 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
+  if (likely(in_pmem(addr))) return mem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
+  if (likely(in_pmem(addr))) { mem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
