8b2d3681660b9785a78a5831867e8fd91182c654
tracer-ysyx <tracer@ysyx.org>
2024-12-23 12:20:34 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:20:34 up  1:07,  1 user,  load average: 0.02, 0.02, 0.04

--- npc/obj_dir/Vtop_ysyx_24110017__Dpi.h
+++ npc/obj_dir/Vtop_ysyx_24110017__Dpi.h
@@ -20,11 +20,11 @@ extern "C" {
     extern int gpr_reg_display(int addr);
 
     // DPI IMPORTS
-    // DPI import at vsrc/IFU_ysyx_24110017.v:7:30
+    // DPI import at vsrc/IFU_ysyx_24110017.v:8:30
     extern void ifu(int raddr, int* rdata);
     // DPI import at vsrc/IDU_ysyx_24110017.v:25:30
     extern void npc_trap();
-    // DPI import at vsrc/IFU_ysyx_24110017.v:6:29
+    // DPI import at vsrc/IFU_ysyx_24110017.v:7:29
     extern int pmem_read(int raddr);
     // DPI import at vsrc/EXU_ysyx_24110017.v:86:30
     extern void pmem_write(int waddr, int wdata, char wmask);
