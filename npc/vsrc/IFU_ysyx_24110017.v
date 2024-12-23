3771b29b254f73ce36a0fc53dfde3027b9a9463e
tracer-ysyx <tracer@ysyx.org>
2024-12-23 12:19:32 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:19:32 up  1:06,  1 user,  load average: 0.08, 0.02, 0.05

--- npc/vsrc/IFU_ysyx_24110017.v
+++ npc/vsrc/IFU_ysyx_24110017.v
@@ -2,6 +2,7 @@ module IFU_ysyx_24110017(pc,inst);
 input [31:0]pc;
 output [31:0]inst;
 wire [31:0]pc;
+wire [31:0]inst;
 
 import "DPI-C" function int pmem_read(input int raddr);
 import "DPI-C" function void ifu(input int raddr, output int rdata);
