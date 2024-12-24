148088beb96889f92d396717fe7795491c45b7a0
tracer-ysyx <tracer@ysyx.org>
2024-12-24 17:15:17 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:15:17 up  4:28,  1 user,  load average: 0.18, 0.15, 0.13

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -15,8 +15,9 @@ LDFLAGS   += --gc-sections -e _start
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
 
-NPC_ARGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
-NPC_ARGS += -e $(IMAGE).elf
+LOG=$(shell dirname $(IMAGE).elf)/nemu-log.txt
+IMG=$(IMAGE).bin
+ELF=$(IMAGE).elf
 
 image: $(IMAGE).elf
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
@@ -24,7 +25,7 @@ image: $(IMAGE).elf
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: image
-	$(MAKE) -C $(NPC_HOME)	run ARGS="$(NPC_ARGS)"	IMG=$(IMAGE).bin
+	$(MAKE) -C $(NPC_HOME)	LOG	IMG	ELF
 
 gdb: image
 	$(MAKE) -C $(NPC_HOME)	run IMG=$(IMAGE).bin
