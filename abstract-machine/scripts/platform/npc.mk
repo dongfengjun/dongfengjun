114240b1a0f5b96fdb6453d266e654549f7f3972
tracer-ysyx <tracer@ysyx.org>
2024-12-24 16:46:12 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:46:12 up  3:59,  1 user,  load average: 0.00, 0.00, 0.06

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -15,11 +15,8 @@ LDFLAGS   += --gc-sections -e _start
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
 
-REF_SO ?= ../nemu/build/riscv32-nemu-interpreter-so
-#NPC_ARGS += --img=$(IMAGE).bin
-NPC_ARGS += --elf=$(IMAGE).elf
-NPC_ARGS += --diff=$(REF_SO)
-
+ELF += $(IMAGE).elf
+NPC_ARGS += --elf=$(ELF)
 
 image: $(IMAGE).elf
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
