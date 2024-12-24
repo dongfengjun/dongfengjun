bf543d3c1bc8cefe9911631637114c257aede725
tracer-ysyx <tracer@ysyx.org>
2024-12-24 19:06:43 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:06:43 up  6:20,  1 user,  load average: 0.20, 0.29, 0.20

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -21,9 +21,16 @@ word_t vaddr_ifetch(vaddr_t addr, int len) {
 }
 
 word_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+	word_t result = paddr_read(addr, len);
+#ifdef CONFIG_MTRACE
+	mtrace_p += sprintf(mtrace_p, "addr:%u read:%u\n", addr, result);
+#endif
+	return result;
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
+#ifdef CONFIG_MTRACE
+	mtrace_p += sprintf(mtrace_p, "addr:%u write:%u\n", addr, data);
+#endif
   paddr_write(addr, len, data);
 }
