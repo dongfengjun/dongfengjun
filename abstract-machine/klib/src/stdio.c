52f0a1f34988c721e919f9cabb0a833744e4613b
tracer-ysyx <tracer@ysyx.org>
2024-12-30 21:34:30 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:34:30 up  5:51,  1 user,  load average: 0.84, 0.36, 0.20

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,50 +5,93 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+void int2str(char *str,int value) {                                         
+  char tmp_str[20] = {0};
+  int lidx = 0;
+  char flag = 0;
+  int tmp_val;
+  if(value<0){
+    flag = 1;
+    tmp_val = -value;
+  }else{
+    tmp_val = value;
+  }
+  if(value==0){
+    tmp_str[lidx++] = 0x30+tmp_val%10;
+  }else{
+    while(1){
+      if(tmp_val==0){
+        break;
+      }else{
+        tmp_str[lidx++] = 0x30+tmp_val%10;
+        tmp_val = tmp_val/10;
+      }
+    }
+  }
+  if(flag){
+    tmp_str[lidx++] = '-';
+  }
+  while(lidx--){
+    *str++ = tmp_str[lidx];
+  }
+  *str = 0;
+}
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+	va_list args;
+	va_start(args, fmt);
+	while(*fmt != '\0') {
+		switch(*fmt){
+			case '%': {
+				++ fmt;
+				switch(*fmt) {
+		 			case 'd' : {
+						int val = va_arg(args, int);
+						char str[32]={0};
+						char *strp = str;
+						int2str(str, val);
+						while(*strp != '\0'){
+							putch(*strp);
+							++ strp;
+						}
+						break;
+					}
+					case 's' : {
+						char *tmp = va_arg(args, char *);
+						while(*tmp != '\0') {
+							putch(*tmp);
+							++ tmp;
+						}
+						break;
+		 			}
+					default: {
+						putch('W');
+						putch('A');
+						putch('I');
+						putch('T');
+						putch('\n');
+						break;
+		 			}
+		 		}
+		 	}
+		 	default: {
+				putch(*fmt);
+				break;
+			}
+		++ fmt;
+		 }
+	}
+	va_end(args);
+	return 0;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
-void int2str(char *str,int value)
-{
-	char tmp_str[20] = {0};
-	int lidx = 0;
-	char flag = 0;
-	int tmp_val;
-	if(value<0){
-		flag = 1;
-		tmp_val = -value;
-	}else{
-		tmp_val = value;
-	}
-	if(value==0){
-		tmp_str[lidx++] = 0x30+tmp_val%10;
-	}else{
-		while(1){
-			if(tmp_val==0){
-				break;
-			}else{
-				tmp_str[lidx++] = 0x30+tmp_val%10;
-				tmp_val = tmp_val/10;
-			}
-		}
-	}
-	if(flag){
-		tmp_str[lidx++] = '-';
-	}
-	while(lidx--){
-		*str++ = tmp_str[lidx];
-	}
-	*str = 0;
-}
-
 int sprintf(char *out, const char *fmt, ...) {
 	memset(out, 0, strlen(out));
-	char str[20]={0};
+	char str[32]={0};
 	va_list args;
   va_start(args, fmt);
   while(*fmt != '\0') {
