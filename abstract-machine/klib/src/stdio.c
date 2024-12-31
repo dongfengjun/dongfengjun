f879a4a5a1023a3d83227931aee3e4447143c1a5
tracer-ysyx <tracer@ysyx.org>
2024-12-31 16:33:10 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:33:10 up  6:03,  1 user,  load average: 0.02, 0.04, 0.03

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,37 +6,6 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap);
-void int2str(char *str,int value) {                                         
-  char tmp_str[20] = {0};
-  int lidx = 0;
-  char flag = 0;
-  int tmp_val;
-  if(value<0){
-    flag = 1;
-    tmp_val = -value;
-  }else{
-    tmp_val = value;
-  }
-  if(value==0){
-    tmp_str[lidx++] = 0x30+tmp_val%10;
-  }else{
-    while(1){
-      if(tmp_val==0){
-        break;
-      }else{
-        tmp_str[lidx++] = 0x30+tmp_val%10;
-        tmp_val = tmp_val/10;
-      }
-    }
-  }
-  if(flag){
-    tmp_str[lidx++] = '-';
-  }
-  while(lidx--){
-    *str++ = tmp_str[lidx];
-  }
-  *str = 0;
-}
 
 int printf(const char *fmt, ...) {
 	char buf[1024] = {0};
@@ -46,7 +15,7 @@ int printf(const char *fmt, ...) {
 	char *str = buf;
 	while(*str != '\0'){
 		putch(*str);
-		++ str;
+		str ++;
 	}
 	va_end(ap);
 	return val;
@@ -69,21 +38,40 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
   panic("Not implemented");
 }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+int myvsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
 	char *start = out;
 	while(n -- && *fmt != '\0'){
 		switch(*fmt){
 			case '%': {
-				++ fmt;
+				fmt ++;
 				switch(*fmt) {
 					case 'd': {
 						int val = va_arg(ap, int);
-						int2str(out, val);
+						if(val < 0) {
+							*out ++ = '-';
+							val = -val;
+						}
+						int len = 0;
+						int number = val;
+						do {
+							number /= 10;
+							len ++;
+						} while(number);
+						out = out + len - 1;
+						int tmp_len = len;
+						while(tmp_len --) {
+							int tmp = val % 10;
+							*out-- = tmp + 48;
+							val /= 10;
+						}
+						out += (len + 1);
 						break;
 					}
 					case 's': {
 						char *tmp = va_arg(ap, char *);
-						strcat(out, tmp);
+						while(*tmp != '\0') {
+							*out++ = *tmp++;
+						}
 						break;
 					}
 					case 'c': {
@@ -92,16 +80,13 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
 						break;
 					}
 					default: {
-						putch('W');
-						putch('a');
-						putch('i');
-						putch('t');
 						return -1;
 					}
 				}
+				break;
 			}
 			default: {
-				*out++ = *fmt;
+				*out ++ = *fmt;
 				break;
 			}
 		}
