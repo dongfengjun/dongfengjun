1f12734fc012da9bd417c2b1520573f81eee6d91
tracer-ysyx <tracer@ysyx.org>
2025-01-01 21:12:39 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:12:39 up  6:56,  1 user,  load average: 0.18, 0.08, 0.11

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,8 +1,6 @@
 #include <am.h>
 #include <nemu.h>
 
-#include <stdio.h>
-
 void __am_timer_init() {
 }
 
@@ -10,7 +8,6 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uint32_t low = inl(RTC_ADDR);
 	uint32_t high = inl(RTC_ADDR + 4);
 	uptime->us = low + ((uint64_t)high << 32);
-	printf("us=%d\n", uptime->us);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
