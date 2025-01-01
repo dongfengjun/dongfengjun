6be553e6cbbf1ac4d7529e47d675d3744abb3c5c
tracer-ysyx <tracer@ysyx.org>
2025-01-01 21:42:28 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:42:28 up  7:26,  1 user,  load average: 0.05, 0.09, 0.12

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -107,21 +107,21 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
 					default: {
 						switch(*fmt) {
 							case 'd': {
-								int val = va_arg(ap, int);
+								long int val = va_arg(ap, long int);
 								if(val < 0) {
 								//	*out ++ = '-';
 									val = -val;
 								}
-								int len = 0;
-								int number = val;
+								long int len = 0;
+								long int number = val;
 								do {
 									number /= 10;
 									len ++;
 								} while(number);
 								out = out + len - 1;
-								int tmp_len = len;
+								long int tmp_len = len;
 								while(tmp_len --) {
-									int tmp = val % 10;
+									long int tmp = val % 10;
 									*out-- = tmp + 48;
 									val /= 10;
 								}
