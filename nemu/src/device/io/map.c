7c69373202eb7e2f96bec1584f6f20bf26f6f91a
tracer-ysyx <tracer@ysyx.org>
2025-01-05 21:53:32 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:53:32 up  7:20,  1 user,  load average: 0.15, 0.48, 0.34

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -60,7 +60,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   invoke_callback(map->callback, offset, len, false); //prepare data to read
   word_t ret = host_read(map->space + offset, len);
 #ifdef CONFIG_DTRACE
-	dtrace_p += sprintf(dtrace_p, "device map:%s addr:%u len:%d read:%u\n", map->name, addr, len, ret);
+	dtrace_p += sprintf(dtrace_p, "device map:%s addr:%d len:%d read:%d\n", map->name, addr, len, ret);
 #endif
   return ret;
 }
@@ -72,6 +72,6 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
 #ifdef CONFIG_DTRACE
-	dtrace_p += sprintf(dtrace_p, "device map:%s addr:%u len:%d read:%u\n", map->name, addr, len, data);
+	dtrace_p += sprintf(dtrace_p, "device map:%s addr:%d len:%d read:%d\n", map->name, addr, len, data);
 #endif
 }
