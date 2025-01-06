399fa28636fb19b289adc43c832c9919e0a1fb76
tracer-ysyx <tracer@ysyx.org>
2025-01-06 14:53:53 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:53:53 up  1:41,  1 user,  load average: 0.00, 0.05, 0.13

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -100,18 +100,24 @@ FILE *dtrace_log;
 void cpu_show_ftrace();
 static void execute(uint64_t n) {
   Decode s;
-	char file_path[64] = {0};
-	if (getcwd(file_path, 64) == NULL) {
+#if (CONFIG_MTRACE || CONFIG_DTRACE)
+	char file_path[32] = {0};
+	 if (getcwd(file_path, 32) == NULL) {
         perror("getcwd failed");
-  }
-	#ifdef CONFIG_MTRACE
-		snprintf(file_path, 64, "%s", "build/name-mtrace-log.txt");
-		//mtracelog = fopen("build/nemu-mtrace-log.txt", "w");	//Mtrace
-		mtracelog = fopen(file_path, "w");
-	#endif
-	#ifdef CONFIG_DTRACE
-		dtrace_log = fopen("build/nemu-dtrace-log.txt", "w");	//Dtrace
-	#endif
+	 }
+#endif
+#ifdef CONFIG_MTRACE
+	char mtrace_path[64] = {0};
+	//mtracelog = fopen("build/nemu-mtrace-log.txt", "w");	//Mtrace
+	snprintf(mtrace_path, 64, "%s/%s", file_path,"build/nemu-mtrace-log.txt");
+	mtracelog = fopen(mtrace_path, "w");
+#endif
+#ifdef CONFIG_DTRACE
+	char dtrace_path[64] = {0};
+	//dtrace_log = fopen("build/nemu-dtrace-log.txt", "w");	//Dtrace
+	snprintf(dtrace_path, 64, "%s/%s", file_path,"build/nemu-dtrace-log.txt");
+	dtrace_log = fopen(dtrace_path, "w");
+#endif
   for (;n > 0; n --) {
     exec_once(&s, cpu.pc);
     g_nr_guest_inst ++;
