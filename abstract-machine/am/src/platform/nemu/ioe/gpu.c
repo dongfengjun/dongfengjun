70011ce7da7befb714932f502a138cf20f16d5b8
tracer-ysyx <tracer@ysyx.org>
2025-01-07 21:20:55 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:20:55 up  8:11,  1 user,  load average: 0.10, 0.14, 0.12

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -32,7 +32,8 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 	else {
 		uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
 		for(int k = 300 * 400 - 1; k >= 0; k --) {
-			fb[k] = color_buf[k];
+			fb[k] = 50;
+			//color_buf[k];
 		}
 		outl(SYNC_ADDR, 1);
 	}
