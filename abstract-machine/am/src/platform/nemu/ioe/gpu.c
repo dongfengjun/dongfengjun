9db5f6675cb3e22b1218c459b3dc2ed39441ac08
tracer-ysyx <tracer@ysyx.org>
2025-01-07 19:08:59 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:08:59 up  5:59,  1 user,  load average: 0.00, 0.04, 0.09

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -15,9 +15,9 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = inl(VGACTL_ADDR), 
-		.height = inl(VGACTL_ADDR),
-    .vmemsz = inl(VGACTL_ADDR) * inl(VGACTL_ADDR)
+    .width = inw(VGACTL_ADDR), 
+		.height = inw(VGACTL_ADDR + 1),
+    .vmemsz = inw(VGACTL_ADDR) * inw(VGACTL_ADDR + 1)
   };
 }
 
