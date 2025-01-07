0ddc7a7bef472be51c2b8fc72484d492c723ff0e
tracer-ysyx <tracer@ysyx.org>
2025-01-07 15:02:34 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:02:34 up  1:52,  1 user,  load average: 0.03, 0.15, 0.17

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,7 +5,8 @@
 
 int printf(const char *fmt, ...);
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = true;
-	kbd->keycode = inl(0xa0000060);
+  uint32_t k = inl(0xa0000060);
+	kbd->keydown = true;
+	kbd->keycode = k & ~KEYDOWN_MASK;
 	//printf("keycode:%d\n", kbd->keycode);
 }
