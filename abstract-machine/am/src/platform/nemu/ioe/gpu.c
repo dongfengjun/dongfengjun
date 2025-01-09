dea5e7b493b8dd9f31fe7d28d261e12fdeb294bd
tracer-ysyx <tracer@ysyx.org>
2025-01-09 16:00:42 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:00:42 up  2:04,  1 user,  load average: 0.73, 0.29, 0.20

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -28,7 +28,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 	if(!ctl->sync && (ctl->w == 0 || ctl->h == 0))
 		return;
 	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-	//uint32_t *pixels = ctl->pixels;
+	uint32_t *pixels = ctl->pixels;
 	//int x = ctl->x/ctl->w;
 	//int y = ctl->y/ctl->h;
 	//int w = ctl->w;
@@ -36,7 +36,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 	for(int i = ctl->y; i < (ctl->y + ctl->h); i ++) {
 		for(int j = ctl->x; j < (ctl->x + ctl->w); j ++) {
 			printf("i=%d,j=%d\n",i,j);
-			fb[inw(VGACTL_ADDR) * i + j] = 50;	//w*i+j 
+			fb[inw(VGACTL_ADDR) * i + j] = pixels[0];	//w*i+j 
 			//fb[inw(VGACTL_ADDR) * i + j] = pixels[0];
 			//pixels[(ctl->w) * (i - ctl->y) + (j - ctl->x)];
 		}
