b0228932e3ab2c9ca417172b84e4b1d0d3c2766b
tracer-ysyx <tracer@ysyx.org>
2025-01-09 16:08:22 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:08:22 up  2:12,  1 user,  load average: 0.23, 0.25, 0.22

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -36,7 +36,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 	for(int i = ctl->y; i < (ctl->y + ctl->h); i ++) {
 		for(int j = ctl->x; j < (ctl->x + ctl->w); j ++) {
 			//printf("i=%d,j=%d\n",i,j);
-			fb[inw(VGACTL_ADDR) * i + j] = pixels[0];	//w*i+j 
+			fb[inw(VGACTL_ADDR+2) * i + j] = pixels[0];	//w*i+j 
 			//fb[inw(VGACTL_ADDR) * i + j] = pixels[0];
 			//pixels[(ctl->w) * (i - ctl->y) + (j - ctl->x)];
 		}
