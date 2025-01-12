0be8eac0ea1b7e28324ef4e3fd96c2bfb0623354
tracer-ysyx <tracer@ysyx.org>
2025-01-12 20:21:43 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:21:43 up  9:47,  1 user,  load average: 0.13, 0.35, 0.26

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -65,6 +65,7 @@ void init_audio() {
 
 void audio_callback(void* udata, uint8_t *stream, int len) {
 	SDL_memset(stream, 0, len);
+	printf("len:%d\n", len);
 	SDL_MixAudio(stream, audio_pos, len, SDL_MIX_MAXVOLUME);
 	audio_pos += len;
 	printf("audio_pos = %d\n", *audio_pos);
