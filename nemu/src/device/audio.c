4b90bfca1cf823573b3df184792261702a3bfce6
tracer-ysyx <tracer@ysyx.org>
2025-01-12 17:00:58 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:00:58 up  6:26,  1 user,  load average: 0.00, 0.03, 0.06

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -50,9 +50,9 @@ void init_audio() {
   SDL_AudioSpec s = {};
   s.format = AUDIO_S16SYS;  // 假设系统中音频数据的格式总是使用16位有符号数>来表示
   s.userdata = NULL;	// 不使用
-  s.freq = paddr_read(0xa0000200, 4);
-  s.channels = paddr_read(0xa0000204, 4);
-  s.samples = paddr_read(0xa0000208, 4);
+  s.freq = 8000;//paddr_read(0xa0000200, 4);
+  s.channels = 1;//paddr_read(0xa0000204, 4);
+  s.samples = 1024;//paddr_read(0xa0000208, 4);
 	s.callback = audio_callback;
   SDL_InitSubSystem(SDL_INIT_AUDIO);
   if(SDL_OpenAudio(&s, NULL) < 0) {
