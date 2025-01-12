447bb3067ba7927421da4cb7336469d022c51510
tracer-ysyx <tracer@ysyx.org>
2025-01-12 15:59:33 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:59:33 up  5:25,  1 user,  load average: 0.15, 0.20, 0.18

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -69,6 +69,7 @@ void device_update() {
 	uint8_t buf[512] = {0};
 	uint8_t *p = buf;
 	audio_callback(udata, p, 512);
+	printf("aaaaa\n");
 	SDL_PauseAudio(0);
   SDL_CloseAudioDevice(0);
   SDL_CloseAudio();
