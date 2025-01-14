fac6102336624f0905193e689b6eaf8128b18f76
tracer-ysyx <tracer@ysyx.org>
2025-01-14 19:33:47 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:33:47 up 13 min,  1 user,  load average: 0.36, 0.38, 0.29

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -142,7 +142,7 @@ static void execute(uint64_t n) {
 		fclose(dtrace_log);
 	#endif
 	#ifdef CONFIG_HAS_AUDIO
-		SDL_Delay(500000);
+		SDL_Delay(1000);
 		SDL_CloseAudio();
     SDL_Quit();
 	#endif
