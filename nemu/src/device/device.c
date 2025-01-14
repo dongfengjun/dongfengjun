265d644bb631970da0b406312c1955c52ee7ebad
tracer-ysyx <tracer@ysyx.org>
2025-01-14 16:29:30 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:29:30 up 20 min,  1 user,  load average: 0.35, 0.41, 0.25

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -42,12 +42,12 @@ void device_update() {
   }
   last = now;
 
-/***audio***
-  SDL_PauseAudio(0);
-  SDL_Delay(200);
-******/
-
   IFDEF(CONFIG_HAS_VGA, vga_update_screen());
+
+#ifdef CONFIG_HAS_AUDIO
+	SDL_PauseAudio(0);
+	SDL_Delay(200);
+#endif
 
 #ifndef CONFIG_TARGET_AM
   SDL_Event event;
