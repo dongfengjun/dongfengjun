29c82bea6f5ffefffb3af76ba1c207b943d4ddc6
tracer-ysyx <tracer@ysyx.org>
2025-01-15 20:30:24 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:30:24 up  5:25,  1 user,  load average: 0.10, 0.15, 0.23

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -46,6 +46,7 @@ void init_audio() {
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
   add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
 /***audio play***/
+	mmio_write(0xa000020c, 4, CONFIG_SB_SIZE);
   SDL_AudioSpec s = {};
   s.format = AUDIO_S16SYS;  // 系统中音频数据的格式使用16位有符号数来表示
   s.userdata = NULL;	// 不使用
