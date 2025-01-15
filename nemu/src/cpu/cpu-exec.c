3892093debd7ec0b43af0a17da4aae7dd4b65f9e
tracer-ysyx <tracer@ysyx.org>
2025-01-15 18:08:04 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:08:04 up  3:02,  1 user,  load average: 0.08, 0.06, 0.10

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -55,7 +55,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   char *p = s->logbuf;
 	char *irp = iringbuf;
   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
-	irp += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
+	irp += snprintf(irp, sizeof(iringbuf), FMT_WORD ":", s->pc);
   int ilen = s->snpc - s->pc;
   int i;
   uint8_t *inst = (uint8_t *)&s->isa.inst.val;
