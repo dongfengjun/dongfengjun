abc6de63539a251eb4fb05e33a82e9a8f174377a
tracer-ysyx <tracer@ysyx.org>
2025-01-15 21:10:56 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:10:56 up  6:05,  1 user,  load average: 0.25, 0.16, 0.18

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -30,7 +30,7 @@ void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
 uint32_t *sbuf = (uint32_t *)(uintptr_t)AUDIO_SBUF_ADDR;
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
 	if(sbuf - (uint32_t *)(uintptr_t)AUDIO_SBUF_ADDR == 
-		inl(AUDIO_SBUF_SIZE_ADDR)) {
+		inl(AUDIO_SBUF_SIZE_ADDR) - 1) {
 		sbuf = (uint32_t *)(uintptr_t)AUDIO_SBUF_ADDR;
 	}
 	else {
