efb456a05db0886bf2fc7856aea685128bdba451
tracer-ysyx <tracer@ysyx.org>
2025-01-16 19:38:00 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:38:00 up  3:06,  1 user,  load average: 0.68, 0.39, 0.36

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -32,6 +32,7 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
 	uint32_t *ctlbuf = ctl->buf.start;
 	for(uint32_t *p = ctl->buf.start; p != (ctl->buf.end); p ++) {
 		if(sbuf - (uint32_t *)(uintptr_t)AUDIO_SBUF_ADDR >= (inl(AUDIO_SBUF_SIZE_ADDR)/4)) {
+			printf("111\n");
 		  sbuf = (uint32_t *)(uintptr_t)AUDIO_SBUF_ADDR;
 		}
 		*sbuf ++ = *ctlbuf ++;
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -70,6 +70,7 @@ void audio_callback(void* userdata, uint8_t *stream, int len) {
 	SDL_MixAudio(stream, audio_pos, len, SDL_MIX_MAXVOLUME);
 	memset(audio_pos, 0, len);
 	if((audio_pos - sbuf + len) == CONFIG_SB_SIZE/4) {
+		printf("aaa\n");
 		audio_pos = sbuf;
 	}
 	else {
