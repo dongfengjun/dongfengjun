62e40725bae3f8df1f3b2670c62f70e4f137fac3
tracer-ysyx <tracer@ysyx.org>
2025-01-16 18:19:30 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:19:30 up  1:47,  1 user,  load average: 0.08, 0.03, 0.04

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -27,18 +27,21 @@ void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
   stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
-uint32_t *sbuf = (uint32_t *)(uintptr_t)AUDIO_SBUF_ADDR;
+//uint32_t *sbuf = (uint32_t *)(uintptr_t)AUDIO_SBUF_ADDR;
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+	/***
 	if(sbuf >= (uint32_t *)(uintptr_t)AUDIO_SBUF_ADDR + inl(AUDIO_SBUF_SIZE_ADDR) - 1) {
 		printf("abcabcabcacb\n\n");
 		sbuf = (uint32_t *)(uintptr_t)AUDIO_SBUF_ADDR;
 	}
-	else {
+	***/
+	//else {
+		uint32_t *sbuf = (uint32_t *)(uintptr_t)AUDIO_SBUF_ADDR;
 		uint32_t *ctlbuf = ctl->buf.start;
 		for(uint32_t *p = ctl->buf.start; p != (ctl->buf.end); p ++) {
 			*sbuf ++ = *ctlbuf ++;
 		}
 		uint32_t count = sbuf - (uint32_t *)(uintptr_t)AUDIO_SBUF_ADDR;
 		outl(0xa0000214, count);
-	}
+	//}
 }
