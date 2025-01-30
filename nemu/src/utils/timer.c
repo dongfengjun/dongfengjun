179d2f4e402c70fb7aea1fa5536c7c9124a930f8
tracer-ysyx <tracer@ysyx.org>
2025-01-30 22:31:06 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:31:06 up  5:12,  1 user,  load average: 0.45, 0.37, 0.32

--- nemu/src/utils/timer.c
+++ nemu/src/utils/timer.c
@@ -39,8 +39,9 @@ static uint64_t get_time_internal() {
 }
 
 uint64_t get_time() {
-  if (boot_time == 0) {boot_time = get_time_internal(); return 0;}
+  if (boot_time == 0) boot_time = get_time_internal();
   uint64_t now = get_time_internal();
+	printf("boot = %ld\ntime = %ld\n", boot_time, now - boot_time);
   return now - boot_time;
 }
 
