32645f9d788159fc692b61637c836e3cf592f8b2
tracer-ysyx <tracer@ysyx.org>
2025-01-30 23:27:05 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:27:05 up  6:08,  1 user,  load average: 0.04, 0.04, 0.17

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -26,6 +26,7 @@ static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
 		printf("us = %ld\n", us);
     rtc_port_base[0] = (uint32_t)us;
     rtc_port_base[1] = us >> 32;
+		printf("rtc = %d\n", *rtc_port_base);
   }
 }
 
