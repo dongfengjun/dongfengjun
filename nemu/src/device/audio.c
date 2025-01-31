a9eeb81335da92e68d00609b26776850ab0b9474
tracer-ysyx <tracer@ysyx.org>
2025-01-31 22:19:18 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:19:18 up  4:48,  1 user,  load average: 0.50, 0.27, 0.15

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -32,7 +32,7 @@ static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
 //uint8_t *audio_pos;
 static int pos = 0;
-#define SBUF_SIZE audio_base[reg_sbuf_size]
+#define SBUF_SIZE 1024
 
 static void audio_callback(void *udata, uint8_t *stream, int len) {
 	SDL_LockAudio();
