dce837ea1b733bde931cd8a400d80a704f826df1
tracer-ysyx <tracer@ysyx.org>
2025-01-31 17:36:10 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:36:10 up 5 min,  1 user,  load average: 0.29, 0.43, 0.24

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,6 +1,5 @@
 #include <am.h>
 #include <nemu.h>
-#include <stdio.h>////
 
 void __am_timer_init() {
 }
--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -20,13 +20,14 @@
 static uint32_t *rtc_port_base = NULL;
 
 static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
-  assert(offset == 0 || offset == 4);
+  printf("aaa ");
+	assert(offset == 0 || offset == 4);
   if (!is_write && offset == 4) {
+		printf("bbb ");
     uint64_t us = get_time();	//utils clock_gettime
     rtc_port_base[0] = (uint32_t)us;
     rtc_port_base[1] = us >> 32;
   }
-	printf("111\n");
 }
 
 #ifndef CONFIG_TARGET_AM
