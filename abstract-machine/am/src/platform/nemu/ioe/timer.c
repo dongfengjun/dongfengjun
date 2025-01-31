e742f2c3c2d7b214e0f885097618d68da9e9533f
tracer-ysyx <tracer@ysyx.org>
2025-01-31 18:10:23 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:10:23 up 39 min,  1 user,  load average: 0.87, 0.55, 0.32

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -6,11 +6,8 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  printf("000 ");
 	uptime->us = (uint64_t)inl(RTC_ADDR + 4) << 32;
-	printf("111 ");
 	uptime->us = uptime->us + ((uint64_t)inl(RTC_ADDR));
-	printf("uptime us = %d %d\n", uptime->us, uptime->us >> 32);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -20,10 +20,8 @@
 static uint32_t *rtc_port_base = NULL;
 
 static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
-  printf("aaa ");
 	assert(offset == 0 || offset == 4);
   if (!is_write && offset == 4) {
-		printf("bbb ");
     uint64_t us = get_time();	//utils clock_gettime
     rtc_port_base[0] = (uint32_t)us;
     rtc_port_base[1] = us >> 32;
