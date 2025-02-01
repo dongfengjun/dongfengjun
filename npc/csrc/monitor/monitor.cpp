b7b068d8d68aa3ed92e1619084835b08d905da66
tracer-ysyx <tracer@ysyx.org>
2025-02-01 14:26:40 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:26:40 up  1:16,  1 user,  load average: 0.06, 0.04, 0.07

--- npc/csrc/monitor/monitor.cpp
+++ npc/csrc/monitor/monitor.cpp
@@ -86,7 +86,7 @@ void init_monitor(int argc, char *argv[]) {
   init_mem();
 
 	/* Initialize devices. */
-  IFDEF(CONFIG_DEVICE, init_device());
+  //IFDEF(CONFIG_DEVICE, init_device());
 	
 	/* Perform ISA dependent initialization. TODO:Restart default im reg pc
   init_isa();*/
