08cfe18b523c4a506849b8d0e3f77e035bdb9fe7
tracer-ysyx <tracer@ysyx.org>
2025-02-01 16:58:23 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:58:23 up  3:48,  1 user,  load average: 0.85, 0.54, 0.83

--- npc/csrc/dpic.cpp
+++ npc/csrc/dpic.cpp
@@ -22,9 +22,11 @@ int pmem_read(int raddr) {
 		clock_gettime(CLOCK_MONOTONIC_COARSE, &now);
 		us = now.tv_sec * 1000000 + now.tv_nsec / 1000;
 		result = us >> 32;
+		printf("time high:%d\n", result);
 	}
 	else if(raddr == 0xa0000048) {
 		result = (uint32_t)us;
+		printf("time low:%d\n", result);
 	}
 	else {
 		result = paddr_read(raddr, 4);
