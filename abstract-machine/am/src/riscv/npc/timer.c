2906c37307fca0f6c1973d3cbb3a01a8e6c8f4f7
tracer-ysyx <tracer@ysyx.org>
2025-02-03 22:33:23 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:33:23 up  2:18,  1 user,  load average: 0.20, 0.10, 0.09

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -5,8 +5,8 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uint32_t high = inw(0xa0000052);
-	uint32_t low = inw(0xa0000048);
+  uint32_t high = inl(0xa0000052);
+	uint32_t low = inl(0xa0000048);
 	uptime->us = low + ((uint64_t)high << 32);
 }
 
