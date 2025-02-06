b7920fe7719167e9440623004637dfdad9587bed
tracer-ysyx <tracer@ysyx.org>
2025-02-06 22:18:13 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:18:13 up 11:20,  1 user,  load average: 0.05, 0.08, 0.10

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -18,9 +18,18 @@
 
 #include <common.h>
 
+// system reg
+typedef struct {
+	vaddr_t mepc;
+	word_t mstatus;
+	word_t mcause;
+	word_t mtvec;
+} MUXDEF(CONFIG_RV64, riscv64_CSRs, riscv32_CSRs);
+
 typedef struct {
   word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
   vaddr_t pc;
+	MUXDEF(CONFIG_RV64, riscv64_CSRs, riscv32_CSRs) csr;
 } MUXDEF(CONFIG_RV64, riscv64_CPU_state, riscv32_CPU_state);
 
 // decode
