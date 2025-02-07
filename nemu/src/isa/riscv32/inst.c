ba415cdf4ed39323d4aa79d94572569003f913a5
tracer-ysyx <tracer@ysyx.org>
2025-02-07 18:26:04 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:26:04 up  7:58,  1 user,  load average: 0.91, 0.38, 0.14

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -23,7 +23,7 @@
 #define Mr vaddr_read
 #define Mw vaddr_write
 //CSRs
-static vaddr_t *CSRs(word_t csr) {
+static vaddr_t *csrs(word_t csr) {
 	switch(csr){
 		case 0x341: return &(cpu.csr.mepc);
 		case 0x342: return &(cpu.csr.mcause);
@@ -32,10 +32,10 @@ static vaddr_t *CSRs(word_t csr) {
 		default: panic("Wait Add CSR");
 	}
 }
+#define CSRs(i) *(csrs(i))
 
-#define CSR(i) *(CSRs(i))
 #define MRET() { \
-	s->dnpc = *CSRs(0x341); \
+	s->dnpc = CSRs(0x341); \
 	cpu.csr.mstatus &= ~(1<<3); \
 	cpu.csr.mstatus |= ((cpu.csr.mstatus&(1<<7))>>4); \
 	cpu.csr.mstatus |= (1<<7); \
@@ -168,9 +168,9 @@ static int decode_exec(Decode *s) {
 /******/
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , I, s->dnpc = isa_raise_intr(R(17), s->pc); ETRACE()); //R(17) is $a7
-	INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , I, R(rd) = CSR(imm); CSR(imm) = src1);
-	INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs  , I, R(rd) = *CSRs(imm); *CSRs(imm) |= src1);
-	INSTPAT("??????? ????? ????? 011 ????? 11100 11", csrrc  , I, R(rd) = *CSRs(imm); *CSRs(imm) &= ~src1);
+	INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , I, R(rd) = CSRs(imm); CSRs(imm) = src1);
+	INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs  , I, R(rd) = CSRs(imm); CSRs(imm) |= src1);
+	INSTPAT("??????? ????? ????? 011 ????? 11100 11", csrrc  , I, R(rd) = CSRs(imm); CSRs(imm) &= ~src1);
 	INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , R, MRET(););
 	INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
