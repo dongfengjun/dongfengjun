8e4bba1bcbfaadd8da85e5c8b89a773fd52fe6e5
tracer-ysyx <tracer@ysyx.org>
2025-02-09 17:04:57 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:04:57 up 51 min,  1 user,  load average: 0.16, 0.18, 0.18

--- npc/vsrc/top_ysyx_24110017.v
+++ npc/vsrc/top_ysyx_24110017.v
@@ -56,7 +56,7 @@ assign mepc_wen = (op == 7'b1110011 && imm == 32'd833) ? 1'b1 : 1'b0;
 assign mstatus_wen = (op == 7'b1110011 && imm == 32'd768) ? 1'b1 : 1'b0;
 assign mcause_wen = (op == 7'b1110011 && imm == 32'd834) ? 1'b1 : 1'b0;
 assign mtvec_wen =	(op == 7'b1110011 && imm == 32'd773) ? 1'b1 : 1'b0;
-assign mepc_in = (op == 7'b1110011 && imm == 32'd0 && funct == 3'b000) ? pc : csrs_in;	//ecall
-assign mstatus_in = (op == 7'b1110011 && imm == 32'd0 && funct == 3'b000) ? r2 : csrs_in; //ecall
+assign mepc_in = (op == 7'b1110011 && imm == 32'd0 && funct3 == 3'b000) ? pc : csrs_in;	//ecall
+assign mstatus_in = (op == 7'b1110011 && imm == 32'd0 && funct3 == 3'b000) ? r2 : csrs_in; //ecall
 
 endmodule
