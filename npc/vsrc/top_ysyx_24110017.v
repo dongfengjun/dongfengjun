d1240d27454ca6eae13fe1d97fbfae7ef11fdbd6
tracer-ysyx <tracer@ysyx.org>
2025-02-09 21:33:08 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:33:08 up  5:20,  1 user,  load average: 0.03, 0.08, 0.14

--- npc/vsrc/top_ysyx_24110017.v
+++ npc/vsrc/top_ysyx_24110017.v
@@ -52,8 +52,8 @@ assign csrs = (op == 7'b1110011 && imm == 32'd833) ? mepc :
 							(op == 7'b1110011 && imm == 32'd768) ? mstatus : 
 							(op == 7'b1110011 && imm == 32'd834) ? mcause :
 							(op == 7'b1110011 && imm == 32'd773) ? mtvec : 32'b0;
-assign mepc_wen = (op == 7'b1110011 && imm == 32'd833) ? 1'b1 : 1'b0;
-assign mstatus_wen = (op == 7'b1110011 && imm == 32'd768) ? 1'b1 : 1'b0;
+assign mepc_wen = ((op == 7'b1110011 && imm == 32'd833) || (op == 7'b1110011 && imm == 32'd0 && funct3 == 3'b000)) ? 1'b1 : 1'b0;
+assign mstatus_wen = ((op == 7'b1110011 && imm == 32'd768) || (op == 7'b1110011 && imm == 32'd0 && funct3 == 3'b000)) ? 1'b1 : 1'b0;
 assign mcause_wen = (op == 7'b1110011 && imm == 32'd834) ? 1'b1 : 1'b0;
 assign mtvec_wen =	(op == 7'b1110011 && imm == 32'd773) ? 1'b1 : 1'b0;
 assign mepc_in = (op == 7'b1110011 && imm == 32'd0 && funct3 == 3'b000) ? pc : csrs_in;	//ecall
