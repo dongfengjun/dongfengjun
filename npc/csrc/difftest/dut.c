d0b61382f8f1f932be2bd8d485e5939fc409e586
tracer-ysyx <tracer@ysyx.org>
2025-02-10 17:16:44 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:16:44 up  6:09,  1 user,  load average: 0.24, 0.15, 0.17

--- npc/csrc/difftest/dut.c
+++ npc/csrc/difftest/dut.c
@@ -51,28 +51,27 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 		printf("pc diff ref\nref_pc= %08x\tcpu.pc = %08x\n", ref_r->pc, cpu.pc);
 		return false;
 	}
-/***/
+/*** risv_e volatile("li a5, -1; ecall") else volatile("li a7, -1; ecall")
 	if(ref_r->csr.mepc != cpu.csr.mepc) {
 		printf("mepc diff ref\nref_mepc= %08x\tcpu.mepc = %08x\n", ref_r->csr.mepc, cpu.csr.mepc);
 		return false;
 	}
-/***
-	if(ref_r->csr.mstatus != cpu.csr.mstatus) {
+
+//nemu un restart or init
+if(ref_r->csr.mstatus != cpu.csr.mstatus) {
     printf("mstatus diff ref\nref_mstatus= %08x\tcpu.mstatus = %08x\n", ref_r->csr.mstatus, cpu.csr.mstatus);
     return false;
   }
-***/
-/***
+
 	if(ref_r->csr.mcause != cpu.csr.mcause) {
     printf("mcause diff ref\nref_mcause= %08x\tcpu.mcause = %08x\n", ref_r->csr.mcause, cpu.csr.mcause);
     return false;
   }
-***/
 	if(ref_r->csr.mtvec != cpu.csr.mtvec) {
     printf("pc diff ref\nref_mtvec= %08x\tcpu.mtvec = %08x\n", ref_r->csr.mtvec, cpu.csr.mtvec);
     return false;
   }
-/***/
+***/
 	return true;
 }
 
