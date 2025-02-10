01948ab0225de5fa1ea63f98561a79bba023edbf
tracer-ysyx <tracer@ysyx.org>
2025-02-10 17:00:35 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:00:35 up  5:53,  1 user,  load average: 0.92, 0.44, 0.22

--- npc/csrc/isa/riscv32/reg.c
+++ npc/csrc/isa/riscv32/reg.c
@@ -9,15 +9,20 @@ const char *regs[] = {
   "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
+extern CPU_state cpu;
 
 void isa_regs_display() {
   for(int i = 0; i < 32; i ++) {
     printf("%s:0x%08x\t", regs[i], gpr_regs_display(i));
   }
   printf("\n");
+	printf("%s:0x%08x\n", "pc", cpu.pc);
+	printf("%s:0x%08x\n", "mepc", cpu.csr.mepc);
+	printf("%s:0x%08x\n", "mstatus", cpu.csr.mstatus);
+	printf("%s:0x%08x\n", "mcause", cpu.csr.mcause);
+	printf("%s:0x%08x\n", "mtvec", cpu.csr.mtvec);
 }
 
-extern CPU_state cpu;
 word_t isa_reg_str2val(const char *s, bool *success) {
   for(int i = 0 ; i < 32 ; i ++){
    	if(strcmp(regs[i], s) == 0){
@@ -27,7 +32,19 @@ word_t isa_reg_str2val(const char *s, bool *success) {
 	}
 	if(strcmp(s, "pc") == 0) {
 		return cpu.pc;
-	}  
+	}
+	if(strcmp(s, "mepc") == 0) {
+		return cpu.csr.mepc;
+	}
+	if(strcmp(s, "mstatus") == 0) {
+		return cpu.csr.mstatus;
+	}
+	if(strcmp(s, "mcause") == 0) {
+		return cpu.csr.mcause;
+	}
+	if(strcmp(s, "mtvec") == 0) {
+		return cpu.csr.mtvec;
+	}
 	*success = false;
 	printf("Invalid reg name\n");
 	return 0;
