1c8e457c45621fad5e53df764eb3d05b7b3b4bd9
tracer-ysyx <tracer@ysyx.org>
2025-02-11 16:27:04 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:27:04 up  5:27,  1 user,  load average: 0.02, 0.09, 0.14

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -10,12 +10,13 @@ Context* __am_irq_handle(Context *c) {
     switch (c->mcause) {
 			//case : ev.event = EVENT_IRQ_TIMER; break;
 			//case : ev.event = EVENT_IRQ_IODEV; break;
-			case -1: {ev.event = EVENT_YIELD;
-							c->mepc += 4;  break;}
+			case -1: ev.event = EVENT_YIELD; break;
       default: ev.event = EVENT_ERROR; break;
     }
-
+		
+		printf("mepc = %08x\n", c->mepc);
     c = user_handler(ev, c);
+		printf("		mepc = %08x\n", c->mepc);
     assert(c != NULL);
   }
 
