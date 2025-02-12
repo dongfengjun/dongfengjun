3a52151c48fed1e4ae734f0c1c634f1ab99e4238
tracer-ysyx <tracer@ysyx.org>
2025-02-12 14:13:07 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:13:07 up  3:00,  1 user,  load average: 0.02, 0.06, 0.25

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -43,6 +43,16 @@ IFDEF(CONFIG_ITRACE, char logbuf[128]);
 IFDEF(CONFIG_ITRACE, char iringbuf[128]);//Itrace
 static bool g_print_step = false;
 uint8_t fopcode;
+
+static void statistic() {
+  IFNDEF(CONFIG_TARGET_AM, setlocale(LC_NUMERIC, ""));
+#define NUMBERIC_FMT MUXDEF(CONFIG_TARGET_AM, "%", "%'") PRIu64
+  Log("host time spent = " NUMBERIC_FMT " us", g_timer);
+  Log("total guest instructions = " NUMBERIC_FMT, g_nr_guest_inst);
+  if (g_timer > 0) Log("simulation frequency = " NUMBERIC_FMT " inst/s", g_nr_guest_inst * 1000000 / g_timer);
+  else Log("Finish running in less than 1 us and can not calculate the simulation frequency");
+}
+
 void assert_fail_msg() {
   isa_regs_display();
 	IFDEF(CONFIG_ITRACE, iringbuf_push(iringbuf));
