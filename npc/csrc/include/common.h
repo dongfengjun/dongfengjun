bb156cd08786f1ef35abb9058fa76776051135db
tracer-ysyx <tracer@ysyx.org>
2025-02-12 14:56:36 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:56:36 up  3:44,  1 user,  load average: 0.10, 0.08, 0.22

--- npc/csrc/include/common.h
+++ npc/csrc/include/common.h
@@ -65,8 +65,6 @@ typedef struct {
 } CPU_state;
 
 //Imemory
-int pmem_read(int raddr); //dpic	
-void pmem_write(int waddr, int wdata, char wmask);
 uint8_t* guest_to_host(paddr_t paddr);
 paddr_t host_to_guest(uint8_t *haddr);
 void init_mem();
