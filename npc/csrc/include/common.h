8e867c562faa8fd888b09cf20595a5cd57f05e78
tracer-ysyx <tracer@ysyx.org>
2025-02-13 21:36:02 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:36:02 up 10:22,  1 user,  load average: 0.96, 0.31, 0.11

--- npc/csrc/include/common.h
+++ npc/csrc/include/common.h
@@ -35,7 +35,7 @@ typedef uint16_t ioaddr_t;
 			//#define CONFIG_MTRACE 1 
 		//#define CONFIG_WATCHPOINT 1
 //	#endif
-#define CONFIG_DIFFTEST 1
+//#define CONFIG_DIFFTEST 1
 
 #define CONFIG_MBASE 0x80000000//IM
 #define CONFIG_MSIZE 0x8000000
@@ -98,5 +98,6 @@ void iringbuf_display();
 //difftest
 void init_difftest(char *ref_so_file, long img_size, int port);
 void difftest_step(vaddr_t pc, vaddr_t npc);
+void difftest_skip_ref();
 
 #endif
