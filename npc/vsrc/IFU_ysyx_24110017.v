14323949b80320763ac40491044be356c94469dc
tracer-ysyx <tracer@ysyx.org>
2025-02-14 13:07:42 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:07:42 up  2:22,  1 user,  load average: 0.08, 0.06, 0.08

--- npc/vsrc/IFU_ysyx_24110017.v
+++ npc/vsrc/IFU_ysyx_24110017.v
@@ -6,18 +6,15 @@ wire [31:0]pc;
 reg [31:0]inst;
 //wire [31:0]inst;
 
-/***DPIC***/
+/***单周期***
 import "DPI-C" function int pmem_read(input int raddr);
 
-wire ifuen;
-assign ifuen = 1'b1;
-//(pc != 32'h00000000) ? 1'b1:1'b0;
 always @(*) begin
-	if(ifuen) begin
+	if(pc != 32'h0) begin
 		inst = pmem_read(pc);
 	end
 	else begin
-		inst = 32'h00000000;
+		inst = 32'h0;
 	end
 end
 /***E*N*D***/
@@ -27,6 +24,28 @@ wire wen;
 wire [7:0]waddr;
 wire [31:0]wdata;
 Sta_RegisterFile Sta_RegisterFile(clk,wdata,wdata[7:0],wen,pc[7:0],inst);
-***E*N*D***/
+/***E*N*D***/
+
+/***多周期sram***/
+import "DPI-C" function int pmem_read(input int raddr);
+
+reg [31:0]rdata
+always @(*) begin
+  if(pc != 32'h0) begin
+    rdata = pmem_read(pc);
+  end
+  else begin
+    rdata = 32'h0;
+  end
+end
+
+always @(posedge clk) begin
+	if(rst) begin
+		inst <= 32'h0;
+	end
+	else begin
+		inst <= rdata;
+	end
+endmodule
 
 endmodule
