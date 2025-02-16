980c1624b37d45b306260b89a759e48db8bc73af
tracer-ysyx <tracer@ysyx.org>
2025-02-16 20:56:15 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:56:15 up  4:40,  1 user,  load average: 0.00, 0.10, 0.13

---
+++ npc/vsrc/SRAM_IFU_ysyx_24110017.v
@@ -0,0 +1,30 @@
+module SRAM_IFU_ysyx_24110017(clk,rst,raddr,rdata);
+input clk;
+input rst;
+input [31:0]raddr;
+output [31:0]rdata;
+
+wire [31:0]raddr;
+reg [31:0]rdata;
+reg[31:0]tmp;
+import "DPI-C" function int pmem_read(input int raddr);
+
+always @(*) begin
+	if(raddr != 32'h0) begin
+		tmp = pmem_read(raddr);
+	end
+	else begin
+		tmp = 32'h0;
+	end
+end
+
+always @(posedge clk) begin
+	if(rst) begin
+		rdata <= 32'h0;
+	end
+	else begin
+		rdata <= tmp;
+	end
+end
+
+endmodule
