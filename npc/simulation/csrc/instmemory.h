7d52c3c6d738cf12a46f3827bd9a34128fc6439b
tracer-ysyx <tracer@ysyx.org>
2025-02-16 17:06:07 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:06:07 up 49 min,  1 user,  load average: 0.22, 0.34, 0.30

--- npc/simulation/csrc/instmemory.h
+++
@@ -1,9 +0,0 @@
-#ifndef INSTMEMORY_H
-#define INSTMEMORY_H
-
-int mem_init();
-uint32_t pmem_read(int addr);
-int pmem_write(uint32_t content, uint64_t addr, uint32_t len);
-int free_memory();
-
-#endif
