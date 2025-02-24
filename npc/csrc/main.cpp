1a81016412e194997f8dc3379fe72ff48a06d715
tracer-ysyx <tracer@ysyx.org>
2025-02-24 21:06:05 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:06:05 up  1:37,  1 user,  load average: 0.11, 0.14, 0.15

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -235,14 +235,14 @@ void dump_wave() {
 }
 
 void single_cycle() {
-	top->clk=1;top->eval();
+	top->clk=1;top->clk=0;top->clk=1;top->clk=0;top->clk=1;top->clk=0;top->clk=1;top->clk=0;top->clk=1;top->clk=0;top->clk=1;top->clk=0;top->clk=1;top->clk=0;top->eval();
 #ifdef CONFIG_DUMP_WAVE
 	dump_wave();
 #endif
-	top->clk=0;top->eval();
-#ifdef CONFIG_DUMP_WAVE
-	dump_wave();
-#endif
+	//top->clk=0;top->eval();
+//#ifdef CONFIG_DUMP_WAVE
+	//dump_wave();
+//#endif
 }
 static void reset(int n) {
 	top->rst=1;top->eval();
