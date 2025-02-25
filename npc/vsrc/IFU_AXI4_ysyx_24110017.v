4d9ff28e514ca0a410febed7327b219bd5b82de7
tracer-ysyx <tracer@ysyx.org>
2025-02-25 16:52:10 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:52:10 up  3:07,  1 user,  load average: 0.00, 0.02, 0.09

--- npc/vsrc/IFU_AXI4_ysyx_24110017.v
+++ npc/vsrc/IFU_AXI4_ysyx_24110017.v
@@ -399,7 +399,7 @@
               end                                                  
               // Signals a new write address/ write data is        
               // available by user logic                           
-            else if (M_AXI_RVALID && axi_rready)                   
+            else if (M_AXI_RVALID && axi_rready M_AXI_RVALID && axi_rready && (mst_exec_state == (2'b10)))                   
               begin                                                
                 inst <= M_AXI_RDATA;
               end                                                  
