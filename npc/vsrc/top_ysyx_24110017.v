98947414ff39a73fe13033605de49cdc7e607a70
tracer-ysyx <tracer@ysyx.org>
2025-02-26 16:33:05 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:33:05 up  3:41,  1 user,  load average: 0.14, 0.15, 0.11

--- npc/vsrc/top_ysyx_24110017.v
+++ npc/vsrc/top_ysyx_24110017.v
@@ -26,7 +26,7 @@ wire mepc_wen, mstatus_wen, mcause_wen, mtvec_wen;
 PCU_ysyx_24110017 PCU(clk,rst,op,funct3,imm,r1,r2,mtvec,mepc,pc,dnpc);
 IFU_ysyx_24110017 IFU(clk,rst,pc,inst);
 IDU_ysyx_24110017 IDU(inst,op,rd,funct3,rs1,rs2,imm,funct7,shamt,wr_en);
-RegisterFile_ysyx_24110017 #(5,32) RF (clk,xrd,rd,wr_en,raddr1,r1,raddr2,r2);
+RegisterFile_ysyx_24110017 #(5,32) RF (clk,xrd,rd,wr_en,rs1,r1,rs2,r2);
 Reg_ysyx_24110017 #(32, 32'b0) mepc_ysyx_24110017 (clk, rst, mepc_in, mepc, mepc_wen);
 Reg_ysyx_24110017 #(32, 32'h1800) mstatus_ysyx_24110017 (clk, rst, csrs_in, mstatus, mstatus_wen);
 Reg_ysyx_24110017 #(32, 32'b0) mcause_ysyx_24110017 (clk, rst, mcause_in, mcause, mcause_wen);
