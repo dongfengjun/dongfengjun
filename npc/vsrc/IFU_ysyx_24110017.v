3f2e4cd5e17db45630c19a4c12b40f4b4ee2917c
tracer-ysyx <tracer@ysyx.org>
2025-02-28 20:10:34 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:10:34 up  1:11,  1 user,  load average: 0.16, 0.06, 0.05

--- npc/vsrc/IFU_ysyx_24110017.v
+++ npc/vsrc/IFU_ysyx_24110017.v
@@ -25,7 +25,7 @@ output M_AXI_BREADY;
 output [31:0] M_AXI_ARADDR;
 output M_AXI_ARVALID;
 input M_AXI_ARREADY;
-input [C_M_AXI_DATA_WIDTH-1 : 0] M_AXI_RDATA;
+input [31:0] M_AXI_RDATA;
 input [1 : 0] M_AXI_RRESP;
 input M_AXI_RVALID;
 output M_AXI_RREADY;
