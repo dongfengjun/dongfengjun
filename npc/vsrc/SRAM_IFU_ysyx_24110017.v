2d14572fa79718cfdcad5d16fa2f983417e625b3
tracer-ysyx <tracer@ysyx.org>
2025-02-28 13:59:33 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:59:33 up 19 min,  1 user,  load average: 0.46, 0.35, 0.34

--- npc/vsrc/SRAM_IFU_ysyx_24110017.v
+++ npc/vsrc/SRAM_IFU_ysyx_24110017.v
@@ -1,24 +1,25 @@
-module SRAM_IFU_ysyx_24110017(clk,rst,S_AXI_AWADDR,S_AXI_AWVALID,S_AXI_AWREADY,S_AXI_WDATA,S_AXI_WSTRB,S_AXI_WVALID,S_AXI_WREADY,S_AXI_BRESP,S_AXI_BVALID,S_AXI_BREADY,S_AXI_ARADDR,S_AXI_ARVALID,S_AXI_ARREADY,S_AXI_RDATA,S_AXI_RRESP,S_AXI_RVALID,S_AXI_RREADY);
-input clk;
-input rst;
-input wire [31:0] S_AXI_AWADDR;
-input wire  S_AXI_AWVALID;
-output wire  S_AXI_AWREADY;
-input wire [31:0] S_AXI_WDATA;
-input wire [3:0] S_AXI_WSTRB;
-input wire  S_AXI_WVALID;
-output wire  S_AXI_WREADY;
-output wire [1:0] S_AXI_BRESP;
-output wire  S_AXI_BVALID;
-input wire  S_AXI_BREADY;
+module SRAM_IFU_ysyx_24110017(
+input clk,
+input rst,
+input wire [31:0] S_AXI_AWADDR,
+input wire  S_AXI_AWVALID,
+output wire  S_AXI_AWREADY,
+input wire [31:0] S_AXI_WDATA,
+input wire [3:0] S_AXI_WSTRB,
+input wire  S_AXI_WVALID,
+output wire  S_AXI_WREADY,
+output wire [1:0] S_AXI_BRESP,
+output wire  S_AXI_BVALID,
+input wire  S_AXI_BREADY,
 
-input wire [31: 0] S_AXI_ARADDR;
-input wire  S_AXI_ARVALID;
-output wire  S_AXI_ARREADY;
-output wire [31:0] S_AXI_RDATA;
-output wire [1:0] S_AXI_RRESP;
-output wire  S_AXI_RVALID;
-input wire  S_AXI_RREADY;
+input wire [31: 0] S_AXI_ARADDR,
+input wire  S_AXI_ARVALID,
+output wire  S_AXI_ARREADY,
+output wire [31:0] S_AXI_RDATA,
+output wire [1:0] S_AXI_RRESP,
+output wire  S_AXI_RVALID,
+input wire  S_AXI_RREADY
+);
 
 wire [31:0]raddr;
 reg [31:0]rdata;
