74549183fabcc2e3c5c01458cdceb7878cbcb8a9
tracer-ysyx <tracer@ysyx.org>
2025-02-28 15:08:14 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:08:14 up  1:22,  1 user,  load average: 0.19, 0.14, 0.17

--- npc/vsrc/PCU_ysyx_24110017.v
+++ npc/vsrc/PCU_ysyx_24110017.v
@@ -35,14 +35,14 @@ assign bgeuen = (op == 7'b1100011 && funct3 == 3'b111 && (r1 >= r2));
 assign ecall_en = (op == 7'b1110011 && offset == 32'd0 && funct3 == 3'b000);
 assign mret_en = (op == 7'b1110011 && offset == 32'b1100000010 && funct3 == 3'b000);
 
-assign dnpc = (jalen) ? (pc + offset)	//jal
+assign dnpc = (jalen) ? (pc + offset - 4)	//jal
 	: (jalren) ? ((r1 + offset) & ~1) //jalr
-	: (beqen) ? (pc + offset)	//beq
-	: (bneen) ? (pc + offset)	//bne
-	: (blten) ? (pc + offset)	//blt
-	: (bgeen) ? (pc + offset)	//bge
-	: (bltuen) ? (pc + offset)	//bltu
-	:	(bgeuen) ? (pc + offset)	//bgeu
+	: (beqen) ? (pc + offset - 4)	//beq
+	: (bneen) ? (pc + offset - 4)	//bne
+	: (blten) ? (pc + offset - 4)	//blt
+	: (bgeen) ? (pc + offset - 4)	//bge
+	: (bltuen) ? (pc + offset - 4)	//bltu
+	:	(bgeuen) ? (pc + offset - 4)	//bgeu
 	: (ecall_en) ? mtvec  //ecall
 	: (mret_en) ? mepc  //mret
 	: pc + 4;
