6e96d5161d703aaff7f2c8085cdb3615e0d321f3
tracer-ysyx <tracer@ysyx.org>
2025-03-01 17:54:08 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:54:08 up  5:42,  1 user,  load average: 0.01, 0.04, 0.08

--- npc/vsrc/LSU_ysyx_24110017.v
+++ npc/vsrc/LSU_ysyx_24110017.v
@@ -45,6 +45,7 @@ Sta_RegisterFile Sta_RegisterFile(clk,wdata,wdata[7:0],wen,raddr[7:0],rdata);
 ***E*N*D***/
 
 /***多周期***/
+reg [31:0]axi_araddr_reg;
 reg [31:0]axi_awaddr_reg,axi_wdata_reg;
 reg [7:0]axi_wstrb_reg;
 wire [31:0] AXI_AWADDR,AXI_WDATA,AXI_ARADDR,AXI_RDATA;
@@ -96,6 +97,7 @@ always @(posedge clk or posedge rst) begin
 						else begin
 							axi_arvalid <= 1'b1;
 							state <= READ;
+							axi_araddr_reg <= raddr;
 						end
 					end
 				end
@@ -103,7 +105,7 @@ always @(posedge clk or posedge rst) begin
           if(AXI_ARREADY) begin
 						axi_arvalid <= 0;
             axi_rready <= 1;//加判断条件
-						axi_araddr <= raddr;
+						axi_araddr <= axi_araddr_reg;
           end
 	        if(AXI_RVALID) begin
             rdata <= AXI_RDATA;
