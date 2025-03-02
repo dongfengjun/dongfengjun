6223b4fdcabb12370ccfcca4f84f410454da0d4f
tracer-ysyx <tracer@ysyx.org>
2025-03-02 19:44:49 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:44:49 up  5:48,  1 user,  load average: 0.22, 0.15, 0.12

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -14,6 +14,7 @@ LDFLAGS   += --gc-sections -e _start
 NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
 #NEMUFLAGS += -b	#批处理
 NEMUFLAGS += -e $(IMAGE).elf	#ftrace
+NEMUFLAGS += am_hello
 
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I$(AM_HOME)/am/src/platform/nemu/include
