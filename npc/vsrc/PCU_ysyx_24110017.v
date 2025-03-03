5b389f0ed1e1315473fcb065f77482ab0346c151
tracer-ysyx <tracer@ysyx.org>
2025-03-03 17:21:19 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:21:19 up  5:12,  1 user,  load average: 0.03, 0.05, 0.07

--- npc/vsrc/PCU_ysyx_24110017.v
+++ npc/vsrc/PCU_ysyx_24110017.v
@@ -1,16 +1,10 @@
-module PCU_ysyx_24110017(clk,rst,op,funct3,offset,r1,r2,mtvec,mepc,pc,dnpc,IF_DONE);
+module PCU_ysyx_24110017(clk,rst,pc,dnpc,IF_DONE);
 input	clk;
 input rst;
-input [6:0]op;
-input [2:0]funct3;
-input [31:0]offset;
-input [31:0]r1;
-input [31:0]r2;
-input [31:0]mtvec;
-input [31:0]mepc;
 output [31:0]pc;
 input [31:0]dnpc;
 input IF_DONE;
+
 reg [31:0]pc;
 wire [31:0]dnpc;
 
