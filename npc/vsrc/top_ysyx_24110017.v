6999e6d4091fed3696ae6314283759ca0148ab8a
tracer-ysyx <tracer@ysyx.org>
2025-03-04 16:51:29 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:51:29 up  3:24,  1 user,  load average: 0.04, 0.05, 0.01

--- npc/vsrc/top_ysyx_24110017.v
+++ npc/vsrc/top_ysyx_24110017.v
@@ -6,43 +6,49 @@ output [31:0]dnpc;
 output [31:0]inst;
 output DIFFTEST;
 
-wire [6:0]op;
-wire [4:0]rd,lrd;	//R I U J
-wire [2:0]funct3;
-wire [4:0]rs1;	//R I S B 
-wire [4:0]rs2;	//R S B
-wire [31:0]imm;
-wire [6:0]funct7;	//R
-wire [4:0]shamt;	//I shamt
-wire gpr_wen;
+/***PCU***/
+wire [31:0]pc;
+wire [31:0]dnpc;
+/***IFU***/
 wire [31:0]inst;
-
-wire [31:0]res;
-wire l_wen;
-wire [31:0]r1,r2,a,b,xrd;
-wire [31:0]mepc,o_mepc,w_mepc,mstatus,o_mstatus,w_mstatus,mcause,o_mcause,w_mcause,mtvec,o_mtvec,w_mtvec;
-wire mepc_wen,mstatus_wen,mcause_wen,mtvec_wen;
-wire [4:0]rf_addr;
-wire rf_wen;
-
-wire ls_valid,ls_wen;
-wire [31:0]ls_waddr,ls_wdata,ls_raddr;
-wire [7:0]ls_wmask;
-
 wire IFU_DONE,DIFFTEST;//IFU_AXI4-LITE
 wire [31:0] IFU_AXI_AWADDR,IFU_AXI_WDATA,IFU_AXI_ARADDR,IFU_AXI_RDATA;
 wire [3:0] IFU_AXI_WSTRB;
 wire [1:0] IFU_AXI_BRESP,IFU_AXI_RRESP;
 wire IFU_AXI_AWVALID,IFU_AXI_AWREADY,IFU_AXI_WVALID,IFU_AXI_WREADY,IFU_AXI_BVALID,IFU_AXI_BREADY,IFU_AXI_ARVALID,IFU_AXI_ARREADY,IFU_AXI_RVALID,IFU_AXI_RREADY;
-
+/***IDU***/
+wire [6:0]op;
+wire [4:0]rd;	//R I U J
+wire [2:0]funct3;
+wire [4:0]rs1;	//R I S B 
+wire [4:0]rs2;	//R S B
+wire [31:0]imm;
+wire [6:0]funct7;	//R
+wire [4:0]shamt; //I shamt
+/***EXU***/
+wire [31:0]res;
+wire ls_valid,ls_wen;
+wire [31:0]ls_waddr,ls_wdata,ls_raddr;
+wire lbdone,lhdone,lwdone,lbudone,lhudone;
+wire [7:0]ls_wmask;
+wire [31:0]mepc,o_mepc,mstatus,o_mstatus,mcause,o_mcause,mtvec,o_mtvec;
+wire gpr_wen,mepc_wen,mstatus_wen,mcause_wen,mtvec_wen;
+/***LSU***/
 wire [31:0]ls_rdata;//LSU_AXI4-LITE
 wire [4:0]l_rd;
-wire lbdone,lhdone,lwdone,lbudone,lhudone;
 wire lb_w,lh_w,lw_w,lbu_w,lhu_w;
 wire [31:0] LSU_AXI_AWADDR,LSU_AXI_WDATA,LSU_AXI_ARADDR,LSU_AXI_RDATA;
 wire [7:0] LSU_AXI_WSTRB;
 wire [1:0] LSU_AXI_BRESP,LSU_AXI_RRESP;
 wire LSU_AXI_AWVALID,LSU_AXI_AWREADY,LSU_AXI_WVALID,LSU_AXI_WREADY,LSU_AXI_BVALID,LSU_AXI_BREADY,LSU_AXI_ARVALID,LSU_AXI_ARREADY,LSU_AXI_RVALID,LSU_AXI_RREADY;
+/***WBU***/
+wire [31:0]xrd;
+wire [4:0]rf_addr;
+wire rf_wen;
+wire [31:0]w_mepc,w_mstatus,w_mcause,w_mtvec;
+/***RFU***/
+wire [31:0]r1,r2;
+
 
 PCU_ysyx_24110017 PCU(clk,rst,
 		pc,dnpc,
