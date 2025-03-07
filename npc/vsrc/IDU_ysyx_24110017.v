6dbbb0926e5ec2b5dad38b0480cf2187e89007e5
tracer-ysyx <tracer@ysyx.org>
2025-03-07 18:03:30 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:03:30 up 55 min,  1 user,  load average: 0.14, 0.08, 0.01

--- npc/vsrc/IDU_ysyx_24110017.v
+++ npc/vsrc/IDU_ysyx_24110017.v
@@ -59,6 +59,7 @@ assign imm = (op == 7'b0110111 || op == 7'b0010111) ? immU
 
 
 /***DPIC*etrace***/
+import "DPI-C" function void npc_trap();
 always@(*) begin
 	if(inst == 32'b00000000000100000000000001110011) begin
 		npc_trap();
