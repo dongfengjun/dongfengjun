e2cd28f47466a256d473cc03759730baad080afe
tracer-ysyx <tracer@ysyx.org>
2025-03-08 11:45:31 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:45:31 up 40 min,  1 user,  load average: 0.00, 0.04, 0.07

--- npc/vsrc/IFU_ysyx_24110017.v
+++ npc/vsrc/IFU_ysyx_24110017.v
@@ -114,6 +114,9 @@ always @(posedge clk) begin
 			WAIT_SRAM: begin
 				ifu_ready <= 1'b0;
 				sram_start <= 1'b1;
+				if(sram_start == 1'b1) begin
+					sram_start <= 1'b0;
+				end
 				if(if_done) begin //判断条件
 					ifu_valid <= 1'b1;
 					sram_start <= 1'b0;
