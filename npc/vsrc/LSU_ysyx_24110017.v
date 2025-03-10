31f817c5f18117857ca13f2cad104c4f10adf687
tracer-ysyx <tracer@ysyx.org>
2025-03-10 17:49:30 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:49:30 up  5:48,  1 user,  load average: 0.01, 0.06, 0.08

--- npc/vsrc/LSU_ysyx_24110017.v
+++ npc/vsrc/LSU_ysyx_24110017.v
@@ -63,7 +63,7 @@ Sta_RegisterFile Sta_RegisterFile(clk,wdata,wdata[7:0],wen,raddr[7:0],rdata);
 /***多周期***/
 /***DELAY_TEST_RAND***/
 wire [7:0]rand_delay;
-reg [7:0]delay_counter;
+reg [7:0]delay_counter,delay_counter_2;
 lfsr_ysyx_24110017 lfsr_ysyx_20110017(clk,rst,rand_delay);
 /***END***/
 wire [31:0] M_AXI_AWADDR,M_AXI_WDATA,M_AXI_ARADDR,M_AXI_RDATA;
@@ -102,6 +102,8 @@ always @(posedge clk or posedge rst) begin
 		  axi_wvalid <= 0;
       axi_bready <= 0;
 			LSU_DONE <= 0;
+			delay_counter <= 0;
+			delay_counter_2 <= 0;
     end 
 		else begin
       case (state)
@@ -166,18 +168,18 @@ always @(posedge clk or posedge rst) begin
 					end
 /***DELAY_TEST_WVALID***/
 					if(M_AXI_AWVALID && M_AXI_AWREADY) begin
-            delay_counter <= rand_delay;
+            delay_counter_2 <= rand_delay;
           end
 					else begin
 						if(delay_counter == 0) begin
-              delay_counter <= delay_counter;
+              delay_counter_2 <= delay_counter_2;
             end
-						else if(delay_counter == 1) begin
+						else if(delay_counter_2 == 1) begin
 							axi_wvalid <= 1;
-							delay_counter <= 0;
+							delay_counter_2 <= 0;
 						end
 						else begin
-							delay_counter <= delay_counter - 1;
+							delay_counter_2 <= delay_counter_2 - 1;
 						end
 					end
 /***END***/
