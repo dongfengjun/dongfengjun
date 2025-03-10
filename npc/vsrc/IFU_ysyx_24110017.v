8b314af2d40a8f51126acae385b106862932dc7f
tracer-ysyx <tracer@ysyx.org>
2025-03-10 16:11:33 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:11:33 up  4:10,  1 user,  load average: 0.01, 0.06, 0.06

--- npc/vsrc/IFU_ysyx_24110017.v
+++ npc/vsrc/IFU_ysyx_24110017.v
@@ -215,17 +215,26 @@ always @(posedge clk) begin
                         axi_arvalid <= 1'b0;
 												axi_araddr <= pc;
 												//axi_rready <= 1'b1;
+										end
 										/***DELAY_TEST_RAND***M_AXI_RRDEADY***/
-												if(delay_counter == 0) begin
-													delay_counter <= rand_delay;
-												end
-												else if(delay_counter == 1) begin
-													axi_rready <= 1;
-													delay_counter <= 0;
-												end
-												else begin
-													delay_counter <= delay_counter - 1;
-												end
+										if (M_AXI_ARVALID && M_AXI_ARREADY) begin
+											if(delay_counter == 0) begin
+												delay_counter <= rand_delay;
+											end
+										end
+										else begin
+                      if(delay_counter == 0) begin
+                        delay_counter <= delay_counter;
+                      end
+                      else if(delay_counter == 1) begin
+                        state <= SRAM_FETCH;
+                        axi_rready <= 1;
+                        delay_counter <= 0;
+                      end
+                      else begin
+                        delay_counter <= delay_counter - 1;
+                      end
+                    end
 										/***END***/
                     end
                     if (M_AXI_RVALID && M_AXI_RREADY) begin
