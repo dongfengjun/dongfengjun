bb85de77ed47e6423fc70c2c747dc676d76c4513
tracer-ysyx <tracer@ysyx.org>
2025-03-10 17:06:28 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:06:28 up  5:05,  1 user,  load average: 0.05, 0.07, 0.27

--- npc/vsrc/LSU_ysyx_24110017.v
+++ npc/vsrc/LSU_ysyx_24110017.v
@@ -136,7 +136,7 @@ always @(posedge clk or posedge rst) begin
 					if(M_AXI_WREADY) begin
 						axi_wvalid <= 0;
 						axi_wdata <= wdata;//加判断条件
-						axi_wstrb <= wstrb;
+						axi_wstrb <= wmask;
 						axi_bready <= 1;
 					end
 					if(M_AXI_BVALID) begin
