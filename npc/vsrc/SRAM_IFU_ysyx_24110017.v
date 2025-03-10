4bf55d26702e3f6bacf4d2a72c51db846f8c84e3
tracer-ysyx <tracer@ysyx.org>
2025-03-10 15:40:20 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:40:20 up  3:39,  1 user,  load average: 0.03, 0.05, 0.06

--- npc/vsrc/SRAM_IFU_ysyx_24110017.v
+++ npc/vsrc/SRAM_IFU_ysyx_24110017.v
@@ -70,8 +70,9 @@ always @(posedge clk) begin
 						/***DELAY_TEST_RAND***/
 						if(S_AXI_ARVALID && !S_AXI_ARREADY) begin
 				      if(delay_counter == 0) begin
-								current_delay <= rand_delay;
-								delay_counter <= current_delay;
+								//current_delay <= rand_delay;
+								//delay_counter <= current_delay;
+								delay_counter <= rand_delay;
 							end
 							else if(delay_counter == 1) begin
 								axi_arready <= 1;
