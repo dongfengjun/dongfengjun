c314f3f292d0357d678fefbdc830f5d01d47dd9c
tracer-ysyx <tracer@ysyx.org>
2025-03-11 20:31:27 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:31:27 up  4:06,  1 user,  load average: 0.00, 0.08, 0.09

---
+++ npc/obj_dir/Vtop_ysyx_24110017__Dpi.h
@@ -0,0 +1,32 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Prototypes for DPI import and export functions.
+//
+// Verilator includes this file in all generated .cpp files that use DPI functions.
+// Manually include this file where DPI .c import functions are declared to ensure
+// the C functions match the expectations of the DPI imports.
+
+#ifndef VERILATED_VTOP_YSYX_24110017__DPI_H_
+#define VERILATED_VTOP_YSYX_24110017__DPI_H_  // guard
+
+#include "svdpi.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+    // DPI EXPORTS
+    // DPI export at vsrc/top_ysyx_24110017.v:148:14
+    extern int csr_display(int i);
+    // DPI export at vsrc/RegisterFile_ysyx_24110017.v:22:14
+    extern int gpr_reg_display(int addr);
+
+    // DPI IMPORTS
+    // DPI import at vsrc/IDU_ysyx_24110017.v:159:30
+    extern void npc_trap();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // guard
