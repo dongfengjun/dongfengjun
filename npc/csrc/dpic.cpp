b8dc4632b4769a5ce1311f85f600083aceb01e82
tracer-ysyx <tracer@ysyx.org>
2025-03-13 17:39:06 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:39:06 up  4:33,  1 user,  load average: 0.14, 0.21, 0.17

--- npc/csrc/dpic.cpp
+++ npc/csrc/dpic.cpp
@@ -51,7 +51,6 @@ void pmem_write(int waddr, int wdata, char wmask) {
 #ifdef CONFIG_MTRACE
 		mtrace_p += sprintf(mtrace_p, "waddr:%08x write:%08x\n", waddr, wdata);
 #endif
-/***
 #ifdef CONFIG_DEVICE
 	if(waddr == 0xa00003f8) {
 		#ifdef CONFIG_DIFFTEST
@@ -62,7 +61,6 @@ void pmem_write(int waddr, int wdata, char wmask) {
 		return;
 	}
 #endif
-***/
 	switch(wmask) {
 		case 1:	paddr_write(waddr, 1, wdata); break;
 		case 3: paddr_write(waddr, 2, wdata); break;
