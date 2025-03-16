138c1ffa031979c6ac9f493d193de07cbce7ccfc
tracer-ysyx <tracer@ysyx.org>
2025-03-16 13:36:05 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:36:05 up 26 min,  1 user,  load average: 0.06, 0.06, 0.08

--- npc/csrc/dpic.cpp
+++ npc/csrc/dpic.cpp
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <iostream>
-#include "Vysyx_24110017.h"
+#include "VReg_ysyx_24110017.h"
 #include "verilated.h"
 #include "svdpi.h"
-#include "Vysyx_24110017__Dpi.h"
+#include "VReg_ysyx_24110017__Dpi.h"
 #include "./include/common.h"
 
 #ifdef CONFIG_MTRACE
