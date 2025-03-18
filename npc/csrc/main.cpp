69be99aefd055eeba28deddc7f52716f4caee64a
tracer-ysyx <tracer@ysyx.org>
2025-03-18 11:43:15 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:43:15 up  1:40,  1 user,  load average: 0.15, 0.16, 0.12

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -17,14 +17,14 @@ VysyxSoCFull* top = NULL;	//实例化指针
 VerilatedVcdC *tfp=	NULL;	//VCD对象指针
 /***DPI-C***/
 word_t gpr_regs_display(int raddr) {
-  extern int gpr_reg_display(int addr);
-  svSetScope(svGetScopeFromName("TOP.ysyxSoCFull"));
-  return gpr_reg_display(raddr);
+  //extern int gpr_reg_display(int addr);
+  //svSetScope(svGetScopeFromName("TOP.ysyxSoCFull"));
+  //return gpr_reg_display(raddr);
 }
 word_t csrs_display(int i) {
-  extern int csr_display(int i);
-  svSetScope(svGetScopeFromName("TOP.ysyxSoCFull"));
-  return csr_display(i);
+  //extern int csr_display(int i);
+  //svSetScope(svGetScopeFromName("TOP.ysyxSoCFull"));
+  //return csr_display(i);
 }
 
 bool RUNNING;
