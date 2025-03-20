a2946233283bac391dac6d88fcccfa74de778820
tracer-ysyx <tracer@ysyx.org>
2025-03-20 14:48:57 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:48:57 up  4:01,  1 user,  load average: 0.05, 0.03, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -14,7 +14,7 @@ extern "C" void flash_read(int32_t addr, int32_t *data) {
 	assert(0);
 }
 extern "C" void mrom_read(int32_t addr, int32_t *data) {
-	*data = 0b00000000000100000000000001110011;
+	*data = paddr_read(addr,1);
 }
 
 VerilatedContext* contextp = NULL;	//verilator指针
