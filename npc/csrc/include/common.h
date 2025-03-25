41c03cb18f156e29d46aa20b9576bfea9e26168d
tracer-ysyx <tracer@ysyx.org>
2025-03-25 20:00:37 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:00:37 up  4:48,  1 user,  load average: 1.04, 0.69, 0.77

--- npc/csrc/include/common.h
+++ npc/csrc/include/common.h
@@ -27,7 +27,7 @@ typedef uint16_t ioaddr_t;
 
 #define CONFIG_DUMP_WAVE 1//是否保存波形（内存不足）
 
-#define CONFIG_TRACE 1
+//#define CONFIG_TRACE 1
 //	#ifdef CONFIG_TRACE
 		//#define CONFIG_ITRACE 1
 			//#define CONFIG_ITRACE_COND 1
