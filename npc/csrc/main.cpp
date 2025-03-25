50ceecbaec1af4a94b982005fdf2869dac783a92
tracer-ysyx <tracer@ysyx.org>
2025-03-25 16:53:51 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:53:51 up  3:20,  1 user,  load average: 0.17, 0.16, 0.11

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -39,6 +39,7 @@ void npc_trap() {
   int a0 = gpr_regs_display(10);//抓取a0
   char str[15];
   //Log("npc: %s at pc = " FMT_WORD, (a0 == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) : ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED)), top->pc);
+	Log("npc: %s", (a0 == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) : ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED)));
 	RUNNING = false;
 }
 
