edd019761f45474533e56c18af623357c2c466a4
tracer-ysyx <tracer@ysyx.org>
2025-03-25 22:29:19 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:29:19 up  7:17,  1 user,  load average: 0.07, 0.03, 0.00

--- npc/csrc/difftest/dut.c
+++ npc/csrc/difftest/dut.c
@@ -43,7 +43,6 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
 	
 	ref_difftest_init(port);//调用ref_init
 	ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), img_size, DIFFTEST_TO_REF);//from mymem to ref IM
-	printf("aaa\n");
 	ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);//reg from myreg to ref
 }
 
