aeb727875c23d889182a200c8c60184cdbbb8873
tracer-ysyx <tracer@ysyx.org>
2025-03-26 14:38:11 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:38:11 up  5:04,  1 user,  load average: 0.02, 0.03, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -31,6 +31,11 @@ word_t csrs_display(int i) {
   svSetScope(svGetScopeFromName("TOP.ysyxSoCFull.asic.cpu.cpu"));
   return csr_display(i);
 }
+word_t dpic_dnpc() {
+	extern int diff_dnpc();
+  svSetScope(svGetScopeFromName("TOP.ysyxSoCFull.asic.cpu.cpu"));
+	retrun diff_dnpc();
+}
 /***END***/
 
 bool RUNNING;
@@ -267,7 +272,7 @@ void cpu_exec(int n) {
 	uint64_t timer_start = get_time();	
 	while(RUNNING && n != 0) {
 		single_cycle();
-		cpu.pc = diff_dnpc();
+		cpu.pc = dpic_dnpc();
 		isa_gpr_push();
 		g_nr_guest_inst++;
 #ifdef CONFIG_ITRACE
@@ -302,8 +307,6 @@ int main(int argc, char *argv[]) {
 	RUNNING = true;
 /***DPIC-DIFFTEST***/
 	extern int diff_pc();
-	svSetScope(svGetScopeFromName("TOP.ysyxSoCFull.asic.cpu.cpu"));
-	extern int diff_dnpc();
 	svSetScope(svGetScopeFromName("TOP.ysyxSoCFull.asic.cpu.cpu"));
 	extern int diff_inst();
 	svSetScope(svGetScopeFromName("TOP.ysyxSoCFull.asic.cpu.cpu"));
