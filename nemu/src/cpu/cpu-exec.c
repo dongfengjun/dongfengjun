47ef155555277c0ae0a17482ef146697c0bda3d1
tracer-ysyx <tracer@ysyx.org>
2025-03-26 11:41:14 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:41:14 up  2:07,  1 user,  load average: 0.05, 0.05, 0.07

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -48,7 +48,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 static void exec_once(Decode *s, vaddr_t pc) {
   s->pc = pc;
   s->snpc = pc;
-	uint32_t *inst = &s->isa.inst.val;
+	uint32_t inst = paddr(pc);
 	printf("pc = %08x inst = %08x\n", pc, inst);
   isa_exec_once(s);
   cpu.pc = s->dnpc;
