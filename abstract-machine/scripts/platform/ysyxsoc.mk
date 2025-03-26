e112c16e305f6782c63369c7c2efe222494d225b
tracer-ysyx <tracer@ysyx.org>
2025-03-26 15:57:36 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:57:36 up  6:23,  1 user,  load average: 0.20, 0.21, 0.11

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -13,7 +13,6 @@ LDFLAGS   += -T $(AM_HOME)/scripts/linker-ysyxsoc.ld \
 						 --defsym=_pmem_start=0x20000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
-NPCFLAGS += -b  #批处理
 .PHONY: $(AM_HOME)/am/src/riscv/ysyxsoc/trm.c
 
 image: $(IMAGE).elf
