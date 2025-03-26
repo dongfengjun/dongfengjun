5cc1de17985114c77a66b80e2b61552a71df7044
tracer-ysyx <tracer@ysyx.org>
2025-03-26 14:59:41 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:59:41 up  5:25,  1 user,  load average: 0.13, 0.06, 0.01

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -267,7 +267,7 @@ void cpu_exec(int n) {
 	uint64_t timer_start = get_time();	
 	while(RUNNING && n != 0) {
 		single_cycle();
-		cpu.pc = top->asic->cpu->cpu->dnpc;
+		cpu.pc = diff_dnpc();
 		isa_gpr_push();
 		g_nr_guest_inst++;
 #ifdef CONFIG_ITRACE
