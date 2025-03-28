a06cb87073bc77458a93a5cd4007b9b6dee35dc3
tracer-ysyx <tracer@ysyx.org>
2025-03-28 18:33:09 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:33:09 up  1:01,  1 user,  load average: 0.03, 0.01, 0.00

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -30,24 +30,49 @@ int atoi(const char* nptr) {
   return x;
 }
 
+struct block_header {
+	size_t size;
+	int is_free;
+	struct block_header* next;
+};
+
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-	static char *hbrk = NULL;
-	if(hbrk == NULL) {
-		hbrk = (void *)ROUNDUP(heap.start, 8);
-	}
+	if(size == 0) return NULL;
 	size = (size_t)ROUNDUP(size, 8);
+	size_t total_size = sizeof(struct block_header) + size;
+
+	struct block_header* current = (struct block_header*)heap.start;
+	struct block_header* prev = NULL;
+
+	while ((void*)current < heap.end) {
+		if(current->is_free && (current->size >= size)) {
+			struct block_header* new_block = (struct block_header*)((char*)current + total_size);
+			new_block->size = current->size - total_size;
+			new_block->is_free = 1;
+			new_block->next = current->next;
+
+			current->size = size;
+			current->is_free = 0;
+			current->next = new_block;
+		}
+		prev = current;
+		current = current->next;
+		return (void*)prev;
+	}
+	return NULL;
+/***
 	char *old = hbrk;
 	hbrk += size;
 	assert((uintptr_t)heap.start <= (uintptr_t)hbrk && (uintptr_t)hbrk < (uintptr_t)heap.end);
 	for(uint64_t *p = (uint64_t *)old; p != (uint64_t *)hbrk; p ++) {
 		*p = 0;
 	}
-	//assert((uintptr_t)hbrk - (uintptr_t)heap.start <= MAX_MALLOC);//越界
 	return old;
+***/
 }
 #endif
 
