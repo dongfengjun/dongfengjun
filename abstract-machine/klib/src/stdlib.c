14f08720df8baeebd7c680314a928a4ddd0b7782
tracer-ysyx <tracer@ysyx.org>
2025-03-28 15:56:31 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:56:31 up  3:35,  1 user,  load average: 0.00, 0.00, 0.00

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -35,7 +35,7 @@ void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-	char *hbrk = NULL;
+	static char *hbrk = NULL;
 	if(hbrk == NULL) {
 		hbrk = (void *)ROUNDUP(heap.start, 8);
 	}
