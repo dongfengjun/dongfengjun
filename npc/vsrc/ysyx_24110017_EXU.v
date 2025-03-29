977fc1356d1c4dc9efdc2614efb409f0e16029bc
tracer-ysyx <tracer@ysyx.org>
2025-03-29 20:29:08 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:29:08 up  7:33,  1 user,  load average: 0.22, 0.13, 0.07

--- npc/vsrc/ysyx_24110017_EXU.v
+++ npc/vsrc/ysyx_24110017_EXU.v
@@ -275,6 +275,8 @@ assign ls_valid = (op == 7'b0000011 || op == 7'b0100011) ? 1'b1 : 1'b0;
 assign ls_wen = (op == 7'b0100011) ? 1'b1 : 1'b0;
 assign ls_waddr = (op == 7'b0100011) ? (r1 + offset) : 32'h80000000;
 assign ls_wdata = (op == 7'b0100011) ? r2 : 32'b0;
+assign ls_wmask = (op == 7'b0100011 && funct3 == 3'b000) ? 4'b0001 : (op == 7'b0100011 && funct3 == 3'b001) ? 4'b0011 : (op == 7'b0100011 && funct3 == 3'b010) ? 4'b1111 : 4'b0000;
+/***
 assign ls_wmask = 
 ((ls_waddr%4 == 0) && op == 7'b0100011 && funct3 == 3'b000) ? 4'b0001 : ((ls_waddr%4 == 0) && op == 7'b0100011 && funct3 == 3'b001) ? 4'b0011 : ((ls_waddr%4 == 0) && op == 7'b0100011 && funct3 == 3'b010) ? 4'b1111 //对齐访问
  : 
@@ -284,6 +286,7 @@ assign ls_wmask =
  :
  ((ls_waddr%4 == 3) && op == 7'b0100011 && funct3 == 3'b000) ? 4'b1000 : ((ls_waddr%4 == 3) && op == 7'b0100011 && funct3 == 3'b001) ? 4'b1000 : ((ls_waddr%4 == 3) && op == 7'b0100011 && funct3 == 3'b010) ? 4'b1000 //单次非对齐
  : 4'b0;
+ ***/
 assign ls_raddr = (op == 7'b0000011) ? (r1 + offset) : 32'h80000000;
 /***J_B_dnpc***/
 wire [31:0]pc;
