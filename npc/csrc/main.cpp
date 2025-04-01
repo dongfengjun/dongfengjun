89df3172638cee91e682c7e827e043e4192270dc
tracer-ysyx <tracer@ysyx.org>
2025-04-01 12:54:12 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:54:12 up  1:29,  1 user,  load average: 0.12, 0.05, 0.01

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -300,7 +300,7 @@ extern char eMA;
 extern char _data;
 void bootloader() {
 	uintptr_t* MA = (uintptr_t *)_MA;
-	printf("MA addr:%d\n", MA);
+	printf("MA addr:%ln\n", MA);
 }
 
 int main(int argc, char *argv[]) {
