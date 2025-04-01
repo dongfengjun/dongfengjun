f21084268eeffa8f79a937f478b81128ebe084ea
tracer-ysyx <tracer@ysyx.org>
2025-04-01 15:50:13 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:50:13 up  4:25,  1 user,  load average: 0.15, 0.06, 0.02

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -308,7 +308,7 @@ void bootloader(void) {
 	memset(&_bss_start, 0, bss_len);
 }
 ***/
-extern char _heap_start;
+extern uint32_t TEST[];
 
 int main(int argc, char *argv[]) {
 /***inst***/
@@ -324,8 +324,8 @@ int main(int argc, char *argv[]) {
 	init_monitor(argc, argv);//load inst
 	//bootloader();//MROM -> SRAM
 	reset(2);
-	char v = _heap_start;
-	printf("%c\n", v);
+	uint32_t test = TEST;
+	printf("%d\n", test);
 #ifdef CONFIG_TARGET_AM
   cpu_exec(-1);
 #else
