c68f36290e9ec44a7932307bc080375fd4c37916
tracer-ysyx <tracer@ysyx.org>
2025-04-01 15:15:50 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:15:50 up  3:51,  1 user,  load average: 0.02, 0.05, 0.02

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -295,16 +295,16 @@ void cpu_exec(int n) {
 #endif
 }
 
-extern uint8_t _data_lma_start[];
-extern uint8_t _data_vma_start[];
-extern uint8_t _data_vma_end[];
-extern uint8_t _bss_start[];
-extern uint8_t _bss_end[];
+extern uint32_t _data_lma_start[];
+extern uint32_t _data_vma_start[];
+extern uint32_t _data_vma_end[];
+extern uint32_t _bss_start[];
+extern uint32_t _bss_end[];
 void bootloader(void) {
 	size_t data_len = _data_vma_end - _data_lma_start;
-	memcpy(&_data_vma_start, &_data_lma_start, data_len);
+	memcpy((void*)&_data_vma_start, (void*)&_data_lma_start, data_len);
 	size_t bss_len = _bss_end - _bss_start;
-	memset(&_bss_start, 0, bss_len);
+	memset((void*)&_bss_start, 0, bss_len);
 }
 
 int main(int argc, char *argv[]) {
