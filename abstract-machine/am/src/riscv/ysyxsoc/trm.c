d1621505fb9a087de12d4eec2c3380733d9a6c85
tracer-ysyx <tracer@ysyx.org>
2025-04-08 19:51:38 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:51:38 up  1:40,  1 user,  load average: 0.23, 0.08, 0.02

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -32,10 +32,10 @@ extern char _data_vma_end[];
 extern char _bss_start[];
 extern char _bss_end[];
 void bootloader() {
-	size_t data_len = _data_vma_end - _data_vma_start;
+	int data_len = _data_vma_end - _data_vma_start;
 	printf("data_len = %d\n", data_len);
 	memcpy(_data_vma_start, _data_lma_start, data_len);
-	size_t bss_len = _bss_end - _bss_start;
+	int bss_len = _bss_end - _bss_start;
 	printf("bss_len = %d\n", bss_len);
 	memset(_bss_start, 1, bss_len);
 }
