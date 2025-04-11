c612fa45d73f15c30999f0592a60b190de23a145
tracer-ysyx <tracer@ysyx.org>
2025-04-11 16:20:08 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:20:08 up  2:05,  1 user,  load average: 0.06, 0.01, 0.00

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -26,26 +26,26 @@ void halt(int code) {
 	while (1);
 }
 
-extern char _data_lma_start;
-extern char _data_vma_start;
-extern char _data_vma_end;
-extern char _bss_start;
-extern char _bss_end;
-void bootloader() {
-	size_t data_len = &_data_vma_end - &_data_vma_start;
-	if(&_data_vma_end == &_data_vma_start) {
-		halt(1);
-	}
-	memcpy(&_data_vma_start, &_data_lma_start, data_len);
-	size_t bss_len = &_bss_end - &_bss_start;
-	if(bss_len == 0) {                                                       
-    halt(1);
-  }
-	memset(&_bss_start, 1, bss_len);
-}
+//extern char _data_lma_start;
+//extern char _data_vma_start;
+//extern char _data_vma_end;
+//extern char _bss_start;
+//extern char _bss_end;
+//void bootloader() {
+//	size_t data_len = &_data_vma_end - &_data_vma_start;
+//	if(&_data_vma_end == &_data_vma_start) {
+//		halt(1);
+//	}
+//	memcpy(&_data_vma_start, &_data_lma_start, data_len);
+//	size_t bss_len = &_bss_end - &_bss_start;
+//	if(bss_len == 0) {
+//    halt(1);
+//  }
+//	memset(&_bss_start, 1, bss_len);
+//}
 
 void _trm_init() {
-	bootloader();
+//	bootloader();
 	int ret = main(mainargs);
 	halt(ret);
 }
