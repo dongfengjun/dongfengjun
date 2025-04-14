57aac79da1ab02979fa9b2f3a072b3972e9c6686
tracer-ysyx <tracer@ysyx.org>
2025-04-14 16:04:07 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:04:07 up  2:40,  1 user,  load average: 0.16, 0.03, 0.01

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -26,26 +26,20 @@ void halt(int code) {
 	while (1);
 }
 
-//extern char _data_lma_start;
-//extern char _data_vma_start;
-//extern char _data_vma_end;
-//extern char _bss_start;
-//extern char _bss_end;
-//void bootloader() {
-//	size_t data_len = &_data_vma_end - &_data_vma_start;
-//	if(&_data_vma_end == &_data_vma_start) {
-//		halt(1);
-//	}
-//	memcpy(&_data_vma_start, &_data_lma_start, data_len);
-//	size_t bss_len = &_bss_end - &_bss_start;
-//	if(bss_len == 0) {
-//    halt(1);
-//  }
-//	memset(&_bss_start, 1, bss_len);
-//}
+extern char data_vma_start[];
+extern char data_lma_start[];
+extern char data_size[];
+void bootloader(void) {
+	if(&data_vma_start != &data_lma_start) {
+		memcpy(data_vma_start, data_lma_start, (size_t)data_size);
+	}
+	else {
+		putch('a');
+	}
+}
 
 void _trm_init() {
-//	bootloader();
+	bootloader();
 	int ret = main(mainargs);
 	halt(ret);
 }
