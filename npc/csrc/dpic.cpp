d12db5f2a01822eb23b2252b49bb764adf283b5f
tracer-ysyx <tracer@ysyx.org>
2025-04-24 22:03:46 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:03:46 up  7:51,  1 user,  load average: 0.05, 0.20, 0.17

--- npc/csrc/dpic.cpp
+++ npc/csrc/dpic.cpp
@@ -49,7 +49,7 @@ void pmem_write(int waddr, int wdata, char wmask) {
   // 总是往地址为`waddr & ~0x3u`的4字节按写掩码`wmask`写入`wdata`
   // `wmask`中每比特表示`wdata`中1个字节的掩码,
   // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
-	waddr = waddr & 0xfffffffc;
+	waddr = waddr & ~0x3u;
 #ifdef CONFIG_MTRACE
 		mtrace_p += sprintf(mtrace_p, "waddr:%08x write:%08x\n", waddr, wdata);
 #endif
