665b3c68e94f233c4321957314f9ab0254bac2d2
tracer-ysyx <tracer@ysyx.org>
2025-04-25 22:10:27 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:10:27 up  6:16,  1 user,  load average: 0.02, 0.02, 0.00

--- npc/csrc/memory/flash.c
+++ npc/csrc/memory/flash.c
@@ -2,7 +2,7 @@
 
 uint8_t flash[0x10000000] = {};
 
-uint8_t* flash_to_host(int addr) { return flash + addr - 0x30000000; }
+uint8_t* flash_to_host(paddr_t paddr) { return flash + paddr - 0x30000000; }
 
 uint32_t flash_host_read(void *addr, int len) {
   switch (len) {
