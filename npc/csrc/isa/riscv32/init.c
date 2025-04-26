480d66b87356315ceae5188b0fea5692f8a5f07b
tracer-ysyx <tracer@ysyx.org>
2025-04-26 14:58:35 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:58:35 up 51 min,  1 user,  load average: 0.03, 0.04, 0.06

--- npc/csrc/isa/riscv32/init.c
+++ npc/csrc/isa/riscv32/init.c
@@ -20,7 +20,7 @@ static void restart() {
 
 void init_isa() {
   /* Load built-in image. */
-  memcpy(flash_to_host(0x30000000), img, sizeof(img));
+  memcpy(flash_to_host(0x0), img, sizeof(img));
 
   /* Initialize this virtual computer system. */
   restart();
