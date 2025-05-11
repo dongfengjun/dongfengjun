e935eb5eda2264a7f2a68b8c0b8eb01bd7787f0f
tracer-ysyx <tracer@ysyx.org>
2025-05-11 15:59:00 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:59:00 up  1:55,  1 user,  load average: 0.00, 0.00, 0.00

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -56,8 +56,8 @@ void uart_init(void) {
 //am flash_read
 #define SPI_BASE 0X10001000
 uint32_t flash_read(uint32_t addr) {
-  uint32_t *Tx = (uint32_t *)SPI_BASE;                                      
-	Tx[0] = 0x01020304;
+  uint64_t *Tx = (uint64_t *)SPI_BASE;                                      
+	Tx[0] = 0x0102030400000000;
 	uint32_t *SS = (uint32_t *)(SPI_BASE + 0x18);
   SS[0] = 0b00000001;
   uint32_t *DIVIDER = (uint32_t *)(SPI_BASE + 0x14);
