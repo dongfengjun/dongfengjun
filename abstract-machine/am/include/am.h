60cf0b0b92fce24b681f7c2b90e41dd08a085826
tracer-ysyx <tracer@ysyx.org>
2025-05-11 14:23:22 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:23:22 up 19 min,  1 user,  load average: 0.30, 0.09, 0.09

--- abstract-machine/am/include/am.h
+++ abstract-machine/am/include/am.h
@@ -47,6 +47,7 @@ extern "C" {
 extern   Area        heap;
 void     putch       (char ch);
 void     halt        (int code) __attribute__((__noreturn__));
+uint32_t flash_read(uint32_t addr);
 
 // -------------------- IOE: Input/Output Devices --------------------
 bool     ioe_init    (void);
--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -53,6 +53,24 @@ void uart_init(void) {
 	outb(UART_BASE + 0X3, 0x3);
 }
 
+//am flash_read
+#define SPI_BASE 0X10001000
+uint32_t flash_read(uint32_t addr) {
+  uint32_t *SS = (uint32_t *)(SPI_BASE + 0x18);
+  SS[0] = 0b00000000;
+  uint32_t *DIVIDER = (uint32_t *)(SPI_BASE + 0x14);
+  DIVIDER[0] = 0x1;
+  uint32_t *CTRL = (uint32_t *)(SPI_BASE + 0x10);
+  CTRL[0] = 0b01100010000; //8+8=16 lsb=? 配合sleve
+  while(1) {
+    volatile uint32_t *COMPLETE = (uint32_t *)(SPI_BASE + 0x10);
+    int GO_BSY = COMPLETE[0] & 0x100;
+    if(GO_BSY == 0) break;
+  }
+  uint32_t *Rx = (uint32_t *)SPI_BASE;
+  return Rx[0];
+}
+
 void _trm_init() {
 	bootloader(); //mrom->sram
 	uart_init(); //uart16500 init
