aa4dd6e41a9cc180761f87445cb6f29489f9d05f
tracer-ysyx <tracer@ysyx.org>
2025-05-27 17:43:57 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:43:57 up  4:04,  1 user,  load average: 0.13, 0.09, 0.03

--- npc/vsrc/ysyx_24110017_PCU.v
+++ npc/vsrc/ysyx_24110017_PCU.v
@@ -59,7 +59,7 @@ always @(posedge clk) begin
 	end
 	else begin
 		case (state)
-			IDLE: begini
+			IDLE: begin
 				if(rst && ((dnpc > 32'h30000000) ||(dnpc < 32'h40000000))) begin //判断条件
 					pcu_valid <= 1'b1;
 				end
