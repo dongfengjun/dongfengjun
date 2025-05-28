d0ee030b79b02f1a2a185f75d176a702433e370d
tracer-ysyx <tracer@ysyx.org>
2025-05-28 16:55:51 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:55:51 up  2:33,  1 user,  load average: 0.03, 0.14, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -13,7 +13,6 @@
 extern "C" void flash_read(int32_t addr, int32_t *data) {
 	addr = addr + 0x30000000;
 	*data = pmem_read(addr);
-	printf("addr = %08x data = %08x\n",addr,*data);
 }
 extern "C" void mrom_read(int32_t addr, int32_t *data) {
 	*data = pmem_read(addr);
