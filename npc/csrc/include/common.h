138853fbb281f1bf7de5e8e8b11e647d11ba7fb3
tracer-ysyx <tracer@ysyx.org>
2025-05-28 16:43:23 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:43:23 up  2:21,  1 user,  load average: 0.19, 0.08, 0.05

--- npc/csrc/include/common.h
+++ npc/csrc/include/common.h
@@ -37,8 +37,8 @@ typedef uint16_t ioaddr_t;
 	//#endif
 //#define CONFIG_DIFFTEST 1
 
-#define CONFIG_MBASE 0x20000000//IM
-#define CONFIG_MSIZE 0x1000
+#define CONFIG_MBASE 0x30000000//IM
+#define CONFIG_MSIZE 0x10000000
 #define CONFIG_PC_RESET_OFFSET 0 //reset pc offset
 #define PMEM_LEFT  ((paddr_t)CONFIG_MBASE)
 #define PMEM_RIGHT ((paddr_t)CONFIG_MBASE + CONFIG_MSIZE - 1)
--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -11,7 +11,7 @@
 
 /***ysyxSoC***/
 extern "C" void flash_read(int32_t addr, int32_t *data) {
-	*data = c_flash_read(addr);
+	*data = pmem_read(addr);
 }
 extern "C" void mrom_read(int32_t addr, int32_t *data) {
 	*data = pmem_read(addr);
