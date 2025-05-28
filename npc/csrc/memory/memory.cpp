96d60cfacd901720ac8df978c275a3de6528101f
tracer-ysyx <tracer@ysyx.org>
2025-05-28 16:48:12 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:48:12 up  2:26,  1 user,  load average: 0.07, 0.13, 0.09

--- npc/csrc/memory/memory.cpp
+++ npc/csrc/memory/memory.cpp
@@ -47,7 +47,7 @@ static void mem_write(paddr_t addr, int len, word_t data) {
 
 extern CPU_state cpu;
 static void out_of_bound(paddr_t addr) {
-	panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD, addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
+//	panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD, addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
 }
 
 void init_mem() {
