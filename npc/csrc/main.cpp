2acbabdfc6b3ccc3e8ffb65a145daaea0ef66b91
tracer-ysyx <tracer@ysyx.org>
2025-06-01 20:09:02 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:09:02 up  4:23,  1 user,  load average: 0.18, 0.20, 0.15

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -19,10 +19,10 @@ extern "C" void mrom_read(int32_t addr, int32_t *data) {
 extern "C" void psram_read(int32_t addr, int32_t *data) {
 	*data = c_psram_read(addr);
 }
-extern "C" void psram_write(int32_t addr, int32_t data) {
-	printf("a\n");
-	c_psram_write(addr,data);
-}
+//extern "C" void psram_write(int32_t addr, int32_t data) {
+//	printf("a\n");
+//	c_psram_write(addr,data);
+//}
 
 VerilatedContext* contextp = NULL;	//verilator指针
 VysyxSoCFull* top = NULL;	//实例化指针
