9283d35598e313e14342b08d47d6cc63aca9cfdf
tracer-ysyx <tracer@ysyx.org>
2025-06-01 20:01:42 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:01:42 up  4:16,  1 user,  load average: 0.01, 0.06, 0.08

--- npc/csrc/memory/psram.c
+++ npc/csrc/memory/psram.c
@@ -31,7 +31,7 @@ static uint8_t* guest_to_psram(paddr_t paddr) { return psram + paddr; }
 static paddr_t psram_to_guest(uint8_t *haddr) { return haddr - psram; }
 
 int c_psram_read(int addr) {
-  if (likely(in_psram(addr))) return psram_host_read(guest_to_psram(addr), 4);
+  if (likely(in_psram(addr))) return host_read(guest_to_psram(addr), 4);
 	for(int i = 0; i < 3; i ++){
 		printf("%08x ", psram[i * 4]);
 	}
@@ -39,8 +39,9 @@ int c_psram_read(int addr) {
 }
 
 void c_psram_write(int addr, int data) {
-  if (likely(in_psram(addr))) { 
-		psram_host_write(guest_to_psram(addr), 4, data);
+  printf("addr = %08x wdata = %08x\n",addr,data);
+	if (likely(in_psram(addr))) { 
+		host_write(guest_to_psram(addr), 4, data);
 		for(int i = 0; i < 3; i ++){
 			printf("%08x ", psram[i * 4]);
 		}
