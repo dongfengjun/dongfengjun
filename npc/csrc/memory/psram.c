d640907ed607cc5af0b6ed0fe0e2b586e5f1e159
tracer-ysyx <tracer@ysyx.org>
2025-06-03 17:34:10 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:34:10 up  3:21,  1 user,  load average: 0.02, 0.07, 0.08

--- npc/csrc/memory/psram.c
+++ npc/csrc/memory/psram.c
@@ -32,6 +32,7 @@ int c_psram_read(int addr) {
 
 void c_psram_write(int addr, int data) {
 	printf("waddr:%08x wdata:%08x\n",addr,data);
+	printf("psram:%08x %08x %08x %08x %08x %08x",psram[0],psram[4],psram[8],psram[0xc],psram[0x10],psram[0x14]);
 	host_write(guest_to_psram(addr), 4, data);
 	return;
 }
