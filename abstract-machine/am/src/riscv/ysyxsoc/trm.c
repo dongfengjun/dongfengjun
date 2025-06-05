ffc63b937a77af6972a9370b0daa833434eb66a3
tracer-ysyx <tracer@ysyx.org>
2025-06-05 21:02:17 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:02:17 up  5:41,  1 user,  load average: 0.28, 0.22, 0.13

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -20,7 +20,7 @@ static const char mainargs[] = MAINARGS;
 
 #define UART_BASE 0X10000000
 void putch(char ch) {
-	while(!(inl(UART_BASE + 0X5) & 0b00100000));
+	while(!(inl(UART_BASE + 0X5) & 0x40));
 	outl(UART_BASE, ch);
 }
 
