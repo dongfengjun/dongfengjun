0210f8999641150f6da1e44e3c6d5a5f7d5639e2
tracer-ysyx <tracer@ysyx.org>
2025-06-07 11:44:28 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:44:28 up  2:15,  1 user,  load average: 0.31, 0.23, 0.11

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -87,8 +87,8 @@ uint32_t flash_read(uint32_t addr) {
 }
 
 void _trm_init() {
-	//uart_init(); //uart16500 init + difftest_skip_ref
-	//bootloader(); //mrom->sram
+	uart_init(); //uart16500 init + difftest_skip_ref
+	bootloader(); //mrom->sram
 	int ret = main(mainargs);
 	halt(ret);
 }
