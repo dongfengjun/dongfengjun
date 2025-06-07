4a225d20c7b53ad730f22389af6dc90b36c28e3d
tracer-ysyx <tracer@ysyx.org>
2025-06-07 21:23:32 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:23:32 up  4:20,  1 user,  load average: 0.51, 0.49, 0.35

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -51,7 +51,7 @@ void bootloader(void) {
 			memset(bss_vma_start, 0, (size_t)data_size);
 		}
 	}
-	uint32_t main_saddr = (uint32_t)&main - 0x30000010 + 0x0f000000;
+	uint32_t main_saddr = (uint32_t)&main - 0x30000010 + 0x0f000000 + (size_t)rodata_size;
 	if((size_t)text_size != 0 && ((size_t)text_size < 0x2000)) {
 		memcpy((void *)main_saddr, text, (size_t)text_size);
 		if((size_t)rodata_size != 0 && ((size_t)rodata_size < 0x2000)) {
