a92ee06f8a50ba399c45d0583e295523e0c23b71
tracer-ysyx <tracer@ysyx.org>
2025-06-07 10:08:40 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:08:40 up 39 min,  1 user,  load average: 0.06, 0.38, 0.40

--- npc/vsrc/ysyx_24110017_PCU.v
+++ npc/vsrc/ysyx_24110017_PCU.v
@@ -61,10 +61,12 @@ always @(posedge clk) begin
 	else begin
 		case (state)
 			IDLE: begin
-				if((dnpc >= 32'h30000000) && (dnpc < 32'h40000000)) begin //判断条件
+				if(((dnpc >= 32'h30000000) && (dnpc < 32'h40000000))
+					 || ((dnpc >= 32'h0f000000) && (dnpc < 32'h0f002000))) begin 
 					pcu_valid <= 1'b1;
 				end
-				if((pc < 32'h30000000) || (pc >= 32'h40000000)) begin
+				if(((pc < 32'h30000000) || (pc >= 32'h40000000)) 
+					&& ((pc < 32'h0f000000) || (pc >= 32'h0f002000))) begin
 					$fwrite(32'h80000002, "Assertion failed: Invalid PC `%xh`\n",pc);
           $fatal;
 				end
