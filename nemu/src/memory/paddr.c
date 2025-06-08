a28cd19cd9a2e8e856d863ea2b8aad77face1900
tracer-ysyx <tracer@ysyx.org>
2025-06-08 20:10:14 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:10:14 up  5:16,  1 user,  load average: 0.31, 0.19, 0.22

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -55,6 +55,7 @@ word_t paddr_read(paddr_t addr, int len) {
 	if (addr - 0x30000000 < 0x10000000) return flash_read(addr, len);//ysyxsoc-flash
 	if (addr - 0x0f000000 < 0x2000) return sram_read(addr, len);//ysyxsoc-sram
 	if (addr - 0x80000000 < 0x4000000) return psram_read(addr, len);//ysyxsoc-psram
+	if (addr - 0x10000000 < 0x1000) return 0; //ysyxsoc-uart
 #endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
 	IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
@@ -67,7 +68,7 @@ void paddr_write(paddr_t addr, int len, word_t data) {
 	if (addr - 0x30000000 < 0x10000000) return init_flash(addr, len, data);
 	if (addr - 0x0f000000 < 0x2000) return sram_write(addr, len, data);
 	if (addr - 0x80000000 < 0x4000000) return psram_write(addr, len, data);
-	if (addr - 0x10000000 < 0x1000) return;	//ysyxsoc-uart 
+	if (addr - 0x10000000 < 0x1000) return; 
 #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
 	IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
