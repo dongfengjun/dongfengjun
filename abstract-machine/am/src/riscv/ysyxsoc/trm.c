24f7ad04a8726045632d15249b1f88a791605131
tracer-ysyx <tracer@ysyx.org>
2025-06-08 15:33:54 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:33:54 up 40 min,  1 user,  load average: 0.00, 0.02, 0.05

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -30,6 +30,7 @@ void halt(int code) {
 }
 
 extern char text[];
+extern char text_lma_start[];
 extern char text_size[];
 extern char rodata_vma_start[];
 extern char rodata_lma_start[];
@@ -51,15 +52,14 @@ void bootloader(void) {
 			memset(bss_vma_start, 0, (size_t)data_size);
 		}
 	}
-	uint32_t main_saddr = (uint32_t)&main - 0x30000010 + 0x0f000000;
 	if((size_t)text_size != 0 && ((size_t)text_size < 0x2000)) {
-		memcpy((void *)0x0f000000, (void *)0x30000000, (size_t)text_size);
+		memcpy((void *)0x0f000000, text_lma_start, (size_t)text_size);
 		if((size_t)rodata_size != 0 && ((size_t)rodata_size < 0x2000)) {
 			if(&rodata_vma_start != &rodata_lma_start) {
 				memcpy(rodata_vma_start, rodata_lma_start, (size_t)rodata_size);
 			}
 		}
-		asm volatile ("jalr %0" : : "r" (main_saddr));
+		asm volatile ("jalr %0" : : "r" (0x0f000000));
 	}
 }
 
