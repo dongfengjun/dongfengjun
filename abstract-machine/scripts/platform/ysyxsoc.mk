e2cef2842c34ee44f0c9de792e11efe8cebf3eaa
tracer-ysyx <tracer@ysyx.org>
2025-06-09 19:27:06 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:27:06 up  5:03,  1 user,  load average: 0.01, 0.05, 0.01

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -9,8 +9,8 @@ AM_SRCS := riscv/ysyxsoc/start.S \
            platform/dummy/mpe.c
 
 CFLAGS    += -fdata-sections -ffunction-sections
-LDFLAGS   += -T $(AM_HOME)/scripts/linker-ysyxsoc.ld \
-						 --wrap=data.extra \
+LDFLAGS   += --default-script=$(AM_HOME)/scripts/linker-ysyxsoc.ld \
+						 --script=$(AM_HOME)/scripts/linker-ysyxsoc-rtt.ld \
 						 --defsym=_pmem_start=0x30000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
