d2f2ab3c306a6a1fc45fc2a963ca5dd1c965f8b0
tracer-ysyx <tracer@ysyx.org>
2025-06-10 18:28:21 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:28:21 up  5:01,  1 user,  load average: 1.08, 1.05, 1.05

--- nemu/src/memory/psram.c
+++ nemu/src/memory/psram.c
@@ -16,6 +16,6 @@ word_t psram_read(paddr_t addr, int len) {
 void psram_write(paddr_t addr, int len, word_t data) {
   host_write(guest_to_host_psram(addr), len, data);
 	if(addr >= 0x8000001C && addr < 0x80000020) {
-    printf("0x8000001C: %02x%02x%02x%02x\n",psram[0x1f],psram[0x1e],psram[0x1d],psram[0x1c]);
+    printf("0x8000001c:%08x %02x%02x%02x%02x\n",addr,psram[0x1f],psram[0x1e],psram[0x1d],psram[0x1c]);
   }
 }
