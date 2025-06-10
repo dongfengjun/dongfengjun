386a5ed808c01e1d59c54d0c6ad7451e7eedffa6
tracer-ysyx <tracer@ysyx.org>
2025-06-10 16:14:39 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:14:39 up  2:47,  1 user,  load average: 1.25, 1.09, 1.03

--- nemu/src/memory/mrom.c
+++ nemu/src/memory/mrom.c
@@ -6,10 +6,12 @@ static uint8_t flash[0x10000000] PG_ALIGN = {};
 static uint8_t* guest_to_host_flash(paddr_t paddr) { return flash + paddr - 0x30000000;}
 
 word_t flash_read(paddr_t addr, int len) {
-  word_t ret = host_read(guest_to_host_flash(addr), len);
+  addr = addr & ~0x3u;
+	word_t ret = host_read(guest_to_host_flash(addr), len);
   return ret;
 }
 
 void init_flash(paddr_t addr, int len, word_t data) {
+	addr = addr & ~0x3u;
   host_write(guest_to_host_flash(addr), len, data);
 }
