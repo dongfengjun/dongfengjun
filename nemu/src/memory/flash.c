ecf44c5a27b095019dd37e0dbf60a3fff8d31064
tracer-ysyx <tracer@ysyx.org>
2025-06-10 16:32:35 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:32:35 up  3:05,  1 user,  load average: 0.92, 0.98, 0.99

--- nemu/src/memory/flash.c
+++ nemu/src/memory/flash.c
@@ -7,7 +7,6 @@ static uint8_t* guest_to_host_flash(paddr_t paddr) { return flash + paddr - 0x30
 
 word_t flash_read(paddr_t addr, int len) {
   addr = addr & ~0x3u;
-	printf("addr = %08x\n",addr);
 	word_t ret = host_read(guest_to_host_flash(addr), len);
   return ret;
 }
