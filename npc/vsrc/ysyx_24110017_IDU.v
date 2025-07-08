44eff4cd4ac19aed8d3f42f3119e7a3444bf0ae3
tracer-ysyx <tracer@ysyx.org>
2025-07-08 13:12:02 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:12:02 up  1:35,  1 user,  load average: 1.18, 1.06, 1.01

--- npc/vsrc/ysyx_24110017_IDU.v
+++ npc/vsrc/ysyx_24110017_IDU.v
@@ -137,7 +137,7 @@ assign rs1 = (op == 7'b1100111 || op == 7'b0000011 || op == 7'b0010011 || op == 
 assign rs2 = (op == 7'b1100011  //B
  || op == 7'b0100011  //S
  || op == 7'b0110011) ? inst[24:20] //R
- : (op == 7'b1110011 && imm == 32'd0 && funct3 == 3'b000) ? 5'd15 //ecall
+ : (op == 7'b1110011 && imm == 32'd0 && funct3 == 3'b000) ? 5'd17//5'd15 //ecall
  : 5'b0;
 assign funct7 = (op == 7'b0110011 || op == 7'b0010011) ? inst[31:25] : 7'b0;
 assign immI = {{20{inst[31]}},inst[31:20]};	//SEXTIimmediate
