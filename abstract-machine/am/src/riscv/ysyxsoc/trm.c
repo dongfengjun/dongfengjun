4496a1b9b927432cdbfc53e8e438d6e20f9926df
tracer-ysyx <tracer@ysyx.org>
2025-07-10 00:36:44 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:36:44 up  8:38,  1 user,  load average: 0.10, 0.12, 0.27

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -9,7 +9,7 @@ extern char _heap_end;
 int main(const char *args);
 
 extern char _pmem_start;
-#define PMEM_SIZE (1 * 1024)
+#define PMEM_SIZE (4 * 1024 * 1024)
 #define PMEM_END ((uintptr_t)&_pmem_start + PMEM_SIZE)
 
 Area heap = RANGE(&_heap_start, &_heap_end);
