f3f2334212387794221dec2aa1d3119965dad00b
tracer-ysyx <tracer@ysyx.org>
2025-07-12 13:40:35 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:40:35 up  3:10,  1 user,  load average: 0.00, 0.04, 0.05

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -109,14 +109,22 @@ void idcsrs_init(void) {
 	putch((mvendorid[0] >> 8) & 0xFF);
 	putch(mvendorid[0] & 0xFF);
 	volatile int32_t *marchid = (int32_t *)(IDCSR_BASE + 0x4);
-  putch(((marchid[0] >> 24) & 0xFF) / 10 + 48);
-	putch(((marchid[0] >> 24) & 0xFF) % 10 + 48);
-  putch(((marchid[0] >> 16) & 0xFF) / 10 + 48);
-	putch(((marchid[0] >> 16) & 0xFF) % 10 + 48);
-  putch(((marchid[0] >> 8) & 0xFF) / 10 + 48);
-	putch(((marchid[0] >> 8) & 0xFF) % 10 + 48);
-  putch((marchid[0] & 0xFF) / 10 + 48);
-	putch((marchid[0] & 0xFF) % 10 + 48);
+  char a = ((marchid[0] >> 24) & 0xFF) / 10;
+	char b = ((marchid[0] >> 24) & 0xFF) % 10;
+  char c = ((marchid[0] >> 16) & 0xFF) / 10;
+	char d = ((marchid[0] >> 16) & 0xFF) % 10;
+  char e = ((marchid[0] >> 8) & 0xFF) / 10;
+	char f = ((marchid[0] >> 8) & 0xFF) % 10;
+  char g = (marchid[0] & 0xFF) / 10;
+	char h = (marchid[0] & 0xFF) % 10;
+	putch(a);
+	putch(b);
+	putch(c);
+  putch(d); 
+	putch(e);
+  putch(f);
+	putch(g);
+  putch(h);
 }
 
 //am flash_read
