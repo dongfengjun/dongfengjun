65a512740375ac84bfb12e4fbd23ce99d82441c3
tracer-ysyx <tracer@ysyx.org>
2025-07-12 12:04:42 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:04:42 up  1:34,  1 user,  load average: 0.16, 0.16, 0.14

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -104,17 +104,17 @@ void uart_init(void) {
 #define IDCSR_BASE 0x01000000
 void idcsrs_init(void) {
   uint32_t mvendorid = inw(IDCSR_BASE);
-	uint8_t *bytes = (uint8_t *)&mvendorid;
-	putch(bytes[0]);
-	putch(bytes[1]);
-	putch(bytes[2]);
-	putch(bytes[3]);
+	uint8_t *bytes0 = (uint8_t *)&mvendorid;
+	putch(bytes0[0]);
+	putch(bytes0[1]);
+	putch(bytes0[2]);
+	putch(bytes0[3]);
   uint32_t marchid = inw(UART_BASE + 0X4);
-	*bytes = (uint8_t *)&marchid;
-  putch(bytes[0]);
-  putch(bytes[1]);
-  putch(bytes[2]);
-  putch(bytes[3]);
+	uint8_t *bytes1 = (uint8_t *)&marchid;
+  putch(bytes1[0]);
+  putch(bytes1[1]);
+  putch(bytes1[2]);
+  putch(bytes1[3]);
 }
 
 //am flash_read
