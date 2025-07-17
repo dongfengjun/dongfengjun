a7754612f237ab201bbb8b48f6172ec69f54607f
tracer-ysyx <tracer@ysyx.org>
2025-07-18 00:09:56 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:09:56 up  1:54,  1 user,  load average: 0.13, 0.10, 0.03

--- abstract-machine/am/src/platform/nemu/ioe/ioe.c
+++ abstract-machine/am/src/platform/nemu/ioe/ioe.c
@@ -17,12 +17,6 @@ void __am_audio_play(AM_AUDIO_PLAY_T *);
 void __am_disk_config(AM_DISK_CONFIG_T *cfg);
 void __am_disk_status(AM_DISK_STATUS_T *stat);
 void __am_disk_blkio(AM_DISK_BLKIO_T *io);
-#include "../../../riscv/riscv.h"
-#define UART_BASE 0X10000000
-void __am_uart_rx(AM_UART_RX_T *rx) {
-  if(!(inb(UART_BASE + 0X5) & 0x01)) { rx->data = 0xff; }
-  else { rx->data = inb(UART_BASE); }
-}
 
 
 static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = true; }
@@ -41,7 +35,6 @@ static void *lut[128] = {
   [AM_GPU_FBDRAW  ] = __am_gpu_fbdraw,
   [AM_GPU_STATUS  ] = __am_gpu_status,
   [AM_UART_CONFIG ] = __am_uart_config,
-	[AM_UART_RX     ] = __am_uart_rx,
   [AM_AUDIO_CONFIG] = __am_audio_config,
   [AM_AUDIO_CTRL  ] = __am_audio_ctrl,
   [AM_AUDIO_STATUS] = __am_audio_status,
