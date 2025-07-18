0f78e62d5585a42d52ee01f2bae780e03fd0b67c
tracer-ysyx <tracer@ysyx.org>
2025-07-18 23:12:43 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:12:43 up  7:33,  1 user,  load average: 0.14, 0.31, 0.27

--- abstract-machine/am/src/riscv/npc/input.c
+++ abstract-machine/am/src/riscv/npc/input.c
@@ -2,9 +2,9 @@
 #include "../riscv.h"
 
 #define KBD_ADDR 0x10011000
-#define KEYDOWN_MASK 0x80
+#define KEYDOWN_MASK 0x8000
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  uint8_t k = inb(KBD_ADDR);
+  uint8_t k = inw(KBD_ADDR);
   if(k & KEYDOWN_MASK) kbd->keydown = true;
   else kbd->keydown = false;
   kbd->keycode = k & ~KEYDOWN_MASK;
