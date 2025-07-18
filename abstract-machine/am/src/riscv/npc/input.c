67591ed9a34db9969374d0cd830e47343a3553a5
tracer-ysyx <tracer@ysyx.org>
2025-07-18 13:57:55 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:57:55 up  3:18,  1 user,  load average: 0.00, 0.00, 0.00

--- abstract-machine/am/src/riscv/npc/input.c
+++ abstract-machine/am/src/riscv/npc/input.c
@@ -1,6 +1,10 @@
 #include <am.h>
 
+#define KBD_ADDR 0x10011000
+#define KEYDOWN_MASK 0x80
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint8_t k = inb(KBD_ADDR);
+  if(k & KEYDOWN_MASK) kbd->keydown = true;
+  else kbd->keydown = false;
+  kbd->keycode = k & ~KEYDOWN_MASK;
 }
