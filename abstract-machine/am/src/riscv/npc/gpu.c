16a479279f3a68bc5a4996576644d6086a312518
tracer-ysyx <tracer@ysyx.org>
2025-07-20 13:13:49 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:13:49 up 33 min,  1 user,  load average: 0.05, 0.05, 0.04

--- abstract-machine/am/src/riscv/npc/gpu.c
+++ abstract-machine/am/src/riscv/npc/gpu.c
@@ -1,9 +1,8 @@
 #include <am.h>
 #include "../riscv.h"
 
-#define VGACTL_ADDR 0x21000000
+#define FB_ADDR 0x21000000
 #define SIZE 0x200000
-#define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
 }
@@ -11,8 +10,8 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = inw(VGACTL_ADDR + 2),
-		.height = inw(VGACTL_ADDR),
+    .width = 640, //nvboard
+		.height = 480, //nvboard
     .vmemsz = 0
   };
 }
@@ -24,12 +23,12 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 	uint32_t *pixels = ctl->pixels;
 	for(int i = ctl->y; i < (ctl->y + ctl->h); i ++) {
 		for(int j = ctl->x; j < (ctl->x + ctl->w); j ++) {
-			fb[inw(VGACTL_ADDR+2) * i + j] = pixels[ctl->w * (i-(ctl->y)) + (j-(ctl->x))];	//w*i+j 
+			fb[640 * i + j] = pixels[ctl->w * (i-(ctl->y)) + (j-(ctl->x))];	//w*i+j 
 		}
 	}
-	if(ctl -> sync) {
-		outl(SYNC_ADDR, 1);
-	}
+//	if(ctl -> sync) {
+//		outl(SYNC_ADDR, 1);
+//	}
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
