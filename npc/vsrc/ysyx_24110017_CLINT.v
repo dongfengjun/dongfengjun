f6560583907892567b37ea22f829c4ed4ec882fb
tracer-ysyx <tracer@ysyx.org>
2025-07-23 19:53:12 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:53:12 up  8:07,  1 user,  load average: 1.34, 1.58, 1.40

--- npc/vsrc/ysyx_24110017_CLINT.v
+++ npc/vsrc/ysyx_24110017_CLINT.v
@@ -68,8 +68,6 @@ assign c_rdata = {32{(C_AXI_ARVALID && C_AXI_ARREADY)}} &
  {32{(C_AXI_ARADDR == DEVICE_CLINT_LOW_ADDR)}} & mtime[31:0] | 
  {32{(C_AXI_ARADDR == DEVICE_CLINT_HIGH_ADDR)}} & mtime[63:32];
 
-import "DPI-C" function void diff_skip_ref();
-
 always @(posedge clk) begin
   if(rst) begin
 		axi_awready <= 0;
@@ -99,7 +97,6 @@ always @(posedge clk) begin
 			axi_araddr <= 32'b0;
 			axi_rvalid <= 0;
 			axi_rdata <= 32'h0;
-//			diff_skip_ref();
 		end
 		if(C_AXI_AWVALID && !C_AXI_AWREADY) begin
 			axi_awready <= 1; //判断条件
