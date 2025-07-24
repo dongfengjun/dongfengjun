a24a4a638bf1b4cdfff7dc0c97ca56da9ac704ce
tracer-ysyx <tracer@ysyx.org>
2025-07-24 12:55:41 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:55:41 up  1:36,  1 user,  load average: 0.00, 0.00, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -77,7 +77,7 @@ static void statistic() {
 
 void assert_fail_msg() {
   isa_regs_display();
-	IFDEF(CONFIG_ITRACE, iringbuf_push(iringbuf));
+	IFDEF(CONFIG_ITRACE, iringbuf_display());
   statistic();
 }
 
