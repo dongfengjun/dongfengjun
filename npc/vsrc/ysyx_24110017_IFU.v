80653777f83dd06c97d9e4a0b07bb3fe4408251f
tracer-ysyx <tracer@ysyx.org>
2025-07-24 21:43:44 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:43:44 up 10:12,  1 user,  load average: 0.00, 0.02, 0.04

--- npc/vsrc/ysyx_24110017_IFU.v
+++ npc/vsrc/ysyx_24110017_IFU.v
@@ -1,61 +1,50 @@
-module ysyx_24110017_IFU(clk,rst,
-		pc,inst,PCU_VALID,IFU_READY,IFU_VALID,IDU_READY,wbu_done,
-		M_AXI_AWREADY,M_AXI_AWVALID,M_AXI_AWID,M_AXI_AWADDR,
-		M_AXI_AWLEN,M_AXI_AWSIZE,M_AXI_AWBURST,
-		M_AXI_WREADY,M_AXI_WVALID,M_AXI_WDATA,M_AXI_WSTRB,M_AXI_WLAST,
-		M_AXI_BREADY,M_AXI_BVALID,M_AXI_BID,M_AXI_BRESP,
-		M_AXI_ARREADY,M_AXI_ARVALID,M_AXI_ARID,M_AXI_ARADDR,
-		M_AXI_ARLEN,M_AXI_ARSIZE,M_AXI_ARBURST,
-		M_AXI_RREADY,M_AXI_RVALID,M_AXI_RID,M_AXI_RDATA,M_AXI_RRESP,M_AXI_RLAST
-);
-input clk;
-input rst;
-input [31:0]pc;
-output [31:0]inst;
-input PCU_VALID;
-output IFU_READY;
-output IFU_VALID;
-input IDU_READY;
-input wbu_done;
-/***SRAM*W**/
-input M_AXI_AWREADY;
-output M_AXI_AWVALID;
-output [3:0]M_AXI_AWID;
-output [31:0]M_AXI_AWADDR;
-output [7:0]M_AXI_AWLEN;
-output [2:0]M_AXI_AWSIZE;
-output [1:0]M_AXI_AWBURST;
-input M_AXI_WREADY;
-output M_AXI_WVALID;
-output [31:0]M_AXI_WDATA;
-output [3:0]M_AXI_WSTRB;
-output M_AXI_WLAST;
-output M_AXI_BREADY;
-input M_AXI_BVALID;
-input [3:0]M_AXI_BID;
-input [1:0]M_AXI_BRESP;
-/***SRAM*R***/
-input M_AXI_ARREADY;
-output M_AXI_ARVALID;
-output [3:0]M_AXI_ARID;
-output [31:0]M_AXI_ARADDR;
-output [7:0]M_AXI_ARLEN;
-output [2:0]M_AXI_ARSIZE;
-output [1:0]M_AXI_ARBURST;
-output M_AXI_RREADY;
-input M_AXI_RVALID;
-input [3:0]M_AXI_RID;
-input [31:0]M_AXI_RDATA;
-input [1:0]M_AXI_RRESP;
-input M_AXI_RLAST;
+module ysyx_24110017_IFU(
+	input clk,
+	input rst,
+	input [31:0]pc_i,
+	output [31:0]inst_o,
+	input pc_valid_i,
+	output if_ready_o,
+	output if_valid_o,
+	input id_ready_i,
+	input wb_done_i,
+/***AXI4_R**/
+	input if_axi_awready_i,
+	output if_axi_awvalid_o,
+	output [3:0]if_axi_awid_o,
+	output [31:0]if_axi_awaddr_o,
+	output [7:0]if_axi_awlen_o,
+	output [2:0]if_axi_awsize_o,
+	output [1:0]if_axi_awburst_o,
+	input if_axi_wready_i,
+	output if_axi_wvalid_o,
+	output [31:0]if_axi_wdata_o,
+	output [3:0]if_axi_wstrb_o,
+	output if_axi_wlast_o,
+	output if_axi_bready_o,
+	input if_axi_bvalid_i,
+	input [3:0]if_axi_bid_i,
+	input [1:0]if_axi_bresp_i,
+/***AXI4*R***/
+	input if_axi_arready_i,
+	output if_axi_arvalid_o,
+	output [3:0]if_axi_arid_o,
+	output [31:0]if_axi_araddr_o,
+	output [7:0]if_axi_arlen_o,
+	output [2:0]if_axi_arsize_o,
+	output [1:0]if_axi_arburst_o,
+	output if_axi_rready_o,
+	input if_axi_rvalid_i,
+	input [3:0]if_axi_rid_i,
+	input [31:0]if_axi_rdata_i,
+	input [1:0]if_axi_rresp_i,
+	input if_axi_rlast_i,
+};
 
-wire [31:0]pc;
-wire PCU_VALID,IFU_READY;
-reg ifu_ready;
-assign IFU_READY =ifu_ready;
-wire IFU_VALID,IDU_READY;
-reg ifu_valid;
-assign IFU_VALID = ifu_valid;
+reg if_ready_reg;
+assign if_ready = if_ready_reg;
+reg if_valid_reg;
+assign if_valid = if_valid_reg;
 
 /***单周期***
 import "DPI-C" function int pmem_read(input int raddr);
@@ -78,44 +67,45 @@ Sta_RegisterFile Sta_RegisterFile(clk,wdata,wdata[7:0],wen,pc[7:0],inst);
 /***E*N*D***/
 
 /***多周期*分布式控制***/
-reg [31:0]inst;
+reg [31:0]inst_reg;
+assign inst_o = inst_reg;
 
 parameter IDLE_IFU = 2'b00,WAIT_SRAM = 2'b01,WAIT_IDU_READY = 2'b10,DONE_IFU = 2'b11;
-reg [1:0]current_state,next_state;
+reg [1:0]state,next_state;
 
 always @(posedge clk) begin
   if (rst) begin
-    current_state <= IDLE_IFU;
+    state <= IDLE_IFU;
   end
 	else begin
-    current_state <= next_state;
+    state <= next_state;
   end
 end
 
 always @(*) begin
-  next_state = current_state;
+  state = current_state;
 	if(rst) begin
-		next_state = IDLE_IFU;
+		state = IDLE_IFU;
 	end
   else begin
-		case (current_state)
+		case (state)
 			IDLE_IFU: begin
-				if(PCU_VALID) begin
+				if(pc_valid_i) begin
 					next_state = WAIT_SRAM;
 				end
 			end
 			WAIT_SRAM: begin
-				if(IFU_VALID) begin
+				if(if_valid_o) begin
 					next_state = WAIT_IDU_READY;
 				end
 			end
 			WAIT_IDU_READY: begin
-				if(IDU_READY) begin
+				if(id_ready_i) begin
 					next_state = DONE_IFU;
 				end
 			end
 			DONE_IFU: begin
-				if(wbu_done) begin
+				if(wb_done_i) begin
 					next_state = IDLE_IFU;
 				end
 			end
@@ -126,64 +116,54 @@ always @(*) begin
 	end
 end
 
-always @(posedge clk) begin
+always @(posedge clk or posedge rst) begin
 	if(rst) begin
-		ifu_valid <= 1'b0;
-		ifu_ready <= 1'b0;
+		if_valid_reg <= 1'b0;
+		if_ready_reg <= 1'b0;
 		sram_start <= 1'b0;
-		inst <= 32'h0;
+		inst_reg <= 32'h0;
 	end
 	else begin
-		case (current_state)
+		case (state)
 			IDLE_IFU: begin
-				ifu_valid <= 1'b0;
-				if(PCU_VALID) begin
+				if_valid_reg <= 1'b0;
+				if(pc_valid_i) begin
 					sram_start <= 1'b1;
 				end
-				ifu_ready <= 1'b0;
+				if_ready_reg <= 1'b0;
 			end
 			WAIT_SRAM: begin
 				sram_start <= 1'b0;
-				if(M_AXI_RVALID && M_AXI_RREADY) begin
-					ifu_valid <= 1'b1;
+				if(if_axi_rvalid_i && if_axi_rready_o) begin
+					if_valid_reg <= 1'b1;
 				end
 			end
 			WAIT_IDU_READY: begin
-				if(IFU_VALID && IDU_READY) begin
-					ifu_valid <= 1'b0;
-					inst <= inst_reg;
+				if(if_valid_o && id_ready_i) begin
+					if_valid_reg <= 1'b0;
+					inst_reg <= axi_rdata;
 				end
 			end
 			DONE_IFU: begin
-				if(wbu_done) begin
-          ifu_ready <= 1'b1;
+				if(wb_done) begin
+          if_ready_reg <= 1'b1;
         end
 			end
 		endcase
 	end
 end
 
-/***多周期sram***AXI4_LITE***/
-wire [31:0]pc;
-wire [31:0]M_AXI_AWADDR,M_AXI_WDATA,M_AXI_ARADDR,M_AXI_RDATA;             
-wire [3:0]M_AXI_WSTRB;
-wire [7:0]M_AXI_AWLEN,M_AXI_ARLEN;
-wire [3:0]M_AXI_AWID,M_AXI_BID,M_AXI_ARID;
-wire [3:0]M_AXI_AWSIZE,M_AXI_ARSIZE;
-wire [2:0]M_AXI_AWBURST,M_AXI_ARBURST;
-wire [1:0]M_AXI_BRESP,M_AXI_RRESP;
-wire M_AXI_AWVALID,M_AXI_AWREADY,M_AXI_WVALID,M_AXI_WREADY,M_AXI_BVALID,M_AXI_BREADY,M_AXI_ARVALID,M_AXI_ARREADY,M_AXI_RVALID,M_AXI_RREADY,M_AXI_WLAST,M_AXI_RLAST;
-
-assign M_AXI_AWVALID = 0;
-assign M_AXI_WVALID = 0;
-assign M_AXI_AWID = 0;
-assign M_AXI_AWADDR = 0;
-assign M_AXI_WDATA = 0;
-assign M_AXI_AWLEN = 0;
-assign M_AXI_AWSIZE = 0;
-assign M_AXI_AWBURST = 0;
-assign M_AXI_WSTRB = 0;
-assign M_AXI_BREADY = 0;
+/***AXI4_LITE***/
+assign if_axi_awvalid_o = 0;
+assign if_axi_wvalid_o = 0;
+assign if_axi_awid_o = 0;
+assign if_axi_awaddr_o = 0;
+assign if_axi_wdata_o = 0;
+assign if_axi_awlen_o = 0;
+assign if_axi_awsize_o = 0;
+assign if_axi_awburst_o = 0;
+assign if_axi_wstrb_o = 0;
+assign if_axi_bready_o = 0;
 
 reg axi_arvalid,axi_rready;
 reg [3:0]axi_arid;
@@ -199,11 +179,10 @@ assign M_AXI_ARLEN = axi_arlen;
 assign M_AXI_ARSIZE = axi_arsize;
 assign M_AXI_ARBURST = axi_arburst;
 
-
-parameter SRAM_IDLE=1'b0,SRAM_FETCH=1'b1;
-reg state;
-reg sram_start;
-reg [31:0]inst_reg;
+parameter IDLE=1'b0,FETCH=1'b1;
+reg axi_state;
+reg fetch_start;
+reg [31:0]axi_rdata;
 
 /***DELAY_TEST_RAND***
 wire [7:0]rand_delay;
@@ -213,7 +192,7 @@ LFSR_ysyx_24110017 LFSR_ysyx_20110017(clk,rst,rand_delay);
 
 always @(posedge clk) begin
         if(rst) begin
-          state <= SRAM_IDLE;
+          axi_state <= IDLE;
 					axi_arvalid <= 0;
 					axi_rready <= 0;
 					axi_arid <= 4'b0;
@@ -225,12 +204,12 @@ always @(posedge clk) begin
 					//delay_counter <= 8'b0; //delay_test_rand
         end 
 				else begin
-            case (state)
-                SRAM_IDLE: begin
-                    if (sram_start) begin
-                        state <= SRAM_FETCH;
+            case (axi_state)
+                IDLE: begin
+                    if (fetch_start) begin
+                        axi_state <= FETCH;
 												axi_arvalid <= 1'b1;
-												axi_araddr <= pc;
+												axi_araddr <= pc_i;
                     end
 								/***DELAY_TEST_RAND***M_AXI_ARVALID***
 										if(sram_start) begin
@@ -251,12 +230,11 @@ always @(posedge clk) begin
 										end
 								/***END***/
                 end
-                SRAM_FETCH: begin
-                    if(M_AXI_ARVALID && M_AXI_ARREADY) begin
+                FETCH: begin
+                    if(if_axi_arvalid_o && if_axi_arready_i) begin
                         axi_arvalid <= 1'b0;
-												//axi_araddr <= pc;
 										end
-										if(M_AXI_RVALID && !M_AXI_RREADY) begin
+										if(if_axi_rvalid_i && !if_axi_rready_o) begin
 											axi_rready <= 1'b1;
 										end
 /***DELAY_TEST_RAND***M_AXI_RREADY***
@@ -274,10 +252,10 @@ always @(posedge clk) begin
                       end
                     end
 /***END***/
-                    if (M_AXI_RVALID && M_AXI_RREADY) begin
-												state <= SRAM_IDLE; 
+                    if (if_axi_rvalid_o && if_axi_rready_i) begin
+												axi_state <= IDLE; 
                         axi_rready <= 1'b0;
-												inst_reg <= M_AXI_RDATA;
+												axi_rdata <= if_axi_rdata_i;
                     end
                 end
             endcase
