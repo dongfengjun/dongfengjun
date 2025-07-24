8ca271fbfb3960b0f251b58455b488fbf6ad30a7
tracer-ysyx <tracer@ysyx.org>
2025-07-24 19:12:31 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:12:31 up  7:53,  1 user,  load average: 0.00, 0.03, 0.05

--- npc/vsrc/ysyx_24110017_PCU.v
+++ npc/vsrc/ysyx_24110017_PCU.v
@@ -1,20 +1,16 @@
-module ysyx_24110017_PCU(clk,rst,pc,dnpc,
-			PCU_VALID,IFU_READY
+module ysyx_24110017_PCU(
+	input clk,
+	input rst,
+	output [31:0]	pc_o,
+	input [31:0] dnpc_i,
+	output pc_valid_o,
+	input if_ready_i
 );
-input	clk;
-input rst;
-output [31:0]pc;
-input [31:0]dnpc;
 
-output PCU_VALID;
-input IFU_READY;
-reg pcu_valid;
-wire IFU_READY;
-wire PCU_VALID = pcu_valid;
-
-
-reg [31:0]pc;
-wire [31:0]dnpc;
+reg pc_valid_reg;
+wire pc_valid_o = pc_valid_reg;
+reg [31:0]pc_reg;
+wire [31:0]pc_o = pc_reg;
 
 parameter IDLE = 1'b0,WAIT_READY = 1'b1;
 reg state,next_state;
@@ -36,17 +32,17 @@ always @(*) begin
   else begin
 		case (state)
 			IDLE: begin
-				if(PCU_VALID) begin
+				if(pc_valid_o) begin
 					next_state = WAIT_READY;
 				end
 			end
 			WAIT_READY: begin
-				if(PCU_VALID && IFU_READY) begin
+				if(pc_valid_o && if_ready_i) begin
 					next_state = IDLE;
 				end
 			end
 			default: begin
-				next_state = IDLE; // 默认回到初始状态
+				next_state = IDLE;
 			end
 		endcase
 	end
@@ -54,31 +50,31 @@ end
 
 always @(posedge clk) begin
 	if(rst) begin
-		pcu_valid <= 1'b0;
+		pc_valid_reg <= 1'b0;
 		//pc <= 32'h10000000; //mrom
 		pc <= 32'h30000000; //flash
 	end
 	else begin
 		case (state)
 			IDLE: begin
-				if(((dnpc >= 32'h30000000) && (dnpc < 32'h40000000))
-					 || ((dnpc >= 32'h0f000000) && (dnpc < 32'h0f002000))
-					 || ((dnpc >= 32'h80000000) && (dnpc < 32'h84000000))
-					 || ((dnpc >= 32'ha0000000) && (dnpc < 32'hc0000000))) begin 
-					pcu_valid <= 1'b1;
+				if(((dnpc_i >= 32'h30000000) && (dnpc_i < 32'h40000000))
+					 || ((dnpc_i >= 32'h0f000000) && (dnpc_i < 32'h0f002000))
+					 || ((dnpc_i >= 32'h80000000) && (dnpc_i < 32'h84000000))
+					 || ((dnpc_i >= 32'ha0000000) && (dnpc_i < 32'hc0000000))) begin 
+					pc_valid_reg <= 1'b1;
 				end
-				if(((pc < 32'h30000000) || (pc >= 32'h40000000)) 
-					&& ((pc < 32'h0f000000) || (pc >= 32'h0f002000))
-					&& ((pc < 32'h80000000) || (pc >= 32'h84000000))
-					&& ((pc < 32'ha0000000) || (pc >= 32'hc0000000))) begin
-					$fwrite(32'h80000002, "Assertion failed: Invalid PC `%xh`\n",pc);
+				if(((pc_o < 32'h30000000) || (pc_o >= 32'h40000000)) 
+					&& ((pc_o < 32'h0f000000) || (pc_o >= 32'h0f002000))
+					&& ((pc_o < 32'h80000000) || (pc_o >= 32'h84000000))
+					&& ((pc_o < 32'ha0000000) || (pc_o >= 32'hc0000000))) begin
+					$fwrite(32'h80000002, "Assertion failed: Invalid PC `%xh`\n",pc_o);
           $fatal;
 				end
 			end
 			WAIT_READY: begin
-				if(PCU_VALID && IFU_READY) begin
-					pcu_valid <= 1'b0;
-					pc <= dnpc;
+				if(pc_valid_o && if_ready_i) begin
+					pc_valid_reg <= 1'b0;
+					pc_reg <= dnpc_i;
 				end
 			end
 		endcase
