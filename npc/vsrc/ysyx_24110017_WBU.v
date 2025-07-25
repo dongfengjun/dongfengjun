f411c8405eb25b4871967159c7ede71e7f5e0215
tracer-ysyx <tracer@ysyx.org>
2025-07-25 21:27:40 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:27:40 up  6:44,  1 user,  load average: 0.03, 0.04, 0.10

--- npc/vsrc/ysyx_24110017_WBU.v
+++ npc/vsrc/ysyx_24110017_WBU.v
@@ -1,55 +1,45 @@
-module ysyx_24110017_WBU(clk,rst,
-			EXU_VALID,WBU_READY,wbu_done,difftest,
-			xrd_reg,res,
-			rd_reg,rd,
-			wen_reg,gpr_wen,
-			o_mepc,o_mstatus,o_mcause,o_mtvec,
-			w_mepc_reg,w_mstatus_reg,w_mcause_reg,w_mtvec_reg,
-			mepc_wen,mstatus_wen,mcause_wen,mtvec_wen,
-			mepc_wen_reg,mstatus_wen_reg,mcause_wen_reg,mtvec_wen_reg
+module ysyx_24110017_WBU(
+	input clk,
+	input rst,
+	input ex_valid_i,
+	output wb_ready_o,
+	output wb_done_o,
+	output difftest,
+	output [31:0]xrd_o,
+	input [31:0]ex_i,
+	output [4:0]rd_o,
+	input [4:0]rd_i,
+	output wen_o,
+	input gpr_wen_i,
+	input [31:0]mepc_i,mstatus_i,mcause_i,mtvec_i,
+	output [31:0]mepc_o,mstatus_o,mcause_o,mtvec_o,
+	input mepc_wen_i,mstatus_wen_i,mcause_wen_i,mtvec_wen_i,
+	output mepc_wen_o,mstatus_wen_o,mcause_wen_o,mtvec_wen_o;
 );
-input clk;
-input rst;
-input EXU_VALID;
-output WBU_READY;
-output wbu_done;
-output difftest;
-output [31:0]xrd_reg;
-input [31:0]res;
-output [4:0]rd_reg;
-input [4:0]rd;
-output wen_reg;
-input gpr_wen;
-input [31:0]o_mepc,o_mstatus,o_mcause,o_mtvec;
-output [31:0]w_mepc_reg,w_mstatus_reg,w_mcause_reg,w_mtvec_reg;
-input mepc_wen,mstatus_wen,mcause_wen,mtvec_wen;
-output mepc_wen_reg,mstatus_wen_reg,mcause_wen_reg,mtvec_wen_reg;
 
+reg wb_ready_reg;
+assign wb_ready_o = wb_ready_reg;
 
-wire [31:0]xrd;
-wire [4:0]o_rf_raddr;
-wire o_rf_wen;
-wire [31:0]w_mepc,w_mstatus,w_mcause,w_mtvec;
-
-assign xrd = res;
-assign o_rf_raddr = rd;
-assign o_rf_wen = gpr_wen;
-
-assign w_mepc = o_mepc;
-assign w_mstatus = o_mstatus;
-assign w_mcause = o_mcause;
-assign w_mtvec = o_mtvec;
-
-wire EXU_VALID,WBU_READY = wbu_ready;
-reg wbu_ready;
-
-reg wbu_done;
-reg difftest;
+reg wb_done_reg;
+reg difftest_reg;
 reg [31:0]xrd_reg;
 reg [4:0]rd_reg;
-reg wen_reg;
-reg [31:0]w_mepc_reg,w_mstatus_reg,w_mcause_reg,w_mtvec_reg;
+reg gpr_wen_reg;
+reg [31:0]mepc_reg,mstatus_reg,mcause_reg,mtvec_reg;
 reg mepc_wen_reg,mstatus_wen_reg,mcause_wen_reg,mtvec_wen_reg;
+assign wb_done_o = wb_done_reg;
+assign difftest = difftest_reg;
+assign xrd_o = xrd_reg;
+assign rd_o = rd_reg;
+assign gpr_wen_o = gpr_wen_reg;
+assign mepc_o = mepc_reg;
+assign mstatus_o = mstatus_reg;
+assign mcause_o = mcause_reg;
+assign mtvec_o = mtvec_reg;
+assign mepc_wen_o = mepc_wen_reg;
+assign mstatus_wen_o = mstatus_wen_reg;
+assign mcause_wen_o = mcause_wen_reg;
+assign mtvec_wen_o = mtvec_wen_reg;
 
 parameter IDLE = 2'b00,WRITE = 2'b01,DIFF = 2'b10,NULL = 2'b11;
 reg [1:0]state,next_state;
@@ -90,53 +80,53 @@ end
 
 always @(posedge clk) begin
 	if(rst) begin
-		wbu_ready <= 1'b0;
+		wb_ready <= 1'b0;
 		xrd_reg <= 32'h0;
 		rd_reg <= 5'b0;
-		wen_reg <= 1'b0;
+		gpr_wen_reg <= 1'b0;
 		
-		w_mepc_reg <= 32'h0;
-		w_mstatus_reg <= 32'h0;
-		w_mcause_reg <= 32'h0;
-		w_mtvec_reg <= 32'h0;
+		mepc_reg <= 32'h0;
+		mstatus_reg <= 32'h0;
+		mcause_reg <= 32'h0;
+		mtvec_reg <= 32'h0;
 		mepc_wen_reg <= 1'b0;
 		mstatus_wen_reg <= 1'b0;
 		mcause_wen_reg <= 1'b0;
 		mtvec_wen_reg <= 1'b0;
 		
-		wbu_done <= 1'b0;
-		difftest <= 1'b0;
+		wb_done_reg <= 1'b0;
+		difftest_reg <= 1'b0;
 	end
 	else begin
 		case (state)
 			IDLE: begin
-				difftest <= 1'b0;
-				if(EXU_VALID) begin //判断条件
-					wbu_ready <= 1'b1;
+				difftest_reg <= 1'b0;
+				if(ex_valid_i) begin //判断条件
+					wb_ready <= 1'b1;
 				end
-				if(EXU_VALID && WBU_READY) begin
-					wbu_ready <= 1'b0;
+				if(ex_valid_i && wb_ready_o) begin
+					wb_ready <= 1'b0;
 				end
 			end
 			WRITE: begin
-	      xrd_reg <= xrd;
-        rd_reg <= o_rf_raddr;
-        wen_reg <= o_rf_wen;
+	      xrd_reg <= xrd_i;
+        rd_reg <= rd_i;
+        gpr_wen_reg <= gpr_wen_i;
 
-				w_mepc_reg <= w_mepc;
-				w_mstatus_reg <= w_mstatus;
-				w_mcause_reg <= w_mcause;
-				w_mtvec_reg <= w_mtvec;
-				mepc_wen_reg <= mepc_wen;
-	      mstatus_wen_reg <= mstatus_wen;
-				mcause_wen_reg <= mcause_wen;
-		    mtvec_wen_reg <= mtvec_wen;
+				mepc_reg <= mepc_i;
+				mstatus_reg <= mstatus_i;
+				mcause_reg <= mcause_i;
+				mtvec_reg <= mtvec_i;
+				mepc_wen_reg <= mepc_wen_i;
+	      mstatus_wen_reg <= mstatus_wen_i;
+				mcause_wen_reg <= mcause_wen_i;
+		    mtvec_wen_reg <= mtvec_wen_i;
         
-				wbu_done <= 1'b1;
+				wb_done_reg <= 1'b1;
 			end
 			DIFF: begin
-				wbu_done <= 1'b0;
-				difftest <= 1'b1;
+				wb_done_reg <= 1'b0;
+				difftest_reg <= 1'b1;
 			end
 			NULL: begin
 			end
