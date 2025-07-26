276a04f25431ea342e9c4ad0a9b1b1da2f9b6978
tracer-ysyx <tracer@ysyx.org>
2025-07-26 18:27:12 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:27:12 up 38 min,  1 user,  load average: 0.24, 0.17, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -445,8 +445,8 @@ int main(int argc, char *argv[]) {
   Log("CPI = %.6f", (double)g_nr_guest_cycle/(double)g_nr_guest_inst);
   Log("IF FIN:%ld\tID FIN:%ld\tEX FIN:%ld\tLS FIN:%ld",if_fin_cnt,id_fin_cnt,ex_fin_cnt,ls_fin_cnt);
   Log("Integer\tTransfer\tLoad\tStore\tImmediate\tSystem");
-  Log("%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f (Proportion)",(double)Integer_Computational_wait/(double)ex_total_wait,(double)Transfer_wait/(double)ex_total_wait,(double)Load_wait/(double)ex_total_wait,(double)Store_wait/(double)ex_total_wait,(double)Immediate_wait/(double)ex_total_wait,(double)System_wait/(double)ex_total_wait);
-  Log("%ld\t%ld\t%ld\t%ld\t%ld\t%ld (Average Cycles)",Integer_Computational_wait/Integer_Computational_cnt, Transfer_wait/Transfer_cnt, Load_wait/Load_cnt, Store_wait/Store_cnt, Immediate_wait/Immediate_cnt, System_wait/System_cnt);
+  printf("%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f (Proportion)",(double)Integer_Computational_wait/(double)ex_total_wait,(double)Transfer_wait/(double)ex_total_wait,(double)Load_wait/(double)ex_total_wait,(double)Store_wait/(double)ex_total_wait,(double)Immediate_wait/(double)ex_total_wait,(double)System_wait/(double)ex_total_wait);
+  printf("%ld\t%ld\t%ld\t%ld\t%ld\t%ld (Average Cycles)",Integer_Computational_wait/Integer_Computational_cnt, Transfer_wait/Transfer_cnt, Load_wait/Load_cnt, Store_wait/Store_cnt, Immediate_wait/Immediate_cnt, System_wait/System_cnt);
   Log("The proportion of IF MEM access:%.6f", (double)if_mem_wait/(double)if_wait);
 	Log("LS LOAD:%ld (Average)", ls_load_wait/ls_load_cnt);
   Log("LS STORE:%ld (Average)", ls_store_wait/ls_store_cnt);
