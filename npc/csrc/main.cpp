be42069349327211ea408a06302d866847711f30
tracer-ysyx <tracer@ysyx.org>
2025-07-26 18:05:34 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:05:34 up 17 min,  1 user,  load average: 0.04, 0.10, 0.14

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -123,7 +123,7 @@ static void statistic() {
 	Log("%ld\t%ld\t%ld\t%ld\t%ld\t%ld (Average Cycles)",Integer_Computational_wait/Integer_Computational_cnt, Transfer_wait/Transfer_cnt, Load_wait/Load_cnt, Store_wait/Store_cnt, Immediate_wait/Immediate_cnt, System_wait/System_cnt);
 	Log("The proportion of IF MEM access:%.6f", (double)if_mem_wait/(double)if_wait);
 	Log("LS LOAD:%ld (Average)", ls_load_wait/ls_load_cnt);
-	LOg("LS STORE:%ld (Average)", ls_store_wait/ls_store_cnt);
+	Log("LS STORE:%ld (Average)", ls_store_wait/ls_store_cnt);
 }
 
 void assert_fail_msg() {
