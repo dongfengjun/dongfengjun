2c9f6f2e982fd9b6b92e806262b872a2416e6107
tracer-ysyx <tracer@ysyx.org>
2025-07-26 18:39:54 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:39:54 up 51 min,  1 user,  load average: 0.00, 0.03, 0.05

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -448,8 +448,8 @@ int main(int argc, char *argv[]) {
   //printf("%.6f  %.6f  %.6f  %.6f  %.6f  %.6f (Proportion)",(double)Integer_Computational_wait/(double)ex_total_wait,(double)Transfer_wait/(double)ex_total_wait,(double)Load_wait/(double)ex_total_wait,(double)Store_wait/(double)ex_total_wait,(double)Immediate_wait/(double)ex_total_wait,(double)System_wait/(double)ex_total_wait);
   //printf("%ld\t%ld\t%ld\t%ld\t%ld\t%ld (Average Cycles)",Integer_Computational_wait/Integer_Computational_cnt, Transfer_wait/Transfer_cnt, Load_wait/Load_cnt, Store_wait/Store_cnt, Immediate_wait/Immediate_cnt, System_wait/System_cnt);
   Log("The proportion of IF MEM access:%.6f", (double)if_mem_wait/(double)if_wait);
-	Log("LS LOAD:%ld (Average)", ls_load_wait/ls_load_cnt);
-  Log("LS STORE:%ld (Average)", ls_store_wait/ls_store_cnt);
+	Log("LS LOAD:%d (Average)", ls_load_wait/ls_load_cnt);
+  Log("LS STORE:%d (Average)", ls_store_wait/ls_store_cnt);
 /***close**/
 	statistic();
 	tfp->close();
