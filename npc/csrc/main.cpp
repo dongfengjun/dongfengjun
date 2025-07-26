29cd5793b90945e24c2a4f8b029a7b3ad3d119fa
tracer-ysyx <tracer@ysyx.org>
2025-07-26 18:57:31 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:57:31 up  1:09,  1 user,  load average: 0.38, 0.25, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -447,9 +447,9 @@ int main(int argc, char *argv[]) {
   Log("Integer\tTransfer\tLoad\tStore\tImmediate\tSystem");
   //printf("%.6f  %.6f  %.6f  %.6f  %.6f  %.6f (Proportion)",(double)Integer_Computational_wait/(double)ex_total_wait,(double)Transfer_wait/(double)ex_total_wait,(double)Load_wait/(double)ex_total_wait,(double)Store_wait/(double)ex_total_wait,(double)Immediate_wait/(double)ex_total_wait,(double)System_wait/(double)ex_total_wait);
   //printf("%ld\t%ld\t%ld\t%ld\t%ld\t%ld (Average Cycles)",Integer_Computational_wait/Integer_Computational_cnt, Transfer_wait/Transfer_cnt, Load_wait/Load_cnt, Store_wait/Store_cnt, Immediate_wait/Immediate_cnt, System_wait/System_cnt);
-  printf("The proportion of IF MEM access:%.6f\n", (double)if_mem_wait/(double)if_wait);
-	printf("LS LOAD:%d (Average)", ls_load_wait/ls_load_cnt);
-  printf("LS STORE:%d (Average)", ls_store_wait/ls_store_cnt);
+  printf("LS LOAD:%d (Average)", ls_load_wait/ls_load_cnt);                 
+	printf("LS STORE:%d (Average)", ls_store_wait/ls_store_cnt);
+	printf("The proportion of IF MEM access:%.6f\n", (double)if_mem_wait/(double)if_wait);
 /***close**/
 	statistic();
 	tfp->close();
