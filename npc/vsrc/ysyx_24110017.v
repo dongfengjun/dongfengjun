3dc76c0426a59c23db74a5e7bd88d87ab025231b
tracer-ysyx <tracer@ysyx.org>
2025-07-26 20:07:48 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:07:48 up  2:19,  1 user,  load average: 0.03, 0.03, 0.03

--- npc/vsrc/ysyx_24110017.v
+++ npc/vsrc/ysyx_24110017.v
@@ -311,7 +311,7 @@ function int performance_counter(int i);
 															 : (i == 1) ? {31'b0,id_valid && ex_ready}
 															 : (i == 2) ? {31'b0,ex_valid && wb_ready}
 															 : (i == 3) ? {31'b0,ls_done}
-															 : (i == 4) ? {25'b0,op}
+															 : (i == 4) ? {25'b0,inst[6:0]}
 															 : (i == 5) ? {31'b0,wb_done}
 															 : (i == 6) ? {31'b0,if_axi_arvalid && if_axi_arready}
 															 : (i == 7) ? {31'b0,if_axi_rvalid && if_axi_rready}
