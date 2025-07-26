437ea414b4fcbdcb8b57739c69a924fc0ccc5537
tracer-ysyx <tracer@ysyx.org>
2025-07-26 16:48:59 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:48:59 up  5:28,  1 user,  load average: 0.08, 0.06, 0.01

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -108,7 +108,7 @@ static void statistic() {
 	Log("total guest instructions = " NUMBERIC_FMT, g_nr_guest_inst);
 	Log("IPC = %.6f", (double)g_nr_guest_inst/(double)g_nr_guest_cycle);
 	Log("CPI = %.6f", (double)g_nr_guest_cycle/(double)g_nr_guest_inst);
-  Log("IF FIN:%ld\tID FIN:%ld\tEX FIN cnt:%ld\tLS FIN:%ld",if_performance_cnt,id_performance_cnt,ex_performance_cnt,ls_performance_cnt);
+  Log("IF FIN:%ld\tID FIN:%ld\tEX FIN cnt:%ld\tLS FIN:%ld",if_fin_cnt,id_fin_cnt,ex_fin_cnt,ls_fin_cnt);
 	if (g_timer > 0) Log("simulation frequency = " NUMBERIC_FMT " inst/s", g_nr_guest_inst * 1000000 / g_timer);
   else Log("Finish running in less than 1 us and can not calculate the simulation frequency");
 }
