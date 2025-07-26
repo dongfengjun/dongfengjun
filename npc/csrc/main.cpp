b614a7bd7317997160e679d3ba81abac602bc47c
tracer-ysyx <tracer@ysyx.org>
2025-07-26 17:58:56 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:58:56 up 10 min,  1 user,  load average: 0.06, 0.20, 0.19

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -113,18 +113,17 @@ static void statistic() {
   Log("total guest cycles = " NUMBERIC_FMT, g_nr_guest_cycle);
 	Log("total guest instructions = " NUMBERIC_FMT, g_nr_guest_inst);
 	if (g_timer > 0) Log("simulation frequency = " NUMBERIC_FMT " inst/s", g_nr_guest_inst * 1000000 / g_timer);
-  else Log("Finish running in less than 1 us and can not calculate the simulation frequency");
-	
+  else Log("Finish running in less than 1 us and can not calculate the simulation frequency");	
 	Log("***********Performance Evaluation**************");
 	Log("IPC = %.6f", (double)g_nr_guest_inst/(double)g_nr_guest_cycle);
 	Log("CPI = %.6f", (double)g_nr_guest_cycle/(double)g_nr_guest_inst);
 	Log("IF FIN:%ld\tID FIN:%ld\tEX FIN:%ld\tLS FIN:%ld",if_fin_cnt,id_fin_cnt,ex_fin_cnt,ls_fin_cnt);
-	Log("Integer\tTransfer\tLoad\tStore\tImmediate\tSystem");
-	Log("%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f (Proportion)",(double)Integer_Computational_wait/(double)ex_total_wait,(double)Transfer_wait/(double)ex_total_wait,(double)Load_wait/(double)ex_total_wait,(double)Store_wait/(double)ex_total_wait,(double)Immediate_wait/(double)ex_total_wait,(double)System_wait/(double)ex_total_wait);
-	Log("%ld\t%ld\t%ld\t%ld\t%ld\t%ld (Average Cycles)",Integer_Computational_wait/Integer_Computational_cnt, Transfer_wait/Transfer_cnt, Load_wait/Load_cnt, Store_wait/Store_cnt, Immediate_wait/Immediate_cnt, System_wait/System_cnt);
 	Log("The proportion of IF MEM access:%.6f", (double)if_mem_wait/(double)if_wait);
 	printf("LS LOAD:%ld (Average)", ls_load_wait/ls_load_cnt);
 	printf("LS STORE:%ld (Average)", ls_store_wait/ls_store_cnt);
+	Log("Integer\tTransfer\tLoad\tStore\tImmediate\tSystem");
+	Log("%.6f\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f (Proportion)",(double)Integer_Computational_wait/(double)ex_total_wait,(double)Transfer_wait/(double)ex_total_wait,(double)Load_wait/(double)ex_total_wait,(double)Store_wait/(double)ex_total_wait,(double)Immediate_wait/(double)ex_total_wait,(double)System_wait/(double)ex_total_wait);
+	Log("%ld\t%ld\t%ld\t%ld\t%ld\t%ld (Average Cycles)",Integer_Computational_wait/Integer_Computational_cnt, Transfer_wait/Transfer_cnt, Load_wait/Load_cnt, Store_wait/Store_cnt, Immediate_wait/Immediate_cnt, System_wait/System_cnt);
 }
 
 void assert_fail_msg() {
@@ -445,7 +444,6 @@ int main(int argc, char *argv[]) {
 	statistic();
 	tfp->close();
 	nvboard_quit();
-	statistic();
 	delete contextp;
 	return 0;
 }
