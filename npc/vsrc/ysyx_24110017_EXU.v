ee6406dc9edb4bd28c02fcb7096cd22ac2173a0e
tracer-ysyx <tracer@ysyx.org>
2025-07-27 18:18:46 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:18:46 up  4:47,  1 user,  load average: 0.22, 0.24, 0.14

--- npc/vsrc/ysyx_24110017_EXU.v
+++ npc/vsrc/ysyx_24110017_EXU.v
@@ -181,7 +181,7 @@ assign b = (op_i == 7'b0110011 || op_i == 7'b0100011) ? r2_i : imm_i;
 assign a = (op_i == 7'b0010011 || op_i == 7'b0000011 || op_i == 7'b0100011 || op_i == 7'b0110011/*R*/ || (op_i == 7'b1110011 && (funct3_i == 3'b001 || funct3_i == 3'b010 || funct3_i == 3'b011))/*csr*/) ? r1_i : 32'h0;
 /***ALU***/
 wire [31:0]x,y,res;
-assign x = ((op_i == 7'b0010011) && (funct3_i == 3'b000 || funct3_i == 3'b001 || funct3_i == 3'b011 || funct3_i == 3'b100 || funct3_i == 3'b110 || funct3_i == 3'b111)) r1_i : 32'b0
+assign x = ((op_i == 7'b0010011) && (funct3_i == 3'b000 || funct3_i == 3'b001 || funct3_i == 3'b011 || funct3_i == 3'b100 || funct3_i == 3'b110 || funct3_i == 3'b111)) ? r1_i : 32'b0
 assign y = 32'b0;
 
 assign ex = 
@@ -211,7 +211,7 @@ assign ex =
 						({32{(funct3_i == 3'b100) && (funct7_i == 7'b0000000)}} & (a ^ b)) | //xor
 						({32{(funct3_i == 3'b101) && (funct7_i == 7'b0000000)}} & (a >> b[4:0])) | //srl
 `ifndef YOSYS_STA
-						({32{(funct3_i == 3'b101) && (funct7_i == 7'b0100000)}} & {a >>> b) | //sra
+						({32{(funct3_i == 3'b101) && (funct7_i == 7'b0100000)}} & (a >>> b) | //sra
 //						({32{(funct3_i == 3'b101) && (funct7_i == 7'b0100000)}} & {{{32{a[31]}},a} >> b}[31:0] ) | //sra
 `endif	
 						({32{(funct3_i == 3'b110) && (funct7_i == 7'b0000000)}} & (a | b)) | //or
