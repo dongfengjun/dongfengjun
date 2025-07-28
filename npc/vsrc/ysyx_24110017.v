1452b19ed2b0d52d4d51fcb29084bdf139c811b6
tracer-ysyx <tracer@ysyx.org>
2025-07-28 18:55:20 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:55:20 up  7:14,  1 user,  load average: 0.20, 0.11, 0.07

--- npc/vsrc/ysyx_24110017.v
+++ npc/vsrc/ysyx_24110017.v
@@ -192,11 +192,7 @@ ysyx_24110017_LSU LSU(clock,reset,ls_read,ls_write,ls_done,
 		ls_axi_arlen,ls_axi_arsize,ls_axi_arburst,
 		ls_axi_rready,ls_axi_rvalid,ls_axi_rid,ls_axi_rdata,ls_axi_rresp,ls_axi_rlast
 );
-<<<<<<< HEAD
-ysyx_24110017_Xbar Xbar_ysyx_24110017(clock,reset,
-=======
 ysyx_24110017_Xbar Xbar(clock,reset,
->>>>>>> tracer-ysyx
 		if_axi_awready,if_axi_awvalid,if_axi_awid,if_axi_awaddr,
 		if_axi_awlen,if_axi_awsize,if_axi_awburst,
 		if_axi_wready,if_axi_wvalid,if_axi_wdata,if_axi_wstrb,if_axi_wlast,
@@ -267,11 +263,7 @@ ysyx_24110017_UART ysyx_24110017_UART(clock,reset,
 );
 ******/
 /***My-Clint***/
-<<<<<<< HEAD
-ysyx_24110017_CLINT ysyx_24110017_CLINT(clock,reset,
-=======
 ysyx_24110017_CLINT CLINT(clock,reset,
->>>>>>> tracer-ysyx
 		c_axi_awready,c_axi_awvalid,c_axi_awid,c_axi_awaddr,
 		c_axi_awlen,c_axi_awsize,c_axi_awburst,
 		c_axi_wready,c_axi_wvalid,c_axi_wdata,c_axi_wstrb,c_axi_wlast,
@@ -298,10 +290,7 @@ ysyx_24110017_Reg #(32, 32'h79737978) mvendorid_reg (clock,reset,32'b0,mvendorid
 ysyx_24110017_Reg #(32, 32'h016fe3c1) marchid_reg (clock,reset,32'b0,marchid,1'b0);
 
 
-<<<<<<< HEAD
-=======
 `ifndef YOSYS_STA
->>>>>>> tracer-ysyx
 /***DPI-C*CSR***/
 export "DPI-C" function csr_grab;                                    
 function int csr_grab(int i);
