dbcffb82387985093fe2b1e6258993cb28782261
tracer-ysyx <tracer@ysyx.org>
2025-08-02 14:48:38 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:48:38 up  2:45,  1 user,  load average: 0.04, 0.06, 0.07

--- npc/cachesim/cachesim.c
+++ npc/cachesim/cachesim.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <math.h>
 
 typedef struct {
 	uint32_t tag;
@@ -9,41 +10,47 @@ typedef struct {
 } matedata;
 
 int main(int argc, char *argv[]) {
-	int n = atoi(argv[1]);
-	int m = atoi(argv[2]);
-	int w = atoi(argv[3]);
-	matedata lines[2^(m - 2)][2^n];
+	static int n,m,w;
+	static char *f;
+	n = atoi(argv[1]);
+	m = atoi(argv[2]);
+	w = atoi(argv[3]);
+	f = argv[4];
+	matedata lines[(int)pow(2,(m - 2))][(int)pow(2,n)];
 	int access_cnt = 0;
 	int miss_cnt = 0;
 
-	FILE *file = fopen(argv[4], "r");
+	FILE *file = fopen(f, "r");
 	if(!file) {
     perror("无法打开文件\n");
   }
-  char line[11];
-  while(fgets(line, 11, file)) {
+  char line[12];
+  while(fgets(line, sizeof(line), file)) {
 		uint32_t pc;
     if(sscanf(line, "%x", &pc) == 1) {
-			uint32_t index = (pc >> m & ((2 ^ (n - w)) - 1));
+			uint32_t index = (pc >> m & ((int)pow(2,(n - w)) - 1));
 		  uint32_t tag = pc >> (m + n - w);
-		  uint32_t offset = (pc & (2 ^ m - 1)) >> 2;
-		  bool flag = false;
-		  for(int i = 0; i < (2 ^ w); i ++) {
-		    if(lines[offset][index * (2 ^ w) + i].tag == tag && lines[offset][index * (2 ^ w) + i].valid) {
+		  uint32_t offset = (pc & ((int)pow(2,m)-1)) / 4;
+		  printf("%x  ",pc);
+			printf("%d,%d,%d  ",n,m,w);
+			printf("index=%x,tag=%x,offset=%x  ",index,tag,offset);
+			bool flag = false;
+		  for(int i = 0; i < (int)pow(2,w); i ++) {
+		    if(lines[offset][index * (int)pow(2,w) + i].tag == tag && lines[offset][index * (int)pow(2,w) + i].valid) {
 		      access_cnt ++;
 		      flag = true;
 		    }
 		  }
 			if(!flag) {
 		    miss_cnt ++;
-				for(int j = (2 ^ w) - 1; j > 0; j --) {
-		      lines[offset][index * (2 ^ w) + j].valid = lines[offset][index * (2 ^ w) + j - 1].valid;
-		      lines[offset][index * (2 ^ w) + j].tag = lines[offset][index * (2 ^ w) + j - 1].tag;
+				for(int j = (int)pow(2,w) - 1; j > 0; j --) {
+		      lines[offset][index * (int)pow(2,w) + j].valid = lines[offset][index * (int)pow(2,w) + j - 1].valid;
+		      lines[offset][index * (int)pow(2,w) + j].tag = lines[offset][index * (int)pow(2,w) + j - 1].tag;
 				}
-		    lines[offset][index * (2 ^ w)].valid = 1;
-		    lines[offset][index * (2 ^ w)].tag = tag;
+		    lines[offset][index * (int)pow(2,w)].valid = 1;
+		    lines[offset][index * (int)pow(2,w)].tag = tag;
 			}
-			printf("%x  ",pc);
+			printf("access:%d\n",access_cnt);	
 		}
 	}
 	fclose(file);
