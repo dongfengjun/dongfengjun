37a60fbb103dd849d3badf2b587ab131caded4ea
tracer-ysyx <tracer@ysyx.org>
2025-08-04 11:52:46 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:52:46 up  1:01,  1 user,  load average: 0.17, 0.09, 0.03

--- npc/vsrc/ysyx_24110017_CACHE.v
+++ npc/vsrc/ysyx_24110017_CACHE.v
@@ -78,13 +78,20 @@ module ysyx_24110017_CACHE #(n = 4, m = 2, w = 3) (
   wire [n-1-w : 0]index = m_axi_araddr[m+n-w-1 : m];
   wire [m-3 : 0]offset = m_axi_araddr[m-1 : 2];
 
-  wire [(1<<w) - 1 : 0]access;
-	generate 
-    genvar i; 
-      for(i = 0; i < (1<<w); i = i + 1) begin : comparator
-        assign access[i] = (tag == tag_reg[offset][index * (1<<w) + i]) && (valid_reg[offset][index * (1<<w) + i]);
-			end
-	endgenerate
+  reg [(1<<w) - 1 : 0]access;
+	always @(*) begin
+		if(tag == tag_reg[offset][index * (1<<w)]) && (valid_reg[offset][index * (1<<w)]) begin
+			access[0] = 1'b1;
+		end
+		generate 
+			genvar i; 
+				for(i = 1; i < (1<<w); i = i + 1) begin : comparator
+					else if(tag == tag_reg[offset][index * (1<<w) + i]) && (valid_reg[offset][index * (1<<w) + i) begin
+						access[i] = 1'b1;
+					end
+				end
+		endgenerate
+	end
 
 	localparam IDLE = 2'b00;
   localparam TRANS = 2'b01;
@@ -161,6 +168,7 @@ module ysyx_24110017_CACHE #(n = 4, m = 2, w = 3) (
 					if(s_axi_arvalid && s_axi_arready) begin
 						integer a;
             integer b;
+						if()
 						for (b = 0; b < (1<<(m-2)); b = b + 1) begin : fifo
 							cache_reg[b][index * (1<<w)] <= 0;
               tag_reg[b][index * (1<<w)] <= 0;
