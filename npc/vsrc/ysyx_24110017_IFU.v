575263a8b1b3aa310061e32f06c48f6830c01e95
tracer-ysyx <tracer@ysyx.org>
2025-08-07 11:15:40 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:15:40 up 26 min,  1 user,  load average: 0.10, 0.07, 0.08

--- npc/vsrc/ysyx_24110017_IFU.v
+++ npc/vsrc/ysyx_24110017_IFU.v
@@ -45,7 +45,7 @@ module ysyx_24110017_IFU(
 );
 
 /***分布式控制***/
-assign if_valid_o = (state == WAIT || if_axi_rvalid_i && if_axi_rready_o);
+assign if_valid_o = (state == WAIT);
 parameter IDLE = 1'b0,WAIT = 1'b1;
 reg state;
 
