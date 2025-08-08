651bec8080ab973188b9cc19f7087d0f32f7b17e
tracer-ysyx <tracer@ysyx.org>
2025-08-08 15:52:42 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:52:42 up  3:08,  1 user,  load average: 0.08, 0.02, 0.01

--- npc/vsrc/ysyx_24110017.v
+++ npc/vsrc/ysyx_24110017.v
@@ -187,11 +187,15 @@ ysyx_24110017_IDU IDU(clock,reset,
 		mepc_i,mstatus_i,mcause_i,mtvec_i,
 		if_valid,id_ready,id_valid,ex_ready,
 		pc_if,inst_if,
-		pc_id,imm_id,op_id,funct3_id,rd_id,gpr_wen_id,alu_valid_id,alu_sel_id,a_id,b_id,r1_id,r2_id,fencei_id
+		pc_id,imm_id,op_id,funct3_id,rd_id,gpr_wen_id,
+		alu_valid_id,alu_sel_id,a_id,b_id,r1_id,r2_id,
+		mepc_wen_id,mstatus_wen_id,mcause_wen_id,mtvec_wen_id,fencei_id
 );
 ysyx_24110017_EXU EXU(clock,reset,
 		id_valid,ex_ready,ex_valid,wb_ready,
-		pc_id,imm_id,op_id,funct3_id,rd_id,gpr_wen_id,alu_valid_id,alu_sel_id,a_id,b_id,r1_id,r2_id,fencei_id,
+		pc_id,imm_id,op_id,funct3_id,rd_id,gpr_wen_id,
+		alu_valid_id,alu_sel_id,a_id,b_id,r1_id,r2_id,
+		mepc_wen_id,mstatus_wen_id,mcause_wen_id,mtvec_wen_i,fencei_id,
 		rd_ex,gpr_wen_ex,mepc_ex,mstatus_ex,mcause_ex,mtvec_ex,fencei_ex,
 		ls_valid_ex,ls_wen_ex,ls_read_ex,ls_write_ex,
 		ls_waddr_ex,ls_wdata_ex,ls_raddr_ex,ls_wmask_ex,ls_awsize_ex,ls_arsize_ex,ls_awlen_ex,ls_arlen_ex,ls_awburst_ex,ls_arburst_ex,
