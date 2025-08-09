0a52a47d5528db5f5555574342058be740ea3f4b
tracer-ysyx <tracer@ysyx.org>
2025-08-09 18:56:12 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:56:12 up  5:54,  1 user,  load average: 0.05, 0.03, 0.00

--- npc/vsrc/ysyx_24110017_IDU.v
+++ npc/vsrc/ysyx_24110017_IDU.v
@@ -101,8 +101,8 @@ end
 wire [6:0]op;
 wire [4:0]rd; //R I U J
 wire [2:0]funct3;
-wire [4:0]rs1;  //R I S B
-wire [4:0]rs2;  //R S B
+//wire [4:0]rs1_o;  //R I S B
+//wire [4:0]rs2_o;  //R S B
 wire [31:0]immI,immU,immS,immB,immJ,imm;
 wire [6:0]funct7; //R
 wire [4:0]shamt;  //I shamt
