b0ed446e87bf1fe01fec6d798ef291f263e8c304
tracer-ysyx <tracer@ysyx.org>
2025-08-09 21:12:59 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:12:59 up  8:11,  1 user,  load average: 0.07, 0.20, 0.18

--- npc/vsrc/ysyx_24110017_EXU.v
+++ npc/vsrc/ysyx_24110017_EXU.v
@@ -142,7 +142,6 @@ always @(posedge clk or posedge rst) begin
 		    ls_arlen_o    <= 8'b0;
 		    ls_awburst_o  <= 2'b0;
 		    ls_arburst_o  <= 2'b0;
-				dnpc_o				<= 32'h0;
 			end
 			WAIT: begin
 				if(ex_valid_o && ls_ready_i) begin
