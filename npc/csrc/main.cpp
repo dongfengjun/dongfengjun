34b7353f36172bab70a81acbed93d82093727c0d
tracer-ysyx <tracer@ysyx.org>
2025-08-10 22:44:50 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:44:50 up  8:13,  1 user,  load average: 0.40, 0.14, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -438,7 +438,7 @@ void cpu_exec(int n) {
 	while(RUNNING && n != 0) {
 		single_cycle();
 		nvboard_update();
-		cpu.pc = dpic_display(1);
+		cpu.pc = dpic_display(0);
 		isa_gpr_push();
 		performance_evaluation();
 //		if(dpic_display(3)) printf("pc:%08x  access:%ld\n",dpic_display(0),icache_access_cnt);
