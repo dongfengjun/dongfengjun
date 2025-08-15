4567f1686dacdfea19c57a5762c0caa3cd3c5a65
tracer-ysyx <tracer@ysyx.org>
2025-08-15 17:01:08 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:01:08 up  3:14,  1 user,  load average: 0.17, 0.11, 0.13

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -169,7 +169,7 @@ static void itrace_push(){
 	p += snprintf(p, sizeof(logbuf), FMT_WORD ":", dpic_display(0));
 	irp += snprintf(irp, sizeof(iringbuf), FMT_WORD ":", dpic_display(0));
 	if(dpic_display(3)) itrace_p += snprintf(itrace_p, sizeof(itracebuf), FMT_WORD "\n", dpic_display(0));
-	if(dpic_display(3) && ((dpic_display(2) & 0b1111111 ) == 0b1100011)) btrace_p += snprintf(itrace_p, sizeof(itracebuf), FMT_    WORD "\n", dpic_display(0));
+	if(dpic_display(3) && ((dpic_display(2) & 0b1111111 ) == 0b1100011)) btrace_p += snprintf(itrace_p, sizeof(itracebuf), FMT_WORD "\n", dpic_display(0));
 	int ilen = 4;
 	int i;
 	for (i = ilen - 1; i >= 0; i --) {
