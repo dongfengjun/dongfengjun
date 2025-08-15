c4de41ff73e33f9d120f4264fe2f121816e5b69f
tracer-ysyx <tracer@ysyx.org>
2025-08-15 13:51:07 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:51:07 up 4 min,  1 user,  load average: 0.10, 0.34, 0.18

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -429,13 +429,6 @@ void performance_evaluation() {
 	if(ls_store_flag) ls_store_wait ++;
 	if(ls_load_flag) ls_load_wait ++;
 	if(performance_counters(12)) isCHazard_cnt ++;
-	if((performance_counters(5)) && ((if_fin_cnt - id_fin_cnt == 1) || if_fin_cnt - id_fin_cnt == 2)) {
-    printf("pc = %08x cha = %d\n",dpic_display(0),if_fin_cnt - id_fin_cnt);
-  }
-	if(if_fin_cnt - id_fin_cnt == 3) {
-		RUNNING = false;
-		printf("aaaa pc = %08x\n",dpic_display(0));
-	}
 }
 
 void cpu_exec(int n) {
