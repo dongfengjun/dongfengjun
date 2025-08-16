a0c8b4bcea55459cdec417768abbae989815fb68
tracer-ysyx <tracer@ysyx.org>
2025-08-16 15:46:53 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:46:53 up  2:03,  1 user,  load average: 0.00, 0.04, 0.01

--- npc/vsrc/ysyx_24110017.v
+++ npc/vsrc/ysyx_24110017.v
@@ -66,6 +66,8 @@ module ysyx_24110017(
 /***PCU***/
 wire [31:0] pc;
 wire pc_valid;
+/***BTB***/
+wire [31:0]snpc;
 /***IFU***/
 wire [31:0] inst_if,pc_if;
 wire if_valid,if_ready;
@@ -89,6 +91,8 @@ wire icache_axi_awvalid,icache_axi_awready,icache_axi_wvalid,icache_axi_wready,i
 /***IDU***/
 wire [31:0]inst_id;//difftest
 wire id_valid,id_ready;
+wire [31:0]prepc;
+wire prepc_en;
 wire [4:0] rs1,rs2;
 wire [31:0] pc_id,imm_id;
 wire [6:0] op_id;
@@ -159,10 +163,11 @@ wire [31:0]mvendorid,marchid; //ID
 
 
 ysyx_24110017_PCU PCU(clock,reset,isCHazard,
-		pc,dnpc_ex,
+		pc,dnpc_ex,snpc,
 		pc_valid,
 		if_ready
 );
+ysyx_24110017_BTB BTB(clock,reset,pc,snpc,prepc,pc_if,prepc_en);
 ysyx_24110017_IFU IFU(clock,reset,isCHazard,
 		pc_valid,if_ready,if_valid,id_ready,
 		pc,pc_if,inst_if,
@@ -192,6 +197,7 @@ ysyx_24110017_CACHE #(4,4,3) ICACHE(clock,reset,fencei_id, //w < n
 );
 ysyx_24110017_IDU IDU(clock,reset,isRAW,isCHazard,
 		inst_id,//difftest
+		prepc,prepc_en,
 		rs1,rs2,r1,r2,
 		mepc,mstatus,mcause,mtvec,
 		if_valid,id_ready,id_valid,ex_ready,
---
+++ npc/vsrc/ysyx_24110017_BTB.v
@@ -0,0 +1,53 @@
+module ysyx_24110017_BTB #(n = 3, w = 2) (
+	input clk,
+	input rst,
+	input  wire [31:0] pc_i,
+	output wire [31:0] snpc_o,
+	input  wire [31:0] dnpc_i,
+	input  wire [31:0] dnpc_tag_i,
+	input  wire dnpc_en_i
+);
+	
+	reg [31:0] snpc_reg [(1<<n)-1 : 0];
+  reg [29-n+w : 0] tag_reg [(1<<n)-1 : 0];
+	wire [29-n+w : 0]tag = pc_i[31 : 2+n-w];
+	wire [n-1-w : 0]index = pc_i[1+n-w : 2];
+
+  wire [(1<<w) - 1 : 0]hit;
+	generate 
+    genvar i; 
+      for(i = 0; i < (1<<w); i = i + 1) begin : comparator
+        assign hit[i] = (tag == tag_reg[index * (1<<w) + i]);
+			end
+	endgenerate
+	
+	assign snpc_o = (hit) ? snpc_reg[index * (1 << w) + $clog2(hit)] : pc_i + 4;
+
+	reg enable;
+	always @(posedge clk) begin
+		if(rst) enable <= 1'b0;
+		else
+			enable <= dnpc_en_i;
+		end
+	end
+
+	always @(posedge clk) begin
+		if(rst) begin
+      integer j;
+			for (j = 0; j < (1<<(m-2)); j = j + 1) begin : init_reg
+					snpc_reg[j]	<= 0;
+					tag_reg[j]		<= 0;
+			end
+		end
+		else if(dnpc_en_i && !enable) begin
+			integer a;
+			for (a = 1; a < (1<<w); a = a + 1) begin
+        snpc_reg[index * (1<<w) + a] <= snpc_reg[index * (1<<w) + a - 1];
+        tag_reg[index * (1<<w) + a] <= tag_reg[index * (1<<w) + a - 1];
+      end
+			snpc_reg[index * (1<<w)] <= dnpc_i;
+			tag_reg[index * (1<<w)] <= dnpc_tag_i[31 : 2+n-w];
+		end
+	end
+
+endmodule
