b22a4900173eb293d03c75163290fb70edfc3134
tracer-ysyx <tracer@ysyx.org>
2025-08-27 16:45:14 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:45:14 up  2:25,  1 user,  load average: 0.27, 0.13, 0.10

--- npc/vsrc/ysyx_24110017.v
+++ npc/vsrc/ysyx_24110017.v
@@ -288,8 +288,8 @@ ysyx_24110017_Reg #(32, 32'b0) mtvec_reg (clock,reset,csrsw_ls,mtvec,csrs_wen_ls
 ysyx_24110017_Reg #(32, 32'h79737978) mvendorid_reg (clock,reset,32'b0,mvendorid,1'b0);
 ysyx_24110017_Reg #(32, 32'h016fe3c1) marchid_reg (clock,reset,32'b0,marchid,1'b0);
 
-wire isRAW = ((rs1 != 0) && (((!ls_ready) && (rs1 == rd_ex)) || (rs1 == rd_ls))) || 
-						 ((rs2 != 0) && (((!ls_ready) && (rs2 == rd_ex)) || (rs2 == rd_ls)));
+wire isRAW = ((rs1_id != 0) && (((!ls_ready) && (rs1_id == rd_ex)) || (rs1_id == rd_ls))) || 
+						 ((rs2_id != 0) && (((!ls_ready) && (rs2_id == rd_ex)) || (rs2_id == rd_ls)));
 
 wire isCHazard = (ex_valid && ls_ready) && (dnpc_ex != pc_id) && (pc_id != 32'h0) && (dnpc_ex != 32'h0);
 
