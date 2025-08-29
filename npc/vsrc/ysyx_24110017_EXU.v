0e44054bcfe67dee8064985550fa9c226d667e31
tracer-ysyx <tracer@ysyx.org>
2025-08-29 15:35:33 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:35:33 up  2:23,  1 user,  load average: 0.26, 0.09, 0.08

--- npc/vsrc/ysyx_24110017_EXU.v
+++ npc/vsrc/ysyx_24110017_EXU.v
@@ -191,21 +191,16 @@ assign b = ((op_i == 5'b00100) && (funct3_i == 3'b000 || funct3_i == 3'b001 || f
 	: ((op_i == 5'b01100) && ((funct3_i == 3'b010 && funct7_i == 1'b0))) ? $signed(r2_i)
 	: 32'b0;
 ***/
-wire use_r1 = (op_i == 5'b00100) && (|funct3_i) ||
-              (op_i == 5'b01100) && (funct7_i == 1'b0) && (|funct3_i);
-wire use_signed_r1 = ((op_i == 5'b00100) && (funct3_i == 3'b010)) ||
-                     ((op_i == 5'b01100) && (funct3_i == 3'b010) && (funct7_i == 1'b0));
+wire use_r1 = (op_i == 5'b00100) || (op_i == 5'b01100);
+wire use_signed_r1 = (funct3_i == 3'b010 || (funct3_i == 3'b010 && funct7_i == 1'b0));
 assign a = use_r1 ? (use_signed_r1 ? $signed(r1_i) : r1_i) : 32'b0;
-wire use_imm = (op_i == 5'b00100) && (funct3_i == 3'b000 || funct3_i == 3'b010 || funct3_i == 3'b011 || funct3_i == 3'b100 || funct3_i == 3'b110 || funct3_i == 3'b111);
-wire use_signed_imm = (op_i == 5'b00100) && (funct3_i == 3'b010);
-wire use_shamt = (op_i == 5'b00100) && (funct3_i == 3'b001 || funct3_i == 3'b101);
-wire use_r2 = (op_i == 5'b01100) && (funct7_i == 1'b0) && (funct3_i != 3'b010);
-wire use_signed_r2 = (op_i == 5'b01100) && (funct3_i == 3'b010) && (funct7_i == 1'b0);
-assign b = use_imm ? (use_signed_imm ? $signed(imm_i) : imm_i) :
-           use_shamt ? {27'b0, shamt_i} :
-           use_r2 ? r2_i :
-           use_signed_r2 ? $signed(r2_i) :
-           32'b0;
+wire use_imm = (op_i == 5'b00100);
+wire use_signed_imm = (funct3_i == 3'b010);
+wire use_shamt = (funct3_i == 3'b001 || funct3_i == 3'b101);
+wire use_r2 = (op_i == 5'b01100);
+wire use_signed_r2 = (funct3_i == 3'b010) && (funct7_i == 1'b0);
+assign b = use_imm ? (use_signed_imm ? $signed(imm_i) : use_shamt ? {27'b0, shamt_i}) :
+           use_r2 ? (use_signed_r2 ? $signed(r2_i) : r2_i) : 32'b0;
 
 localparam ADD  = 4'b0001;
 localparam SUB  = 4'b0010;
