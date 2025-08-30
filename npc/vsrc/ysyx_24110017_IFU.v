e613befdacbb63705ad2f1aa565b07835a2026f5
tracer-ysyx <tracer@ysyx.org>
2025-08-30 18:07:14 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:07:14 up  6:45,  1 user,  load average: 0.11, 0.05, 0.04

--- npc/vsrc/ysyx_24110017_IFU.v
+++ npc/vsrc/ysyx_24110017_IFU.v
@@ -31,14 +31,13 @@ module ysyx_24110017_IFU(
 assign if_ready_o = (state == IDLE);
 parameter IDLE = 1'b0,WAIT = 1'b1;
 reg state;
-wire ifaddr_valid = ((pc_i >= 32'h30000000) && (pc_i < 32'h40000000)) || ((pc_i >= 32'h0f000000) && (pc_i < 32'h0f002000)) || ((pc_i >= 32'h80000000) && (pc_i < 32'h84000000)) || ((pc_i >= 32'ha0000000) && (pc_i < 32'hc0000000));
+wire ifaddr_valid = (pc_i[31:28] == 4'h3) || (pc_i[31:24] == 8'h0f) || (pc_i[31] == 1'b1 && pc_i[30:29] != 2'b11);
 
 always @(posedge clk) begin
-	if(rst) state <= IDLE;
-	else if(isCHazard) state <= IDLE;
+	if(rst || isCHazard) state <= IDLE;
 	else begin
 		case(state)
-			IDLE: state <= (if_ready_o && ifaddr_valid) ? WAIT : state;
+			IDLE: state <= (ifaddr_valid) ? WAIT : state;
 			WAIT:	state <= (if_valid_o && id_ready_i) ? IDLE : state;
 		endcase
 	end
@@ -58,8 +57,7 @@ always @(posedge clk) begin
 end
 
 always @(posedge clk) begin
-	if(rst) if_valid_o <= 1'b0;
-	else if(isCHazard) if_valid_o <= 1'b0;
+	if(rst || isCHazard) if_valid_o <= 1'b0;
 	else begin
 		if(if_axi_rvalid_i && if_axi_rready_o && !isCHazard_reg) begin
 			if_valid_o <= 1'b1;
@@ -71,13 +69,9 @@ always @(posedge clk) begin
 end
 
 always @(posedge clk) begin
-	if(rst) begin
+	if(rst || isCHazard) begin
 		pc_o	 <= 32'h0;
 		inst_o <= 32'h0;
-	end
-	else if(isCHazard) begin
-		pc_o   <= 32'h0;
-    inst_o <= 32'h0;
 	end
   else begin
 		case(state)
@@ -86,7 +80,6 @@ always @(posedge clk) begin
 			WAIT: begin
 				if(if_valid_o && id_ready_i) begin
 					pc_o	 <= if_axi_araddr_o;
-					//inst_o <= axi_rdata_reg;
 					inst_o <= if_axi_rdata_i;
 				end
 			end
@@ -95,52 +88,43 @@ always @(posedge clk) begin
 end
 
 /***AXI4_LITE***/
-parameter AXI_IDLE = 1'b0,AXI_FETCH = 1'b1;
-reg axi_state;
-//reg [31:0] axi_rdata_reg;
+//parameter AXI_IDLE = 1'b0,AXI_FETCH = 1'b1;
+//reg axi_state;
 assign if_axi_arid_o    = 4'b0;
 assign if_axi_arlen_o   = 8'b0;
 assign if_axi_arsize_o  = 3'b0;
 assign if_axi_arburst_o = 2'b0;
 
-always @(posedge clk) begin
-	if(rst) axi_state <= AXI_IDLE;
-	else if(isCHazard) axi_state <= AXI_IDLE;
-	else begin
-		case(axi_state)
-			AXI_IDLE  : axi_state <= (if_ready_o && ifaddr_valid)             							? AXI_FETCH : axi_state;
-			AXI_FETCH : axi_state <= (if_axi_rvalid_i && if_axi_rready_o && !isCHazard_reg) ? AXI_IDLE  : axi_state;
-		endcase
-	end
-end
+//always @(posedge clk) begin
+	//if(rst) axi_state <= AXI_IDLE;
+	//else if(isCHazard) axi_state <= AXI_IDLE;
+	//else begin
+		//case(axi_state)
+			//AXI_IDLE  : axi_state <= (if_ready_o && ifaddr_valid)             							? AXI_FETCH : axi_state;
+			//AXI_FETCH : axi_state <= (if_axi_rvalid_i && if_axi_rready_o && !isCHazard_reg) ? AXI_IDLE  : axi_state;
+		//endcase
+	//end
+//end
 
 always @(posedge clk) begin
-  if(rst) begin
+  if(rst || isCHazard) begin
 		if_axi_arvalid_o <= 1'b0;
 		if_axi_rready_o  <= 1'b1;
 		if_axi_araddr_o  <= 32'h0;
   end 
-	else if(isCHazard) begin
-    if_axi_arvalid_o <= 1'b0;
-    if_axi_rready_o  <= 1'b1;
-    if_axi_araddr_o  <= 32'h0;
-  end
 	else begin
-    case (axi_state)
-      AXI_IDLE: begin
-        if(if_ready_o) begin
-					if_axi_arvalid_o <= 1'b1;
-					if_axi_araddr_o  <= pc_i;
-        end
+    case (state)
+      IDLE: begin
+				if_axi_arvalid_o <= 1'b1;
+				if_axi_araddr_o  <= pc_i;
       end
-      AXI_FETCH: begin
+      WAIT: begin
 				if(if_axi_arvalid_o && if_axi_arready_i) begin
 					if_axi_arvalid_o <= 1'b0;
 					if_axi_rready_o  <= 1'b1;
 				end
         if(if_axi_rvalid_i && if_axi_rready_o) begin
 					if_axi_rready_o <= 1'b0;
-					//axi_rdata_reg   <= if_axi_rdata_i;
 				end
       end
     endcase
