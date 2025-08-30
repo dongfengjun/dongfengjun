f884da647686d92d37fef1b98b2ce836b1d5ce5d
tracer-ysyx <tracer@ysyx.org>
2025-08-30 22:24:05 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:24:05 up 11:02,  1 user,  load average: 0.00, 0.01, 0.00

--- npc/vsrc/ysyx_24110017_IFU.v
+++ npc/vsrc/ysyx_24110017_IFU.v
@@ -14,9 +14,9 @@ module ysyx_24110017_IFU(
   output reg  [31:0] inst_o,
 /***AXI4*R***/
 	input  wire				 if_axi_arready_i,
-	output wire				 if_axi_arvalid_o,
+	output reg				 if_axi_arvalid_o,
 	output wire [ 3:0] if_axi_arid_o,
-	output wire [31:0] if_axi_araddr_o,
+	output reg  [31:0] if_axi_araddr_o,
 	output wire [ 7:0] if_axi_arlen_o,
 	output wire	[ 2:0] if_axi_arsize_o,
 	output wire	[ 1:0] if_axi_arburst_o,
@@ -47,8 +47,7 @@ end
 always @(posedge clk) begin
 	if(rst || flush) if_valid_o <= 1'b0;
 	else begin
-//		if(if_axi_rvalid_i && if_axi_rready_o && !if_axi_arvalid_o && !flush) begin
-		if(if_axi_rvalid_i && if_axi_rready_o) begin
+		if(if_axi_rvalid_i && if_axi_rready_o && !if_axi_arvalid_o && (state == WAIT)) begin
 			if_valid_o <= 1'b1;
 		end
 		if(if_valid_o && id_ready_i) begin
@@ -81,27 +80,22 @@ assign if_axi_arid_o    = 4'b0;
 assign if_axi_arlen_o   = 8'b0;
 assign if_axi_arsize_o  = 3'b0;
 assign if_axi_arburst_o = 2'b0;
-assign if_axi_arvalid_o = (flush) ? 1'b0  : if_axi_arvalid;
-assign if_axi_araddr_o  = (flush) ? 32'h0 : if_axi_araddr;
-reg if_axi_arvalid;
-reg [31:0]if_axi_araddr;
-
 always @(posedge clk) begin
   if(rst || flush) begin
-		if_axi_arvalid   <= 1'b0;
+		if_axi_arvalid_o <= 1'b0;
 		if_axi_rready_o  <= 1'b1;
-		if_axi_araddr    <= 32'h0;
+		if_axi_araddr_o  <= 32'h0;
   end 
 	else begin
     case (state)
       IDLE: begin
-				if_axi_arvalid <= 1'b1;
-				if_axi_araddr  <= pc_i;
+				if_axi_arvalid_o <= 1'b1;
+				if_axi_araddr_o  <= pc_i;
       end
       WAIT: begin
 				if(if_axi_arvalid_o && if_axi_arready_i) begin
-					if_axi_arvalid  <= 1'b0;
-					if_axi_rready_o <= 1'b1;
+					if_axi_arvalid_o <= 1'b0;
+					if_axi_rready_o  <= 1'b1;
 				end
         if(if_axi_rvalid_i && if_axi_rready_o) begin
 					if_axi_rready_o <= 1'b0;
