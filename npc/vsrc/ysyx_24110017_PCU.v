c38b25783bc1dfce1190177e4e481d6aa76b34b8
tracer-ysyx <tracer@ysyx.org>
2025-08-31 13:26:26 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:26:26 up 49 min,  1 user,  load average: 0.18, 0.04, 0.01

--- npc/vsrc/ysyx_24110017_PCU.v
+++ npc/vsrc/ysyx_24110017_PCU.v
@@ -14,13 +14,12 @@ assign pc_valid_o = !flush;
 
 localparam RESET_PC = 32'h30000000;
 
-wire [31:0] npc;
 always @(*) begin
   casez({rst, flush, if_ready_i})
-    3'b1??:  npc = RESET_PC;
-    3'b01?:  npc = dnpc_i;
-    3'b001:  npc = snpc_i;
-		default: npc = pc_o;
+    3'b1??:  pc_o <= RESET_PC;
+    3'b01?:  pc_o <= dnpc_i;
+    3'b001:  pc_o <= snpc_i;
+		default: pc_o <= pc_o;
 	endcase
 end
 
