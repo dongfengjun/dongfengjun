e818b2e371ff61206141bd3a878861081755ecdb
tracer-ysyx <tracer@ysyx.org>
2025-08-31 17:35:41 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:35:41 up  4:58,  1 user,  load average: 0.00, 0.03, 0.06

--- npc/vsrc/ysyx_24110017_CLINT.v
+++ npc/vsrc/ysyx_24110017_CLINT.v
@@ -38,9 +38,16 @@ always @(posedge clk) begin
       mtime[0] <= ~mtime[0];
 end
 
+always @(posedge rst) begin
+	if(rst)
+		mtime[1] <= 1'b0;
+	else
+		mtime[1] <= ~mtime[1];
+end
+
 genvar i;
 generate
-  for (i = 1; i < 64; i = i + 1) begin : counter_chain
+  for (i = 2; i < 64; i = i + 1) begin : counter_chain
     always @(posedge rst or negedge mtime[i-1]) begin
       if(rst)
         mtime[i] <= 1'b0;
