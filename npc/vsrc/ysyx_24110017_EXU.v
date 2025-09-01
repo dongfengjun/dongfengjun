ca0159867e81ff6469b26bd1745332ff480ba20b
tracer-ysyx <tracer@ysyx.org>
2025-09-01 13:33:18 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:33:18 up  2:28,  1 user,  load average: 0.11, 0.06, 0.04

--- npc/vsrc/ysyx_24110017_EXU.v
+++ npc/vsrc/ysyx_24110017_EXU.v
@@ -211,8 +211,8 @@ always@(posedge clk) begin
 	ls_wen_enable <= ls_wen;
 	ls_ren_enable <= ls_ren;
 end
-assign ls_wen_o <= ls_wen && !ls_wen_enable;
-assign ls_ren_o <= ls_ren && !ls_ren_enable;
+assign ls_wen_o = ls_wen && !ls_wen_enable;
+assign ls_ren_o = ls_ren && !ls_ren_enable;
 wire [31:0]ls_addr  = r1_i + offset;
 wire [31:0]ls_waddr = (op_i == 5'b01000) ? ls_addr : 32'h0;
 wire [31:0]ls_wdata = (op_i == 5'b01000) ? ((ls_waddr[1:0] == 0) ? r2_i : (ls_waddr[1:0] == 1) ? {r2_i[23:0],8'b0} : (ls_waddr[1:0] == 2) ? {r2_i[15:0],16'b0} : (ls_waddr[1:0] == 3) ? {r2_i[7:0],24'b0} : 32'h0) : 32'h0;
