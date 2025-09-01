7bec25f03faafeb626dc8f947e26e1c637a9c066
tracer-ysyx <tracer@ysyx.org>
2025-09-01 16:19:52 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:19:52 up  5:14,  1 user,  load average: 0.34, 0.13, 0.08

--- npc/vsrc/ysyx_24110017_EXU.v
+++ npc/vsrc/ysyx_24110017_EXU.v
@@ -35,7 +35,6 @@ module ysyx_24110017_EXU(
   output reg  [31:0] csrsw_o,
   output reg  [ 3:0] csrs_wen_o,
 
-	output wire	ls_wen_o,ls_ren_o,
 	output wire [31:0] ls_waddr_o,ls_wdata_o,ls_raddr_o,
 	input  wire [31:0] ls_rdata_i,
 	input  wire ls_done_i,
@@ -207,8 +206,6 @@ assign alu_res = (alu_sel == ADD) ? (a + b)
 
 /***LSU***/
 wire ls_valid = (op_i == 5'b01000) || (op_i == 5'b00000);
-assign ls_ren_o = (op_i == 5'b00000);
-assign ls_wen_o = (op_i == 5'b01000);
 wire [31:0]ls_addr  = r1_i + offset;
 assign ls_waddr_o = (op_i == 5'b01000) ? ls_addr : 32'h0;
 assign ls_wdata_o = (op_i == 5'b01000) ? ((ls_waddr_o[1:0] == 0) ? r2_i : (ls_waddr_o[1:0] == 1) ? {r2_i[23:0],8'b0} : (ls_waddr_o[1:0] == 2) ? {r2_i[15:0],16'b0} : (ls_waddr_o[1:0] == 3) ? {r2_i[7:0],24'b0} : 32'h0) : 32'h0;
