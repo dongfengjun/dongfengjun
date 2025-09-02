20d9ab207bf00bb07b01966c654be61a740d873f
tracer-ysyx <tracer@ysyx.org>
2025-09-02 17:14:23 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:14:23 up  6:39,  1 user,  load average: 0.20, 0.19, 0.37

--- npc/vsrc/ysyx_24110017_LSU.v
+++ npc/vsrc/ysyx_24110017_LSU.v
@@ -73,7 +73,7 @@ wire [ 2:0]ls_awsize_i = (op_i == 5'b01000 && funct3_i == 3'b000) ? 3'b000 : (op
 wire [ 2:0]ls_arsize_i = (op_i == 5'b00000 && (funct3_i == 3'b000 || funct3_i == 3'b100)) ? 3'b0 : (op_i == 5'b00000 && (funct3_i == 3'b001 || funct3_i == 3'b101)) ? 3'b1 : (op_i == 5'b00000 && funct3_i == 3'b010) ? 3'b10 : 3'b10;
 
 reg ls_read_done;
-always @(posedge clk)
+always @(posedge clk) begin
 	if(ls_axi_rvalid && ls_axi_rready) begin
 		ls_read_done <= 1'b1;
 		ls_rdata_o <= ls_rdata;
