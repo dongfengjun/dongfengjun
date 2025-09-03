7fb87ba90ec3962bcd76afdb629572b5116247aa
tracer-ysyx <tracer@ysyx.org>
2025-09-03 21:16:04 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:16:04 up 10:15,  1 user,  load average: 0.37, 0.41, 0.33

--- npc/vsrc/ysyx_24110017_CACHE.v
+++ npc/vsrc/ysyx_24110017_CACHE.v
@@ -22,7 +22,7 @@ module ysyx_24110017_CACHE #(n = 1, m = 4, w = 0) (
 	output reg  s_axi_arvalid,
 	output wire [ 3:0]s_axi_arid,
 	output reg  [31:0]s_axi_araddr,
-	output wire [ 7:0]s_axi_arlen,
+	output reg  [ 7:0]s_axi_arlen,
 	output wire [ 2:0]s_axi_arsize,
 	output wire [ 1:0]s_axi_arburst,
 	output reg  s_axi_rready,
@@ -93,7 +93,6 @@ module ysyx_24110017_CACHE #(n = 1, m = 4, w = 0) (
 	end
 
 	assign s_axi_arburst = 2'b01;
-	assign s_axi_arlen = (s_axi_arvalid) ? ((s_axi_araddr - 32'ha0000000 < 32'h20000000) ? CACHE_WIDTH - {6'b0,offset} - 1 : 8'b0) : 8'b0;
 	assign s_axi_arsize = 3'b10;
 	reg [m-3 : 0] burst_counter;
 
@@ -113,6 +112,9 @@ module ysyx_24110017_CACHE #(n = 1, m = 4, w = 0) (
 						if(hit == 0) begin
 							m_axi_arready <= 1'b0;
 							s_axi_arvalid <= 1'b1;
+							if(m_axi_araddr - 32'ha0000000 < 32'h20000000) begin
+								s_axi_arlen <= CACHE_WIDTH - {6'b0,offset} - 1;
+							end
 							burst_counter <= offset;
 						end
 						else begin
