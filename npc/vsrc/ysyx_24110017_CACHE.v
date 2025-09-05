c1a5eeab4f4ebd33ecc7dcbede5cb75c9f5c1bda
tracer-ysyx <tracer@ysyx.org>
2025-09-05 12:15:29 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:15:29 up  1:48,  1 user,  load average: 0.03, 0.11, 0.16

--- npc/vsrc/ysyx_24110017_CACHE.v
+++ npc/vsrc/ysyx_24110017_CACHE.v
@@ -76,7 +76,7 @@ module ysyx_24110017_CACHE
 	endfunction
 
 	assign m_axi_rvalid = axi_rvalid && !axi_rvalid_enable;
-	assign m_axi_rdata  = ((|hit) && (!unvalid)) ? cache_reg[offset][index * CACHE_WAY + log2(hit)] : 32'h0;
+	assign m_axi_rdata  = (|hit) ? cache_reg[offset][index * CACHE_WAY + log2(hit)] : 32'h0;
 	wire	 axi_rvalid   = (s_axi_arlen != 0) ? s_axi_rlast : (|hit) && !(m_axi_arvalid && m_axi_arready);
 	reg axi_rvalid_enable;
 	always @(posedge clk) begin
@@ -92,7 +92,7 @@ module ysyx_24110017_CACHE
 		if(rst) state <= IDLE;
 		else begin
 			case(state)
-				IDLE:    state <= (m_axi_arvalid && m_axi_arready) && ((hit == 0) || unvalid) ? TRANS : state;
+				IDLE:    state <= (m_axi_arvalid && m_axi_arready) && (hit == 0) || unvalid) ? TRANS : state;
 				TRANS:   state <= (m_axi_rready && m_axi_rvalid) ? IDLE : state;
         default: state <= state;
 			endcase
@@ -104,7 +104,7 @@ module ysyx_24110017_CACHE
 	reg [m-3 : 0] burst_counter;
 
 	always @(posedge clk) begin
-		if(fencei_i) begin
+		if(fencei_i || unvalid) begin
 			integer f;
 			for (f = 0; f < CACHE_WIDTH; f = f + 1) begin : fencei
 				valid_reg[f] <= 0;
