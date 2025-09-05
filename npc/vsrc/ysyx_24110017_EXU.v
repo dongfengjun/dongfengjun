5a41890c82a386fe7d5e33f9512318d2abaa903b
tracer-ysyx <tracer@ysyx.org>
2025-09-05 16:38:49 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:38:49 up  6:12,  1 user,  load average: 0.66, 0.49, 0.39

--- npc/vsrc/ysyx_24110017_EXU.v
+++ npc/vsrc/ysyx_24110017_EXU.v
@@ -155,10 +155,10 @@ wire [31:0]xrd =
 /***R_add~R_remu***/
 				(op_i == 5'b01100) ? (alu_res) :
 /*********/
-				(op_i == 5'b11011) ? pc_i + 4			: //I_jal
-				(op_i == 5'b11001) ? pc_i + 4			: //I_jalr
-				(op_i == 5'b01101) ? imm_i				: //U_lui
-				(op_i == 5'b00101) ? pc_i + imm_i :	//U_auipc
+				(op_i == 5'b11011) ? pc_i + 4	: //I_jal
+				(op_i == 5'b11001) ? pc_i + 4	: //I_jalr
+				(op_i == 5'b01101) ? imm_i		: //U_lui
+				(op_i == 5'b00101) ? add_res  :	//U_auipc
 /***LSU***/
 				(op_i == 5'b00000) ? ls_rdata_i   : //LOAD
 /***CSRU***/
@@ -224,7 +224,6 @@ assign alu_res = (alu_sel == ADD) ? (a + b)
 
 /***LSU***/
 wire ls_valid = (op_i == 5'b01000) || (op_i == 5'b00000);
-//assign ls_addr_o = (ls_valid) ? (r1_i + offset) : 32'h0;
 assign ls_addr_o = (ls_valid) ? add_res : 32'h0;
 assign ls_wdata_o = (op_i == 5'b01000) ? ((ls_addr_o[1:0] == 0) ? r2_i 
 			: (ls_addr_o[1:0] == 1) ? {r2_i[23:0],8'b0} 
@@ -232,7 +231,6 @@ assign ls_wdata_o = (op_i == 5'b01000) ? ((ls_addr_o[1:0] == 0) ? r2_i
 			: (ls_addr_o[1:0] == 3) ? {r2_i[7:0],24'b0} : 32'h0) : 32'h0;
 
 /***BU***/
-wire [31:0]offset = imm_i;
 wire jalen,jalren,beqen,bneen,blten,bgeen,bltuen,bgeuen,ecall_en,mret_en;
 assign jalen		= (op_i == 5'b11011);
 assign jalren		= (op_i == 5'b11001);
@@ -245,22 +243,6 @@ assign bgeuen		= (op_i == 5'b11000 && funct3_i == 3'b111 && (r1_i >= r2_i));
 assign ecall_en = (op_i == 5'b11100 && {offset[9],offset[6],offset[1],offset[0]} == 4'b0000 && funct3_i == 3'b0);
 assign mret_en	= (op_i == 5'b11100 && {offset[9],offset[6],offset[1],offset[0]} == 4'b1010 && funct3_i == 3'b0);
 
-/***
-wire [31:0]dnpc = (jalen) ? (pc_i + offset)	//jal
-	: (jalren)   ? ((r1_i + offset) & ~1) //jalr
-	: (beqen)    ? (pc_i + offset)	//beq
-	: (bneen)    ? (pc_i + offset)	//bne
-	: (blten)    ? (pc_i + offset)	//blt
-	: (bgeen)    ? (pc_i + offset)	//bge
-	: (bltuen)   ? (pc_i + offset)	//bltu
-	:	(bgeuen)   ? (pc_i + offset)	//bgeu
-	: (ecall_en) ? mtvec_i  //ecall
-	: (mret_en)  ? mepc_i  //mret
-	: pc_i + 4;
-***/
-
-wire [31:0]add_res = (ls_valid || jalren) ? (r1_i + offset) : pc_i + offset;
-
 wire [31:0]dnpc = (jalen) ? add_res	//jal
 	: (jalren) ? (add_res & ~1) //jalr
 	: (beqen) ? add_res	//beq
@@ -272,5 +254,7 @@ wire [31:0]dnpc = (jalen) ? add_res	//jal
 	: (ecall_en) ? mtvec_i  //ecall
 	: (mret_en) ? mepc_i  //mret
 	: pc_i + 4;
+
+wire [31:0]add_res = (ls_valid || jalren) ? (r1_i + imm_i) : (pc_i + imm_i);
 
 endmodule
