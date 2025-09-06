93337a8373bd6ac8e71b96ee69cc92bf7ab371d2
tracer-ysyx <tracer@ysyx.org>
2025-09-06 23:21:56 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:21:56 up 10:31,  1 user,  load average: 0.04, 0.17, 0.32

--- npc/vsrc/ysyx_24110017_EXU.v
+++ npc/vsrc/ysyx_24110017_EXU.v
@@ -227,6 +227,7 @@ assign ls_wdata_o =
 			: (ls_addr_o[1:0] == 3) ? {r2_i[7:0],24'b0} : 32'h0;
 
 /***BU***/
+/****
 wire jalen,jalren,beqen,bneen,blten,bgeen,bltuen,bgeuen,ecall_en,mret_en;
 assign jalen		= (op_i == 5'b11011);
 assign jalren		= (op_i == 5'b11001);
@@ -236,8 +237,8 @@ assign blten		= (op_i == 5'b11000 && funct3_i == 3'b100 &&  slt_res);
 assign bgeen		= (op_i == 5'b11000 && funct3_i == 3'b101 && !slt_res);
 assign bltuen		= (op_i == 5'b11000 && funct3_i == 3'b110 &&  slt_res);
 assign bgeuen		= (op_i == 5'b11000 && funct3_i == 3'b111 && !slt_res);
-assign ecall_en = (op_i == 5'b11100 && imm_i[1:0] == 2'b00 && funct3_i == 3'b0);
-assign mret_en	= (op_i == 5'b11100 && imm_i[1:0] == 2'b10 && funct3_i == 3'b0);
+assign ecall_en = (op_i == 5'b11100 && {imm_i[9],imm_i[6],imm_i[1],imm_i[0]} == 4'b0000 && funct3_i == 3'b0);
+assign mret_en	= (op_i == 5'b11100 && {imm_i[9],imm_i[6],imm_i[1],imm_i[0]} == 4'b1010 && funct3_i == 3'b0);
 
 wire [31:0]dnpc = (jalen) ? add_res	//jal
 	: (jalren) ? (add_res & ~1) //jalr
@@ -250,5 +251,31 @@ wire [31:0]dnpc = (jalen) ? add_res	//jal
 	: (ecall_en) ? mtvec_i  //ecall
 	: (mret_en) ? mepc_i  //mret
 	: add_pc_4;
+***/
+assign ecall_en = (op_i == 5'b11100 && {imm_i[9],imm_i[6],imm_i[1],imm_i[0]} == 4'b0000 && funct3_i == 3'b0);
+localparam [3:0] jal = 4'd1, jalr = 4'd2, beq = 4'd3, bne = 4'd4, blt = 4'd5, bge = 4'd6, bltu = 4'd7, bgeu = 4'd8, ecall = 4'd9, mret = 4'd10;
+wire [3:0] sel = 
+   (op_i == 5'b11011) ? jal
+ : (op_i == 5'b11001) ? jalr
+ : (op_i == 5'b11000 && funct3_i == 3'b000 && (r1_i == r2_i)) ? beq
+ : (op_i == 5'b11000 && funct3_i == 3'b001 && (r1_i != r2_i)) ? bne
+ : (op_i == 5'b11000 && funct3_i == 3'b100 &&  slt_res) ? blt
+ : (op_i == 5'b11000 && funct3_i == 3'b101 && !slt_res) ? bge
+ : (op_i == 5'b11000 && funct3_i == 3'b110 &&  slt_res) ? bltu
+ : (op_i == 5'b11000 && funct3_i == 3'b111 && !slt_res) ? bgeu
+ : (op_i == 5'b11100 && {imm_i[9],imm_i[6],imm_i[1],imm_i[0]} == 4'b0000 && funct3_i == 3'b0) ? ecall
+ : (op_i == 5'b11100 && {imm_i[9],imm_i[6],imm_i[1],imm_i[0]} == 4'b1010 && funct3_i == 3'b0) ? mret : 4'd0;
+
+wire [31:0]dnpc = (sel == jal) ? add_res //jal
+ : (sel == jalr) ? (add_res & ~1) //jalr
+ : (sel == beq) ? add_res //beq
+ : (sel == bne) ? add_res //bne
+ : (sel == blt) ? add_res //blt
+ : (sel == bge) ? add_res //bge
+ : (sel == bltu) ? add_res  //bltu
+ : (sel == bgeu) ? add_res  //bgeu
+ : (sel == ecall) ? mtvec_i  //ecall
+ : (sel == mret) ? mepc_i  //mret
+ : add_pc_4;
 
 endmodule
