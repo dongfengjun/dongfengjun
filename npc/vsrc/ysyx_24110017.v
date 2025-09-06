4481fe3bc76104e69b4e175a8ab09307ca0f1110
tracer-ysyx <tracer@ysyx.org>
2025-09-07 02:31:55 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:31:55 up  1:27,  1 user,  load average: 0.40, 0.60, 0.57

--- npc/vsrc/ysyx_24110017.v
+++ npc/vsrc/ysyx_24110017.v
@@ -139,14 +139,14 @@ wire [31:0]r1,r2;
 wire [31:0]mepc,mstatus,mcause,mtvec;
 //wire [31:0]mvendorid,marchid; //ID
 
-ysyx_24110017_BTB #(1,0,16,8,1,0,16,16) BTB(clock,reset,if_axi_araddr,snpc,prepc,pc_if,prepc_en);
+ysyx_24110017_BTB #(4,0,16,8,4,0,16,16) BTB(clock,reset,if_axi_araddr,snpc,prepc,pc_if,prepc_en);
 ysyx_24110017_IFU IFU(clock,reset,isCHazard,
 		if_valid,id_ready,dnpc_ex,snpc,pc_if,inst_if,
 		if_axi_arready,if_axi_arvalid,if_axi_arid,if_axi_araddr,
 		if_axi_arlen,if_axi_arsize,if_axi_arburst,
 		if_axi_rready,if_axi_rvalid,if_axi_rid,if_axi_rdata,if_axi_rresp,if_axi_rlast
 );
-ysyx_24110017_CACHE #(1,4,0,8) ICACHE(clock,reset,fencei_id, //w < n
+ysyx_24110017_CACHE #(4,4,0,8) ICACHE(clock,reset,fencei_id, //w < n
 		if_axi_arready,if_axi_arvalid,if_axi_arid,if_axi_araddr,
 		if_axi_arlen,if_axi_arsize,if_axi_arburst,
 		if_axi_rready,if_axi_rvalid,if_axi_rid,if_axi_rdata,if_axi_rresp,if_axi_rlast,
