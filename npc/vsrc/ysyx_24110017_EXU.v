7ad77913f2f27ce6764ea8efec4cb9720fdf12a7
tracer-ysyx <tracer@ysyx.org>
2025-09-06 15:17:06 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:17:06 up  2:26,  1 user,  load average: 0.00, 0.00, 0.06

--- npc/vsrc/ysyx_24110017_EXU.v
+++ npc/vsrc/ysyx_24110017_EXU.v
@@ -69,7 +69,7 @@ always@(posedge clk) begin
 `ifndef YOSYS_STA
 				pc_o	 <= pc_i;
 				inst_o <= inst_i;
-				npc_o  <= npc;
+				npc_o  <= dnpc;
 `endif
 				xrd_o  <= xrd;
 			end
@@ -257,20 +257,6 @@ wire [31:0]dnpc = (jalen) ? add_res	//jal
 	:	(bgeuen) ? add_res	//bgeu
 	: (ecall_en) ? mtvec_i  //ecall
 	: (mret_en) ? mepc_i  //mret
-	: 32'h0;
-
-`ifndef YOSYS_STA
-wire [31:0]npc = (jalen) ? add_res //jal
-	: (jalren) ? (add_res & ~1) //jalr
-  : (beqen) ? add_res //beq
-  : (bneen) ? add_res //bne
-  : (blten) ? add_res //blt
-  : (bgeen) ? add_res //bge
-  : (bltuen) ? add_res  //bltu
-  : (bgeuen) ? add_res  //bgeu
-  : (ecall_en) ? mtvec_i  //ecall
-  : (mret_en) ? mepc_i  //mret
-  : pc_i + 4;
-`endif
+	: add_pc_4;
 
 endmodule
