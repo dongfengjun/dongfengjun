bc897a31805277b60c439d64d526bc3f89bcc843
tracer-ysyx <tracer@ysyx.org>
2025-09-07 18:27:38 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:27:38 up  3:02,  1 user,  load average: 0.01, 0.03, 0.19

--- npc/vsrc/ysyx_24110017.v
+++ npc/vsrc/ysyx_24110017.v
@@ -222,8 +222,7 @@ ysyx_24110017_Reg #(32, 32'h1800) mstatus_reg (clock,reset,xrd_ex,mstatus,csrs_w
 ysyx_24110017_Reg #(32, 32'b0)    mcause_reg  (clock,reset,xrd_ex,mcause ,csrs_wen_ex[2]);
 ysyx_24110017_Reg #(32, 32'b0)    mtvec_reg   (clock,reset,xrd_ex,mtvec  ,csrs_wen_ex[3]);
 
-wire isRAW = 1'b0;//((rs1_id != 0) && (((!ls_ready) && (rs1_id == rd_ex)) || (rs1_id == rd_ls))) || 
-						 //((rs2_id != 0) && (((!ls_ready) && (rs2_id == rd_ex)) || (rs2_id == rd_ls)));
+wire isRAW = ((rs1 != 0) && (rs1 == rd_id) && (!gpr_wen_ex)) || ((rs2 != 0) && (rs2 == rd_id) && (!gpr_wen_ex));
 
 reg CHazarden;
 always @(posedge clock) begin
