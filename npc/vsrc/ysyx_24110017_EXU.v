98668598c88e963a5399641c70d9af2f8803ea44
tracer-ysyx <tracer@ysyx.org>
2025-09-07 18:44:08 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:44:08 up  3:18,  1 user,  load average: 0.06, 0.13, 0.17

--- npc/vsrc/ysyx_24110017_EXU.v
+++ npc/vsrc/ysyx_24110017_EXU.v
@@ -100,14 +100,14 @@ always@(posedge clk) begin
 end
 
 always@(posedge clk) begin
-  casez({flush_i,state})
-		2'b1? : rd_o <= 4'b0;
-    2'b01 : begin
+//  casez({flush_i,state})
+//		2'b1? : rd_o <= 4'b0;
+//    2'b01 : begin
       if(updata) rd_o <= rd_i;
-    end
-		default : begin
-		end
-	endcase
+//    end
+//		default : begin
+//		end
+//	endcase
 end
 
 always@(posedge clk) begin
