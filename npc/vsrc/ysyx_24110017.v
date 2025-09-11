94c65f6dd5f79feb5379c7a6484ff99d1ef991bb
tracer-ysyx <tracer@ysyx.org>
2025-09-11 13:49:52 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:49:52 up  3:09,  1 user,  load average: 0.03, 0.24, 0.23

--- npc/vsrc/ysyx_24110017.v
+++ npc/vsrc/ysyx_24110017.v
@@ -566,8 +566,8 @@ assign if_axi_araddr_o  = pc;
 
 always @(posedge clk) begin
   if(rst || flush) begin
-		if_axi_arvalid_o <= 1'b1;
-		if_axi_rready_o  <= 1'b1;
+		if_axi_arvalid_o <= 1'b0;
+		if_axi_rready_o  <= 1'b0;
   end 
 	else begin
     case (state)
