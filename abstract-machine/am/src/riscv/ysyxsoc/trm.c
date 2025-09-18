a101bc5145696cd31885fe78b67e692aae4b9cb5
11111-dongfengjun <dongfengjun2024@163.com>
2025-09-18 16:42:33 +0800

    CI 3.1.2 uart

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -20,8 +20,8 @@ static const char mainargs[] = MAINARGS;
 
 #define UART_BASE 0X10000000
 void putch(char ch) {
-	while(!(inb(UART_BASE + 0X5) & 0x20));
-	outl(UART_BASE, ch);
+	while((inb(UART_BASE + 0X5) & 0x20) == 0){};
+	outb(UART_BASE, ch);
 }
 
 void halt(int code) {
