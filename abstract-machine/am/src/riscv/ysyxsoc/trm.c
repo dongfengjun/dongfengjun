3d433be291bb13d1c0b440f023456de3f29adcb8
tracer-ysyx <tracer@ysyx.org>
2025-09-18 19:45:19 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:45:19 up  5:13,  1 user,  load average: 0.17, 0.14, 0.06

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -97,7 +97,7 @@ void uart_init(void) {
 	outb(UART_BASE + 1, 0x00);
 	outb(UART_BASE + 3, 0x80);
 	outb(UART_BASE    , 0x00);
-	outb(UART_BASE + 1, 0x01);
+	outb(UART_BASE + 1, 0x10);
 	outb(UART_BASE + 3, 0x03);
 	while (inb(UART_BASE + 5) & 0x01) {
     (void)inb(UART_BASE);
