59e49fe155a5803893b496c98fed864386413e0a
tracer-ysyx <tracer@ysyx.org>
2025-09-21 00:30:11 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:30:11 up  3:51,  1 user,  load average: 0.31, 0.30, 0.25

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -141,7 +141,7 @@ uint32_t flash_read(uint32_t addr) {
 void _trm_init() {
 	uart_init(); //uart16500 init + difftest_skip_ref
 	bootloader_fsbl();
-	idcsrs_init(); //开DIFFTEST时暂时不用
+//	idcsrs_init(); //开DIFFTEST时暂时不用
 	int ret = main(mainargs);
 	halt(ret);
 }
