72d934c33a0290d6d342eed26e6a3b8f7d8d0be7
tracer-ysyx <tracer@ysyx.org>
2025-09-21 12:22:00 +0800

    > compile NEMU ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:22:00 up 15 min,  1 user,  load average: 0.14, 0.19, 0.21

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -20,7 +20,12 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    * Then return the address of the interrupt/exception vector.
    */
 	cpu.csr.mepc = epc;
-	cpu.csr.mcause = NO;
+	if(NO == -1) {
+		cpu.csr.mcause = 11;
+	}
+	else {
+		panic("unknow mcause:%x",NO);
+	}
   return cpu.csr.mtvec;
 }
 
