2d729129699524e78b1ab813b2c9befc3756aa78
tracer-ysyx <tracer@ysyx.org>
2025-09-22 10:56:36 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:56:36 up 22:50,  1 user,  load average: 0.03, 0.05, 0.03

--- npc/vsrc/ysyx_24110017.v
+++ npc/vsrc/ysyx_24110017.v
@@ -299,7 +299,7 @@ end
 /***DPIC*etrace***/
 import "DPI-C" function void npc_trap();
 always@(*) begin
-  if(ec_valid && inst_if == 32'b00000000000100000000000001110011) begin
+  if(ex_valid && inst_if == 32'b00000000000100000000000001110011) begin
     npc_trap();
   end
 end
