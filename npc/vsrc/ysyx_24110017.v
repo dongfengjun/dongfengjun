0b160460036c055f083f03d2898f02067a6b90b0
tracer-ysyx <tracer@ysyx.org>
2025-09-25 19:47:20 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:47:20 up  8:34,  1 user,  load average: 0.07, 0.16, 0.22

--- npc/vsrc/ysyx_24110017.v
+++ npc/vsrc/ysyx_24110017.v
@@ -1,7 +1,7 @@
 `ifdef __ICARUS__
 	`timescale 1ns/1ps
 `endif
-`define ysyx_24110017_SIMFAST
+//`define ysyx_24110017_SIMFAST
 
 module ysyx_24110017(
 	input	 wire clock,
@@ -172,7 +172,7 @@ always @(posedge clock) begin
 end
 wire isCHazard = CHazarden && (dnpc_ex != pc_id) && (pc_id != 32'h0) && (dnpc_ex != 32'h0);
 
-ysyx_24110017_BTB #(1,0,16,8,1,0,16,16) BTB(clock,reset,if_axi_araddr,snpc,prepc,pc_if,prepc_en);
+ysyx_24110017_BTB #(1,16,8,1,16,16) BTB(clock,reset,if_axi_araddr,snpc,prepc,pc_if,prepc_en);
 ysyx_24110017_IFU IFU(clock,reset,isCHazard,
 	if_valid,id_ready,dnpc_ex,snpc,pc_if,inst_if,
 	if_axi_arready,if_axi_arvalid,if_axi_arid,if_axi_araddr,
@@ -474,7 +474,7 @@ endmodule
 
 //`define ysyx_24110017_Associative //w != 0
 module ysyx_24110017_BTB 
-#(B_N = 2, B_W = 0, BTAG = 16, BTARG = 8, J_N = 1, J_W = 0, JTAG = 16, JTARG = 16) 
+#(B_N = 2, BTAG = 16, BTARG = 8, J_N = 1, JTAG = 16, JTARG = 16) 
 (
 	input  wire        clk,
 	input  wire        rst,
@@ -487,82 +487,28 @@ module ysyx_24110017_BTB
 	input  wire [ 1:0] prepc_en_i
 );
 	
-	reg  [JTARG-1:0]        jsnpc_reg [(1<<J_N)-1:0];
-  reg  [JTAG-3-J_N+J_W:0] jtag_reg  [(1<<J_N)-1:0];
-  wire [JTAG-3-J_N+J_W:0] jtag   = pc_i[JTAG-1:2+J_N-J_W];
-  wire [J_N-J_W-1:0]			jindex = pc_i[J_N-J_W+1:2];
+	reg  [JTARG - 1 : 0]      jsnpc_reg [(1<<J_N) - 1 : 0];
+  reg  [JTAG - 3 - J_N : 0] jtag_reg  [(1<<J_N) - 1 : 0];
+  wire [JTAG - 3 - J_N : 0] jtag   = pc_i[JTAG - 1 : 2 + J_N];
+  wire [J_N-1 : 0]			    jindex = pc_i[J_N + 1 : 2];
 
-	reg  [BTARG-1:0]        bsnpc_reg [(1<<B_N)-1:0];
-  reg  [BTAG-3-B_N+B_W:0] btag_reg  [(1<<B_N)-1:0];
-	wire [BTAG-3-B_N+B_W:0] btag   = pc_i[BTAG-1:2+B_N-B_W];
-	wire [B_N-B_W-1 :0]			bindex = pc_i[B_N-B_W+1:2];
+	reg  [BTARG - 1 : 0]      bsnpc_reg [(1<<B_N) - 1 : 0];
+  reg  [BTAG - 3 - B_N : 0] btag_reg  [(1<<B_N) - 1 : 0];
+	wire [BTAG - 3 - B_N : 0] btag   = pc_i[BTAG - 1 : 2 + B_N];
+	wire [B_N - 1 : 0]			  bindex = pc_i[B_N + 1 : 2];
 
-	wire [JTAG-3-J_N+J_W:0] jtag_pre   = prepc_tag_i[JTAG-1:2+J_N-J_W];
-	wire [J_N-J_W-1:0]      jindex_pre = prepc_tag_i[J_N-J_W+1:2];
-	wire [BTAG-3-B_N+B_W:0] btag_pre   = prepc_tag_i[BTAG-1:2+B_N-B_W];
-	wire [B_N-B_W-1 :0]     bindex_pre = prepc_tag_i[B_N-B_W+1:2];
+	wire [JTAG - 3 - J_N : 0] jtag_pre   = prepc_tag_i[JTAG - 1 : 2 + J_N];
+	wire [J_N - 1 : 0]        jindex_pre = prepc_tag_i[J_N + 1 : 2];
+	wire [BTAG - 3 - B_N : 0] btag_pre   = prepc_tag_i[BTAG - 1 : 2 + B_N];
+	wire [B_N - 1 : 0]        bindex_pre = prepc_tag_i[B_N + 1 : 2];
 
-	function integer blog2;
-    input [(1<<B_W) - 1 : 0] value;
-    integer loop_var;
-    begin
-      for (loop_var = 0; loop_var < (1<<B_W); loop_var = loop_var + 1) begin
-        if(value != 0) begin
-          value = value >> 1;
-          blog2 = loop_var;
-        end
-      end
-    end
-  endfunction
-	function integer jlog2;
-    input [(1<<J_W) - 1 : 0] value;
-    integer loop_var;
-    begin
-      for (loop_var = 0; loop_var < (1<<J_W); loop_var = loop_var + 1) begin
-	      if(value != 0) begin
-          value = value >> 1;
-          jlog2 = loop_var;
-        end
-      end
-    end
-  endfunction
+	wire bhit;
+  assign bhit[i1] = (btag == btag_reg[bindex]);
 
-	wire [(1<<B_W)-1:0]bhit;
-	generate 
-    genvar i1; 
-      for(i1 = 0; i1 < (1<<B_W); i1 = i1 + 1) begin : comparator_b
-        assign bhit[i1] = (btag == btag_reg[bindex * (1<<B_W) + i1]);
-			end
-	endgenerate
+	wire jhit;
+  assign jhit[i2] = (jtag == jtag_reg[jindex]);
 
-	wire [(1<<J_W)-1:0]jhit;
-  generate
-    genvar i2;
-      for(i2 = 0; i2 < (1<<J_W); i2 = i2 + 1) begin : comparator_j
-        assign jhit[i2] = (jtag == jtag_reg[jindex * (1<<J_W) + i2]);
-      end
-  endgenerate
-
-`ifdef ysyx_24110017_Associative
-	wire [(1<<B_W)-1:0]balready;
-  generate
-    genvar i3;
-      for(i3 = 0; i3 < (1<<B_W); i3 = i3 + 1) begin : comparator_b
-        assign balready[i3] = (btag_pre == btag_reg[bindex_pre * (1<<B_W) + i3]);
-      end
-  endgenerate
-
-	wire [(1<<J_W)-1:0]jalready;
-  generate
-    genvar i4;
-      for(i4 = 0; i4 < (1<<J_W); i4 = i4 + 1) begin : comparator_j
-        assign jalready[i4] = (jtag_pre == jtag_reg[jindex_pre * (1<<J_W) + i4]);
-      end
-  endgenerate
-`endif
-
-//	assign snpc_o = (|jhit) ? {pc_i[31:JTARG],jsnpc_reg[jindex * (1<<J_W) + jlog2(jhit)]} : (|bhit) ? {pc_i[31:BTARG],bsnpc_reg[bindex * (1<<B_W) + blog2(bhit)]} : pc_i + 4; //直接相联hit为1位
-assign snpc_o = (|jhit) ? {pc_i[31:JTARG],jsnpc_reg[jindex * (1<<J_W)]} : (|bhit) ? {pc_i[31:BTARG],bsnpc_reg[bindex * (1<<B_W)]} : pc_i + 4;
+	assign snpc_o = (|jhit) ? {pc_i[31:JTARG],jsnpc_reg[jindex]} : (|bhit) ? {pc_i[31:BTARG],bsnpc_reg[bindex]} : pc_i + 4;
 
   always @(posedge clk) begin
 		if(rst) begin
@@ -572,16 +518,8 @@ assign snpc_o = (|jhit) ? {pc_i[31:JTARG],jsnpc_reg[jindex * (1<<J_W)]} : (|bhit
       end
     end
 		else begin
-`ifdef ysyx_24110017_Associative
-			if(prepc_en_i[1] && (!jalready)) begin
-`else
 			if(prepc_en_i[1]) begin
-`endif
-				integer a;
-				for (a = 1; a < (1<<J_W); a = a + 1) begin
-					jsnpc_reg[jindex_pre * (1<<J_W) + a] <= jsnpc_reg[jindex_pre * (1<<J_W) + a - 1];
-				end
-				jsnpc_reg[jindex_pre * (1<<J_W)] <= prepc_i[JTARG-1:0];
+				jsnpc_reg[jindex_pre] <= prepc_i[JTARG-1:0];
 			end
 		end
   end
@@ -594,16 +532,9 @@ assign snpc_o = (|jhit) ? {pc_i[31:JTARG],jsnpc_reg[jindex * (1<<J_W)]} : (|bhit
       end
 		end
 		else begin
-`ifdef ysyx_24110017_Associative
-			if(prepc_en_i[1] && (!jalready)) begin
-`else
 			if(prepc_en_i[1]) begin
-`endif
 				integer b;
-				for (b = 1; b < (1<<J_W); b = b + 1) begin
-					jtag_reg[jindex_pre * (1<<J_W) + b]  <= jtag_reg[jindex_pre * (1<<J_W) + b - 1];
-				end
-			jtag_reg[jindex_pre * (1<<J_W)]  <= prepc_tag_i[JTAG-1:2+J_N-J_W];
+			jtag_reg[jindex_pre]  <= prepc_tag_i[JTAG-1:2+J_N];
 			end
 		end
 	end
@@ -616,16 +547,8 @@ assign snpc_o = (|jhit) ? {pc_i[31:JTARG],jsnpc_reg[jindex * (1<<J_W)]} : (|bhit
 	    end
 	  end
 		else begin
-`ifdef ysyx_24110017_Associative
-			if(prepc_en_i[0] && (!balready)) begin
-`else
 			if(prepc_en_i[0]) begin
-`endif
-				integer c;
-				for (c = 1; c < (1<<B_W); c = c + 1) begin
-					bsnpc_reg[bindex_pre * (1<<B_W) + c] <= bsnpc_reg[bindex_pre * (1<<B_W) + c - 1];
-				end
-				bsnpc_reg[bindex_pre * (1<<B_W)] <= prepc_i[BTARG-1:0];
+				bsnpc_reg[bindex_pre] <= prepc_i[BTARG-1:0];
 			end
 		end
 	end
@@ -638,16 +561,8 @@ assign snpc_o = (|jhit) ? {pc_i[31:JTARG],jsnpc_reg[jindex * (1<<J_W)]} : (|bhit
       end
 		end
 		else begin
-`ifdef ysyx_24110017_Associative
-			if(prepc_en_i[0] && (!balready)) begin
-`else
 			if(prepc_en_i[0]) begin
-`endif
-				integer d;
-				for (d = 1; d < (1<<B_W); d = d + 1) begin
-					btag_reg[bindex_pre * (1<<B_W) + d]  <= btag_reg[bindex_pre * (1<<B_W) + d - 1];                            
-				end
-				btag_reg[bindex_pre * (1<<B_W)]  <= prepc_tag_i[BTAG-1:2+B_N-B_W];
+				btag_reg[bindex_pre]  <= prepc_tag_i[BTAG-1:2+B_N];
 			end
 		end
 	end
