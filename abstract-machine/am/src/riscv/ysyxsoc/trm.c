3e00527341e57ac1a63285cf540724cc26c7b3e8
tracer-ysyx <tracer@ysyx.org>
2025-09-28 14:20:08 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:20:08 up  3:04,  1 user,  load average: 0.19, 0.21, 0.18

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -106,25 +106,14 @@ void uart_init(void) {
 	outb(UART_BASE + 1, 0x01);
 }
 
-/***
-#define IDCSR_BASE 0X01000000
-void idcsrs_init(void) {
-	volatile uint32_t *mvendorid = (uint32_t *)(IDCSR_BASE);
-	putch((mvendorid[0] >> 24) & 0xFF);
-	putch((mvendorid[0] >> 16) & 0xFF);
-	putch((mvendorid[0] >> 8) & 0xFF);
-	putch(mvendorid[0] & 0xFF);
-	volatile uint32_t *marchid = (uint32_t *)(IDCSR_BASE + 0x4);
-	printf("%d\n",marchid[0]);
-}
-***/
 void idcsrs_init(void) {
 	uint32_t idcsr;
   asm volatile ("csrr %0, 0xFF0" : "=r" (idcsr));
-  putch((idcsr >> 24) & 0xFF);
-	putch((idcsr >> 16) & 0xFF);
-  putch((idcsr >> 8) & 0xFF);
-  putch(idcsr & 0xFF);
+  //putch((idcsr >> 24) & 0xFF);
+	//putch((idcsr >> 16) & 0xFF);
+  //putch((idcsr >> 8) & 0xFF);
+  //putch(idcsr & 0xFF);
+	printf("%s", idcsr);
 	asm volatile ("csrr %0, 0xFF1" : "=r" (idcsr));
   printf("%d\n", idcsr);
 }
