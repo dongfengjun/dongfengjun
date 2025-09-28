71659a57f098ccb0d83c1bec8e4d1c0a884a4428
tracer-ysyx <tracer@ysyx.org>
2025-09-28 14:20:28 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:20:28 up  3:04,  1 user,  load average: 0.20, 0.21, 0.18

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -113,7 +113,7 @@ void idcsrs_init(void) {
 	//putch((idcsr >> 16) & 0xFF);
   //putch((idcsr >> 8) & 0xFF);
   //putch(idcsr & 0xFF);
-	printf("%s", idcsr);
+	printf("%s",(char)idcsr);
 	asm volatile ("csrr %0, 0xFF1" : "=r" (idcsr));
   printf("%d\n", idcsr);
 }
