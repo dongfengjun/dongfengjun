0c65687a0cf66373fd6cf11fcf2940822369b33d
tracer-ysyx <tracer@ysyx.org>
2025-10-23 22:36:02 +0800

    > sim RTL ysyx_24110017 dongfengjun Linux xi-VMware-Virtual-Platform 6.8.0-50-generic #51-Ubuntu SMP PREEMPT_DYNAMIC Sat Nov  9 17:58:29 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:36:02 up  7:35,  1 user,  load average: 0.78, 0.62, 0.57

--- abstract-machine/am/src/riscv/ysyxsoc/vga.c
+++ abstract-machine/am/src/riscv/ysyxsoc/vga.c
@@ -38,9 +38,9 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 			fb[640 * i + j] = pixels[ctl->w * (i-(ctl->y)) + (j-(ctl->x))];	//w*i+j 
 		}
 	}
-	if(ctl->sync) {
+	//if(ctl->sync) {
 		outl(SYNC_ADDR, 1);
-	}
+	//}
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
